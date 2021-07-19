ee2c3f90fb59ad17062cff564dc67459cf122292
Joseph Maldjian <josephavm@icloud.com>
2021-07-19 01:03:35 -0500

    Move new DashboardContainer to main DashboardContainer

--- src/components/dashboard/DashboardContainer.jsx
+++ src/components/dashboard/DashboardContainer.jsx
@@ -1,146 +1,163 @@
-import {
-    Component,
-    useRef
-} from "react"
-import axios from 'axios'
-import {
-    Link,
-    Redirect
-} from "react-router-dom"
-import jwt from "jsonwebtoken"
-import Cookies from 'js-cookie';
-import ReactLoading from 'react-loading'
-import ServerRow from './ServerRow'
-import prettyBytes from 'pretty-bytes'
-import Loading from 'react-loading-bar'
-import 'react-loading-bar/dist/index.css'
+import ServerRow from "./ServerRow";
+import Header from '../Header'
+import Navigation from '../Navigation'
+import getServers from "../../api/server/getServers";
+import { useEffect, useState} from 'react'
+import getServerResources from "../../api/server/resources/getServerResources";
 import FadeIn from '../Fade'
-import getServerResources from '../../api/server/resources/getServerResources'
-import SideBar from '../Sidebar'
-class DashboardContainer extends Component {
+import Loading from 'react-loading-bar'
+import getAllocation from "../../api/allocations/getAllocation";
+function DashboardContainer() {
+    document.title = `Servers | Ararat`
+    var [isLoading, setLoading] = useState(true)
+    var [servers, setServers] = useState(() => {
+        return(null)
+    })
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            loadingUserData: true,
-            loadingServerData: true,
-            email: null,
-            first_name: null,
-            last_name: null,
-            user_id: null,
-            servers: null,
-            server_resources: null
-        }
-    }
-    async componentDidMount() {
-        document.title = 'Dashboard'
-        this.setState({ show: true })
-        var data = jwt.decode(Cookies.get('token'))
-        this.setState({
-            email: data.email,
-            first_name: data.first_name,
-            last_name: data.last_name,
-            user_id: data.id,
-            loadingUserData: false
+
+    useEffect(() => {
+        setLoading(true)
+        getServers(function(response){
+            setServers(response)
+            setLoading(false)
         })
-        var servers = await axios.get(`https://ararat-backend.hyehosting.com/servers?token=${Cookies.get('token')}`)
-        console.log(servers.data.data)
-        if (servers.data.data) {
-            var server_list = servers.data.data
-        } else {
-            var server_list = servers.data
-        }
-        console.log(server_list)
-        this.setState({
-            servers: server_list,
-            loadingServerData: false
-        })
-        if (server_list.length == 0) {
 
-        } else {
-                var server_resource_data = []
-                for (var server_item of server_list) {
-                    var server_resources = await axios.get(`https://ararat-backend.hyehosting.com/servers/${server_item.attributes.identifier}/resources?token=${Cookies.get('token')}`)
-                    server_resource_data.push({
-                        data: server_resources.data,
-                        uuid: server_item.attributes.identifier
-                    })
-                    this.setState({ show: true })
-
-                }
-                this.setState({
-                    server_resources: server_resource_data
-                })
-
-        }
-        this.setState({ show: false })
-
-    }
-    render() {
-        return (
-            <>
-
-            <div className="sticky-top" style={{
-                float: 'left',
-                marginRight: '2%',
-                marginLeft: '0px',
-                height: '100%',
-            }}>
-<SideBar /> 
-</div>
-                <Loading
-                    show={this.state.show}
+    }, [])
+    useEffect(() => {
+        setInterval(function(){
+            getServers(function(response){
+                setServers(response)
+            })
+        }, 5000)
+    }, [])
+  
+    return (
+        <>
+                                                <Loading
+                    show={isLoading}
                     color="red"
                     showSpinner={false}
                 />
-                <div class="container" >
-                
-                    <FadeIn>
-                        <h1 style={{ fontWeight: 'bold' }}>Your Servers</h1>
-                    </FadeIn>
-                    {this.state.loadingServerData ?
-                        <div className="d-flex justify-content-evenly">
-                            <ReactLoading color="#EC635D" type='spin' />
-                        </div>
-                        :
-                        <>
-                            {this.state.servers.length == 0 ?
-                                <div>
-                                    <h3 className="text-center mt-5">Looks Like You Don't Have Any Servers!</h3>
-                                    <div className="col mt-3 text-center">
-                                        <Link className="mx-auto" to="/order"><button className="btn btn-blue-lg">Order Now</button></Link>
-                                    </div>
-                                </div> :
-                                <div className="row">
-                                    {this.state.servers.map(server => (
-                                        <ServerRow
-                                            key={server.attributes.id}
-                                            name={server.attributes.name}
-                                            description={server.attributes.description}
-                                            current_memory={this.state.server_resources instanceof Array ?
-                                                prettyBytes(this.state.server_resources.filter(resources => resources.uuid == server.attributes.identifier)[0].data.attributes.resources.memory_bytes, { binary: true })
-                                                :
-                                                <ReactLoading color="#EC635D" type='spin' height={20} width={20} />}
-                                            memory={prettyBytes(server.attributes.limits.memory * 1048576, { binary: true })}
-                                            current_disk={this.state.server_resources instanceof Array ?
-                                                prettyBytes(this.state.server_resources.filter(resources => resources.uuid == server.attributes.identifier)[0].data.attributes.resources.disk_bytes, { binary: true })
-                                                :
-                                                <ReactLoading color="#EC635D" type='spin' height={20} width={20} />}
-                                            current_state={this.state.server_resources instanceof Array ?
-                                                this.state.server_resources.filter(resources => resources.uuid == server.attributes.identifier)[0].data.attributes.current_state
-                                                :
-                                                "loading"}
-                                            disk={prettyBytes(server.attributes.limits.disk * 1048576, { binary: true })}
-                                            id={server.attributes.identifier} />
-                                    ))}
-                                </div>
-                            }
-                        </>
-                    }
-                </div>
-            </>
-        )
-    }
-}
+            <div>
+                <base href="../../../" />
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+                <meta name="description" content="The Best Server Control Panel In The Industry" />
+                {/* Fav Icon  */}
+                <link rel="shortcut icon" href="./images/favicon.png" />
+                {/* Page Title  */}
+                <title>Ararat | Dashboard</title>
+                {/* StyleSheets  */}
+                <link rel="stylesheet" href="./assets/css/dashlite.css?ver=2.6.0" />
+                <link id="skin-default" rel="stylesheet" href="./assets/css/theme.css?ver=2.6.0" />
+                <div className="nk-app-root">
+                    {/* main @s */}
+                    <div className="nk-main ">
+                        {/* sidebar @s */}
+                        <Navigation  page="servers"/>
+                        {/* sidebar @e */}
+                        {/* wrap @s */}
+                        <div className="nk-wrap ">
+                            {/* main header @s */}
+                            <Header />
+                            {/* main header @e */}
+                            {/* content @s */}
+                            <div className="nk-content ">
+                                <div className="container-fluid">
+                                    <div className="nk-content-inner">
+                                        <div className="nk-content-body">
+                                            <div className="components-preview wide-md mx-auto">
+                                                <div className="nk-block-head nk-block-head-lg wide-sm">
+                                                    <div className="nk-block-head-content">
+                                                        <h2 className="nk-block-title fw-normal">Your Servers</h2>
+                                                    </div>
+                                                </div>{/* nk-block-head */}
 
+                                                <div className="nk-block nk-block-lg">
+
+                                                    <div className="card card-preview">
+                                                        <table className="table table-orders">
+                                                            <thead className="tb-odr-head">
+                                                                <tr className="tb-odr-item">
+                                                                    <th>
+                                                                        <span>Name</span>
+                                                                    </th>
+                                                                    <th>
+                                                                        <span>IP</span>
+                                                                    </th>
+                                                                    <th>
+                                                                        <span>Memory</span>
+                                                                    </th>
+                                                                    <th>
+                                                                        <span>CPU</span>
+                                                                    </th>
+                                                                    <th>
+                                                                        <span>Storage</span>
+                                                                    </th>
+                                                                    <th>
+                                                                        <span>Status</span>
+                                                                    </th>
+                                                                    <th>
+                                                                        <span>Action</span>
+                                                                    </th>
+
+                                                                </tr>
+                                                            </thead>
+                                                            <tbody className="tb-odr-body">
+                                                                {servers == null ? <p /> :
+                                                                <>
+                                                                {servers.map(server => (
+                                                                    <ServerRow
+                                                                     name={server.attributes.name}
+                                                                     identifier={server.attributes.identifier}
+                                                                     max_memory={server.attributes.limits.memory} 
+                                                                     max_disk={server.attributes.limits.disk}
+                                                                     suspended={server.attributes.suspended}
+                                                                     allocation={server.attributes.allocation}
+                                                                     status={server.attributes.status}
+                                                                    //  status={server.resources.attributes.current_state}
+                                                                    />
+                                                                ))}   
+                                                                </>                 
+                                                                }
+
+
+                                                            </tbody>
+                                                        </table>
+                                                    </div>{/* .card */}
+                                                </div>{/* nk-block */}
+
+                                            </div>{/* .components-preview */}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* content @e */}
+                            {/* footer @s */}
+                            <div className="nk-footer">
+                                <div className="container-fluid">
+                                    <div className="nk-footer-wrap">
+                                        <div className="nk-footer-copyright"> ©2021 Hye Hosting LLC. All Rights Reserved.
+                                        </div>
+                                        <div className="nk-footer-links">
+                                            <ul className="nav nav-sm">
+                                                <li className="nav-item"><a className="nav-link" href="https://hyehosting.com/tos">Terms of Service</a></li>
+                                                <li className="nav-item"><a className="nav-link" href="https://hyehosting.com/privacy">Privacy Policy</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* footer @e */}
+                        </div>
+                        {/* wrap @e */}
+                    </div>
+                    {/* main @e */}
+                </div>
+                {/* app-root @e */}
+                {/* JavaScript */}
+            </div>
+        </>
+    )
+}
 export default DashboardContainer
--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -14,9 +14,6 @@ import NotFound from '../components/NotFound'
 import Cookies from 'js-cookie';
 import SideBar from '../components/Sidebar'
 import Testing from '../components/dashboard/Testing'
-
-
-import DashboardNewContainer from '../components/dashboard/DashboardNewContainer'
 import ServerNewContainer from "../components/server/ServerNewContainer";
 function AppRouter() {
 
@@ -30,10 +27,7 @@ function AppRouter() {
     <Router>
       <Switch>
         <Route exact path="/">
-          {checkAuth() ? <DashboardNewContainer /> : <Redirect to="/login" />}
-        </Route>
-        <Route exact path="/new">
-          {checkAuth() ? <DashboardNewContainer /> : <Redirect to="/login" />}
+          {checkAuth() ? <DashboardContainer /> : <Redirect to="/login" />}
         </Route>
         <Route exact path = "/server/:uuid">
           {checkAuth() ? <ServerContainer /> : <Redirect to="/login" />}
