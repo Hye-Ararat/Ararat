6da1c7611371e0040d3ed8bce2a2555cabf3c4d9
Joseph Maldjian <josephavm@icloud.com>
2021-10-18 15:27:18 -0500

    Add login function

---
+++ src/api/v1/auth/login.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+function login(username, password) {
+  return new Promise((resolve, reject) => {
+    const auth_response = axios.post(`https://basalt.hye.gg:2221/auth/login`, {
+      username: username,
+      password: password,
+    }).data;
+    if (auth_response.status == "error") reject(Error(auth_response.data));
+    var FifteenMinutes = new Date(new Date().getTime() + 15 * 60 * 1000);
+    try {
+      Cookies.set("access_token", auth_response.access_token, {
+        sameSite: "strict",
+        secure: true,
+        expires: FifteenMinutes,
+      });
+    } catch (error) {
+      reject(Error("An error occured while setting the access token cookie"));
+    }
+    try {
+      Cookies.set("refresh_token", auth_response.refresh_token, {
+        sameSite: "strict",
+        secure: true,
+        expires: FifteenMinutes,
+      });
+    } catch (error) {
+      Cookies.remove("access_token");
+      reject(Error("An error occured while setting the refresh token cookie"));
+    }
+    resolve("Success");
+  });
+}
+export default login;
