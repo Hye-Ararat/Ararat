470fb96b7a609f1bad9bb5ccf1aef6e2fb691431
Joseph Maldjian <josephavm@icloud.com>
2021-10-18 16:46:13 -0500

    Make login.js use configured basalt server

--- src/api/v1/auth/login.js
+++ src/api/v1/auth/login.js
@@ -2,32 +2,43 @@ import axios from "axios";
 import Cookies from "js-cookie";
 function login(email, password) {
   return new Promise((resolve, reject) => {
-    const auth_response = axios.post(`https://basalt.hye.gg:2221/auth/login`, {
-      username: email,
-      password: password,
-    }).data;
-    if (auth_response.status == "error") reject(Error(auth_response.data));
-    var FifteenMinutes = new Date(new Date().getTime() + 15 * 60 * 1000);
-    try {
-      Cookies.set("access_token", auth_response.access_token, {
-        sameSite: "strict",
-        secure: true,
-        expires: FifteenMinutes,
+    axios
+      .post(`${process.env.REACT_APP_BASALT_URL}/api/v1/client/auth/login`, {
+        email: email,
+        password: password,
+      })
+      .then((auth_response) => {
+        let auth_data = auth_response.data;
+        if (auth_data.status == "error") return reject(Error(auth_data.data));
+        var FifteenMinutes = new Date(new Date().getTime() + 15 * 60 * 1000);
+        try {
+          Cookies.set("access_token", auth_data.data.access_token, {
+            sameSite: "strict",
+            secure: true,
+            expires: FifteenMinutes,
+          });
+        } catch (error) {
+          return reject(
+            Error("An error occured while setting the access token cookie")
+          );
+        }
+        try {
+          Cookies.set("refresh_token", auth_data.data.refresh_token, {
+            sameSite: "strict",
+            secure: true,
+            expires: FifteenMinutes,
+          });
+        } catch (error) {
+          Cookies.remove("access_token");
+          return reject(
+            Error("An error occured while setting the refresh token cookie")
+          );
+        }
+        return resolve("Success");
+      })
+      .catch(() => {
+        return reject(Error("An error occured while sending the request"));
       });
-    } catch (error) {
-      reject(Error("An error occured while setting the access token cookie"));
-    }
-    try {
-      Cookies.set("refresh_token", auth_response.refresh_token, {
-        sameSite: "strict",
-        secure: true,
-        expires: FifteenMinutes,
-      });
-    } catch (error) {
-      Cookies.remove("access_token");
-      reject(Error("An error occured while setting the refresh token cookie"));
-    }
-    resolve("Success");
   });
 }
 export default login;
