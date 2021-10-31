dc680860b1ab1c70102f85df8358b52c97d83638
Joseph Maldjian <josephavm@icloud.com>
2021-10-31 12:32:45 -0500

    Update _middleware.js

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,12 +1,14 @@
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
+    console.log(req.url)
     if (req.url.includes("/api/v1")) return NextResponse.next();
     if (req.cookies.access_token && req.url == "/auth/login") {
         return NextResponse.redirect("/");
     }
     if (req.cookies.access_token || req.url == "/auth/login") {
         return NextResponse.next();
+    } else {
+        return NextResponse.redirect("/auth/login");
     }
-    return NextResponse.redirect("/auth/login", 307);
 }  
