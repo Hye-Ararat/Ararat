545d9d08ca59d6475ddcedced425191b334669de
Joseph Maldjian <josephavm@icloud.com>
2021-10-31 12:37:07 -0500

    Fix domains

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,14 +1,14 @@
 import { NextResponse } from "next/server";
-
+// ik how let me how can i get the full url
 export async function middleware(req) {
-    console.log(req.url)
-    if (req.url.includes("/api/v1")) return NextResponse.next();
-    if (req.cookies.access_token && req.url == "/auth/login") {
+    var url = new URL(req.url)
+    var path = url.pathname
+    if (path.includes("/api/v1")) return NextResponse.next();
+    if (req.cookies.access_token && path == "/auth/login") {
         return NextResponse.redirect("/");
     }
-    if (req.cookies.access_token || req.url == "/auth/login") {
+    if (req.cookies.access_token || path == "/auth/login") {
         return NextResponse.next();
-    } else {
-        return NextResponse.redirect("/auth/login");
     }
+    return NextResponse.redirect("/auth/login", 307);
 }  
