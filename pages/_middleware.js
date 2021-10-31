e96ef5cadf30ad1c869cf4468243928aeb45c59c
Joseph Maldjian <josephavm@icloud.com>
2021-10-31 12:56:03 -0500

    Improve Auth Middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,8 +1,17 @@
 import { NextResponse } from "next/server";
 // ik how let me how can i get the full url
 export async function middleware(req) {
-    var url = new URL(req.url)
-    var path = url.pathname
+    console.log(req.url)
+    try {
+        var u = req.url
+        if (!u.startsWith('http')) u = 'http://localhost' + u
+        var url = new URL(u)
+        var path = url.pathname
+    } catch (error) {
+       
+      console.log(error)
+    }
+    
     if (path.includes("/api/v1")) return NextResponse.next();
     if (req.cookies.access_token && path == "/auth/login") {
         return NextResponse.redirect("/");
