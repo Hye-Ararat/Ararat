bac4492087487c894fab617470ccd6af1a9df226
Joseph Maldjian <josephavm@icloud.com>
2021-10-30 23:32:03 -0500

    Add Allocation Fetching Route

---
+++ pages/api/v1/client/allocations/[id].js
@@ -0,0 +1,16 @@
+const {connectToDatabase} = require('../../../../../util/mongodb');
+const {ObjectId} = require('mongodb');
+
+export default async function handler(req, res) {
+    const { method, query : {id} } = req;
+    switch (method) {
+        case 'GET': {
+            if (typeof (id) != "string" || Buffer.byteLength(id, "utf8") < 12 ) return res.json({status: "error", data: "Invalid Request"})
+             var {db} = await connectToDatabase();
+             var allocation = await db.collection("allocations").findOne({_id: ObjectId(id)});
+             allocation ? res.json({status: "success", data: allocation}) : res.json({status: "error", data: "Invalid Request"})
+        } case 'PUT': {
+
+        }
+    }
+}
