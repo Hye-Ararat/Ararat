e4fba3cee0ff6e806d51831ab6213163d430abf7
Joseph Maldjian <josephavm@icloud.com>
2021-10-30 23:30:54 -0500

    Add SWR and Progress

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,33 @@ import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/system";
 import theme from "../components/theme";
 import "../styles/globals.css";
+import "nprogress/nprogress.css";
+import "../public/BackdropFilter.css"
+import Router from "next/router";
+import NProgress from "nprogress";
+import axios from "axios";
+import { SWRConfig } from "swr";
+
+NProgress.configure({ showSpinner: false });
+Router.onRouteChangeStart = (url) => {
+	NProgress.start();
+};
+Router.onRouteChangeComplete = () => NProgress.done();
+Router.onRouteChangeError = () => NProgress.done();
+
+axios.interceptors.request.use((config) => {
+  config.headers.authorization = `Bearer EEEE`;
+  return config;
+})
 
 function MyApp({ Component, pageProps }) {
 	return (
+    <SWRConfig value={{ provider: () => new Map() }}>
 		<ThemeProvider theme={theme}>
 			<CssBaseline />
 			<Component {...pageProps} />
 		</ThemeProvider>
+    </SWRConfig>
 	);
 }
 
