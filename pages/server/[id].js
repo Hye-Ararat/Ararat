315765285c0b1da7fb7dc1dd97d89a40417dc3aa
Joseph Maldjian <josephavm@icloud.com>
2021-11-02 14:04:54 -0500

    Fetch Server Data On Server Page

--- pages/server/[id].js
+++ pages/server/[id].js
@@ -1,13 +1,28 @@
 import { useRouter } from "next/router";
 import Navigation from "../../components/server/Navigation";
+import { connectToDatabase } from "../../util/mongodb";
+import { ObjectId } from "mongodb";
+import { Typography } from "@mui/material";
 
-export default function Server({data}) {
-    const router = useRouter()
-    const {id} = router.query
-    return(
-        <Navigation>
-        <p>Hi</p>
-        {id}
-        </Navigation>
-    )
+export async function getServerSideProps(ctx) {
+	const { db } = await connectToDatabase();
+	const server_data = await db.collection("servers").findOne({
+		[`users.616da13fe2f36f19e274a7ca`]: { $exists: true },
+		_id: ObjectId(ctx.query.id),
+	});
+	let data = JSON.parse(JSON.stringify(server_data));
+	return { props: { data } };
+}
+
+export default function Server({ data }) {
+	const router = useRouter();
+	const { id } = router.query;
+	return (
+		<Navigation server={id}>
+          <Typography variant="h4" sx={{ mb: 1 }}>
+            {data.name}
+          </Typography>
+          			{id}
+		</Navigation>
+	);
 }
