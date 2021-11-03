7c5de36533a2084320c5235fd9dba13bdaff57ac
Joseph Maldjian <josephavm@icloud.com>
2021-11-03 13:36:47 -0500

    Format to reduce bundle size

--- pages/api/v1/client/_middleware.js
+++ pages/api/v1/client/_middleware.js
@@ -1,15 +1,17 @@
-import jwt from "jsonwebtoken"
-import { NextResponse, NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
+import { NextResponse } from "next/server";
 export async function middleware(req, res) {
 	if (req.url.includes("/api/v1/client/auth/login")) return NextResponse.next();
-    if (req.headers.get("authorization")) {
-        try {
-            jwt.verify(req.headers.get("authorization").split(" ")[1], process.env.ACCESS_TOKEN_SECRET)
-        }
-        catch (error) {
-            return NextResponse.redirect("/api/v1/unauthorized");
-        }
-        return NextResponse.next();
-    } 
-    return NextResponse.redirect("/api/v1/unauthorized");
+	if (req.headers.get("authorization")) {
+		try {
+			jwt.verify(
+				req.headers.get("authorization").split(" ")[1],
+				process.env.ACCESS_TOKEN_SECRET
+			);
+		} catch (error) {
+			return NextResponse.redirect("/api/v1/unauthorized");
+		}
+		return NextResponse.next();
+	}
+	return NextResponse.redirect("/api/v1/unauthorized");
 }
