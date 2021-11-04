c7e5874f17dfa4aca00fea963c0f559c31343fae
Joseph Maldjian <josephavm@icloud.com>
2021-11-04 13:47:37 -0500

    Start Adding API Key/Node Key Support to Authentication

--- pages/api/v1/client/_middleware.js
+++ pages/api/v1/client/_middleware.js
@@ -3,11 +3,18 @@ import { NextResponse } from "next/server";
 export async function middleware(req) {
 	if (req.url.includes("/api/v1/client/auth/login")) return NextResponse.next();
 	if (req.headers.get("authorization")) {
+		if (req.headers.get("authorization").split(" ")[1].includes(":")) {
+			//Is API Key
+		} else if (req.headers.get("authorization").split(" ")[1].includes("::") ) {
+			//Is Node API Key
+			
+		} else {
 		try {
 			jwt.verify(req.headers.get("authorization").split(" ")[1], process.env.ACCESS_TOKEN_SECRET);
 		} catch (error) {
 			return NextResponse.redirect("/api/v1/unauthorized");
 		}
+	}
 		return NextResponse.next();
 	}
 	return NextResponse.redirect("/api/v1/unauthorized");
