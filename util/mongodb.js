688d37f651bd068bdf514bfd01625cdcbe087638
Joseph Maldjian <josephavm@icloud.com>
2021-11-05 07:49:17 -0500

    Update JSDoc

--- util/mongodb.js
+++ util/mongodb.js
@@ -1,40 +1,40 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient } from "mongodb";
 
-let uri = process.env.MONGODB_URI
-let dbName = process.env.MONGODB_DB
+let uri = process.env.MONGODB_URI;
+let dbName = process.env.MONGODB_DB;
 
-let cachedClient = null
-let cachedDb = null
+let cachedClient = null;
+let cachedDb = null;
 
 if (!uri) {
   throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
-  )
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
 }
 
 if (!dbName) {
   throw new Error(
-    'Please define the MONGODB_DB environment variable inside .env.local'
-  )
+    "Please define the MONGODB_DB environment variable inside .env.local"
+  );
 }
 /**
- * 
- * @returns {import ('mongodb').Db}
+ *
+ * @returns {import {MongoClient} from 'mongodb'.db}
  */
 export async function connectToDatabase() {
   if (cachedClient && cachedDb) {
-    return { client: cachedClient, db: cachedDb }
+    return { client: cachedClient, db: cachedDb };
   }
 
   const client = await MongoClient.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
+  });
 
-  const db = await client.db(dbName)
+  const db = await client.db(dbName);
 
-  cachedClient = client
-  cachedDb = db
+  cachedClient = client;
+  cachedDb = db;
 
-  return { client, db }
+  return { client, db };
 }
