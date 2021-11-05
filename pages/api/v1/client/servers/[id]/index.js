93757f850d9ccc678c26cbe0aad9c98df0844c13
Joseph Maldjian <josephavm@icloud.com>
2021-11-05 13:47:15 -0500

    Organize Server API File Structure

--- pages/api/v1/client/servers/[id]/index.js
+++ pages/api/v1/client/servers/[id]/index.js
@@ -1,5 +1,5 @@
 import { ObjectId } from "mongodb";
-import { connectToDatabase } from "../../../../../util/mongodb";
+import { connectToDatabase } from "../../../../../../util/mongodb";
 
 export default async function handler(req, res) {
     const {method, query: {id}} = req;
