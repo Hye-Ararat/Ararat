dde4b7c7e89dd6fc5bf887cd0c142aa0c890cb9f
Joseph Maldjian <josephavm@icloud.com>
2021-11-07 12:32:31 -0600

    Fix Verification

--- pages/api/v1/client/servers/[id]/monitor/verify.js
+++ pages/api/v1/client/servers/[id]/monitor/verify.js
@@ -14,21 +14,28 @@ export default async function handler(req, res) {
         return res
           .status(400)
           .send({ status: "error", data: "Access Token is required" });
-      if (!req.body.access_token.contains(":::"))
+      console.log("passed 1");
+      if (!req.body.access_token.includes(":::"))
         return res.status(403).send({ status: "error", data: "Unauthorized" });
+      console.log("passed 2");
+      console.log(req.body.access_token.split(":::")[1]);
       try {
         var token_data = await verify(
-          req.body.access_token.substr(1, indexOf(":::")),
+          req.body.access_token.split(":::")[1],
           process.env.ACCESS_TOKEN_SECRET
         );
-      } catch {
+      } catch (error) {
+        console.log(error);
+        console.log("error 1");
         return res.status(403).send({ status: "error", data: "Unauthorized" });
       }
       if (token_data.server_id != id)
         return res.status(403).send({ status: "error", data: "Unauthorized" });
+      console.log("passed 3");
       if (token_data.type != "monitor_access_token")
         return res.status(403).send({ status: "error", data: "Unauthorized" });
-      var sessions = db
+      console.log("passed 4");
+      var sessions = await db
         .collection("sessions")
         .find({
           server_id: id,
@@ -36,19 +43,28 @@ export default async function handler(req, res) {
         })
         .toArray();
       let match = false;
-      sessions.every(async (session) => {
-        if (!match) {
-          let is_match = await bcrypt.compare(
-            req.body.access_token,
-            session.access_token
-          );
-          if (is_match) {
-            match = true;
-          }
-        }
-      });
+      console.log(sessions);
+      async function run() {
+        return new Promise((resolve, reject) => {
+          sessions.forEach(async (session) => {
+            if (!match) {
+              let is_match = await bcrypt.compare(
+                req.body.access_token,
+                session.access_token
+              );
+              console.log(is_match);
+              if (is_match) {
+                match = true;
+                return resolve(true);
+              }
+            }
+          });
+        });
+      }
+      await run();
       if (!match)
         return res.status(403).send({ status: "error", data: "Unauthorized" });
+      console.log("passed 5");
       return res.json({ status: "success", data: "Access Token Is Valid" });
       break;
     default:
