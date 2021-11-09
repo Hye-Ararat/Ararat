4a421ad28e587b06a9bf59a9c2dd5980ef104197
Joseph Maldjian <josephavm@icloud.com>
2021-11-09 10:54:23 -0600

    Add Magma Cube Fetching to API

---
+++ pages/api/v1/client/magma_cubes/[id].js
@@ -0,0 +1,33 @@
+import { ObjectId } from "mongodb";
+import { connectToDatabase } from "../../../../../util/mongodb";
+import {decode} from "jsonwebtoken";
+export default async function handler(req, res) {
+    const {query: {id}} = req;
+    let {db} = await connectToDatabase();
+    try {
+    var user_data = await decode(req.headers.authorization.split(" "), process.env.ACCESS_TOKEN_SECRET);
+    } catch {
+        res.status(403).json({status: "error", data: "Unauthorized"});
+    }
+    let server_check = db.collection("servers").findOne({
+        [`users.${user_data.user}`]: {$exists: true},
+        magma_cube: {
+            cube: id
+        }
+    })
+    if (!server_check) return res.status(404).json({
+        status: "error",
+        data: "Magma Cube does not exist"
+    })
+    let magmaCube = await db.collection("magma_cubes").findOne({
+        _id: ObjectId(id)
+    });
+    if (!magmaCube) res.status(404).json({
+        status: "error",
+        data: "Magma Cube does not exist"
+    })
+    return res.json({
+        status: "success",
+        data: magmaCube
+    })
+}
