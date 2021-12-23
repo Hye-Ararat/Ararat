a2701dd67d9a07dacdf09c1b5d5d0e2eafc9f291
Joseph Maldjian <josephavm@icloud.com>
2021-12-23 15:27:41 -0600

    Console Websocket Access Tokens

---
+++ pages/api/v1/admin/servers/[id]/console/verify.js
@@ -0,0 +1,24 @@
+import { decode, verify } from "jsonwebtoken";
+
+export default async function handler(req, res) {
+    const {query: {id}, method} = req;
+    if (method != "POST") {
+        return res.status(400).send("Invalid Method");
+    }
+    if (!req.body.access_token) return res.status(400).send("Access Token is Required");
+    if (!req.body.access_token.includes(":::")) return res.status(403).send("Unauthorized");
+    try {
+        var valid = verify(req.body.access_token.split(":::")[1], process.env.ENC_KEY);
+    } catch (error) {
+        console.log(error);
+        return res.status(403).send("Unauthorized");
+    }
+    if (!valid)  {
+        console.log("not valid")
+        return res.status(403).send("Unauthorized")
+    }
+    var token_data = decode(req.body.access_token.split(":::")[1]);
+    if (token_data.server_id != req.query.id) return res.status(403).send("Unauthorized");
+    if (token_data.type != "console_access_token") return res.status(403).send("Unauthorized");
+    return res.send("Access Token Is Valid!")
+}
---
+++ pages/api/v1/client/servers/[id]/console/ws/index.js
@@ -0,0 +1,33 @@
+import { decode, sign } from "jsonwebtoken";
+import { connectToDatabase } from "../../../../../../../../util/mongodb";
+import { ObjectId } from "mongodb";
+export default async function handler(req, res) {
+    let user_data = await decode(req.headers.authorization.split(" ")[1]);
+    let { db } = await connectToDatabase();
+    var server = db.collection("servers").findOne({
+        _id: ObjectId(req.query.id),
+        [`users.${user_data.id}`]: { $exists: true },
+    })
+    if (server) {
+        var access_token_jwt = sign(
+            {
+                server_id: req.query.id,
+                type: "console_access_token",
+                user: user_data.id
+            },
+            process.env.ENC_KEY,
+            {
+                expiresIn: "15m",
+                algorithm: "HS256",
+            }
+        );
+        var access_token_identifier = access_token_jwt.split(
+            0,
+            access_token_jwt.indexOf(".")
+        )[0];
+        var access_token = access_token_identifier + ":::" + access_token_jwt;
+        return res.send(access_token)
+    } else {
+        return res.status(403).send("You do not have permission to access the console for this server")
+    }
+}
