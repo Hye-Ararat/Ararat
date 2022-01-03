a2486430c2f33d6217692bea9133e001e5f47d64
Joseph Maldjian <josephavm@icloud.com>
2022-01-02 18:15:14 -0600

    Add proper xterm scaling

--- components/instances/terminal.js
+++ components/instances/terminal.js
@@ -1,48 +1,67 @@
-import { useEffect, useLayoutEffect, useState } from "react"
+import { useEffect, memo } from "react"
 import { Terminal } from "xterm"
 import { AttachAddon } from "xterm-addon-attach"
 import {FitAddon} from "xterm-addon-fit"
 import Spice from "./spice";
 import ResizeObserver from "react-resize-observer";
 const fitAddon = new FitAddon();
-export default function TermComponent(props) {
+var term = null;
+function TermComponent(props) {
     useEffect(() => {
         if (props.instance.relationships.magma_cube != null) {
             if (props.instance.relationships.magma_cube.console == "xterm") {
-                const term = new Terminal({
-                    renderType: "dom",
-
-                })
+                term = new Terminal()
                 term.loadAddon(fitAddon);
                 term.open(document.getElementById("terminal"))
-                term.write(".")
-                var interval = setInterval(() => {
-                    term.write(".")
-                }, 4)
                     fitAddon.fit()
-                const socket = new WebSocket(`wss://${props.instance.relationships.node.address.hostname}:${props.instance.relationships.node.address.port}/api/v1/instances/${props.instance.id}/console?type=xterm`)
+                const socket = new WebSocket(`ws://81.205.168.8:3535/api/v1/instances/${props.instance.id}/console`)
                 socket.onopen = () => {
                     socket.send('{"event":"authenticate"}')
-                    clearInterval(interval)
                 }
                 socket.onerror = (error) => {
-
+                    console.log(error)
                 }
                 const attachAddon = new AttachAddon(socket);
                 term.loadAddon(attachAddon);
-
+                var controlws = new WebSocket(`ws://81.205.168.8:3535/api/v1/instances/${props.instance.id}/control`)
+                controlws.onopen = () => {
+                    window.onresize = () => {
+                        var dimensions = fitAddon.proposeDimensions();
+                        if (controlws) {
+                            controlws.send(JSON.stringify({event: "resize", rows: dimensions.rows, cols: dimensions.cols}))
+                        } 
+                    }
+                }
+                return () => {
+                    socket.close()
+                    term.dispose()
+                    controlws.close()
+                }
             }
         }
     }, [props.instance])
+    useEffect(() => {
+        if (term && props.status != null) {
+        term.write("Server " + props.status)
+        }
+    }, [props.status])
     return (
         <>
+        <link rel="stylesheet" type="text/css" href="/xterm.css" />
             {props.instance.relationships.magma_cube != null ?
                 props.instance.relationships.magma_cube.console == "vga" ?
                     <Spice />
                     :
                     <>
                     <ResizeObserver onResize={rect => {
-                            fitAddon.fit()
+                        var yes = document.getElementsByClassName("xterm-viewport");
+                        yes = yes[0];
+                        if (yes) {
+                        yes.style.width = `${rect.width}px`;
+                        yes.style.height = `${rect.height}px`;
+                        }
+                        fitAddon.fit()
+                
                    }} />
                     <div id="terminal" style={{width: "100%"}}></div>
                     </>
@@ -51,3 +70,11 @@ export default function TermComponent(props) {
         </>
     )
 }
+function areEqual(prevProps, nextProps) {
+    console.log("are equal?", JSON.stringify(prevProps) === JSON.stringify(nextProps))
+    if (nextProps.status == undefined || nextProps.status == null) {
+        return true
+    }
+    return JSON.stringify(prevProps) === JSON.stringify(nextProps)
+}
+export default memo(TermComponent, areEqual)
