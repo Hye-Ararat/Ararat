2108f9d74ae21a32372fd527d60b71fba6557fe0
Joseph Maldjian <josephavm@icloud.com>
2022-01-10 18:40:12 -0600

    Add one-off state fetching

--- pages/api/v1/client/instances/[id]/state/index.js
+++ pages/api/v1/client/instances/[id]/state/index.js
@@ -1,10 +1,10 @@
-const {post} = require("axios");
+const {post, get} = require("axios");
 import {connectToDatabase} from "../../../../../../../util/mongodb"
 import { ObjectId } from "mongodb";
 import { decode } from "jsonwebtoken";
 import crypto from "crypto";
 export default async function handler(req, res) {
-    const {query: {id}} = req;
+    const {query: {id}, method} = req;
     var {db} = await connectToDatabase();
     try {
         var user = decode(req.headers.authorization.split(" ")[1])
@@ -37,19 +37,43 @@ export default async function handler(req, res) {
     if (!node) {
         return res.status(500).send("Node associated with server does not exist")
     }
-    try {
-        var decipher = crypto.createDecipheriv("aes-256-ctr", process.env.ENC_KEY, Buffer.from(node.access_token_iv, "hex"))
-        var access_token = Buffer.concat([decipher.update(Buffer.from(node.access_token, "hex")), decipher.final()])
-        var state = await post(`${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances/${id}/state`, req.body, {
-            headers: {
-                Aunthorization: `Bearer ${access_token.toString()}`
+
+    switch (method) {
+        case "GET":
+            try {
+                var decipher = crypto.createDecipheriv("aes-256-ctr", process.env.ENC_KEY, Buffer.from(node.access_token_iv, "hex"))
+                var access_token = Buffer.concat([decipher.update(Buffer.from(node.access_token, "hex")), decipher.final()])
+                var state = await post(`${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances/${id}/state`, req.body, {
+                    headers: {
+                        Aunthorization: `Bearer ${access_token.toString()}`
+                    }
+                })
+            } catch (error){
+                console.log(error)
+                console.log("error 2")
+                return res.status(500).send("An error occured while sending the power action")
             }
-        })
-    } catch (error){
-        console.log(error)
-        console.log("error 2")
-        return res.status(500).send("An error occured while sending the power action")
+            return res.send("Success")
+            break;
+        case "POST":
+            try {
+                var decipher = crypto.createDecipheriv("aes-256-ctr", process.env.ENC_KEY, Buffer.from(node.access_token_iv, "hex"))
+                var access_token = Buffer.concat([decipher.update(Buffer.from(node.access_token, "hex")), decipher.final()])
+                var state = await post(`${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances/${id}/state`, req.body, {
+                    headers: {
+                        Aunthorization: `Bearer ${access_token.toString()}`
+                    }
+                })
+            } catch (error){
+                console.log(error)
+                console.log("error 2")
+                return res.status(500).send("An error occured while fetching the power state")
+            }
+            return res.send(state.data)
+            break;
+        default:
+            return res.status(405).send("Method not allowed")
+            break;
     }
-    return res.send("Success")
 
 }
