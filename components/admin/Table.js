aa9d4732b09d9a9380e7f3dd4c299123bc0987af
Joseph Maldjian <josephavm@icloud.com>
2022-01-10 18:37:56 -0600

    Create a simple table component

---
+++ components/admin/Table.js
@@ -0,0 +1,32 @@
+import {Paper, TableContainer, Table as MuiTable, TableHead, TableCell, TableBody, TableRow} from "@mui/material";
+import { useRouter } from "next/router";
+
+export default function Table({cells, rows, ...props}) {
+    const router = useRouter();
+    return (
+        <Paper sx={{borderRadius: 1.5}}>
+        <TableContainer sx={{borderRadius: 1.5}}>
+            <MuiTable>
+                <TableHead>
+                    {cells.map(cell => (
+                        <TableCell>{cell}</TableCell>
+                    ))}
+                </TableHead>
+                <TableBody >
+                    {rows.map(row => (
+                        <TableRow  onClick={() => {
+                            if (row.link) {
+                                router.push(row.link);
+                            }
+                        }} style={{cursor: row.link ? "pointer": ""}}>
+                            {row.cells.map(cell => (
+                                <TableCell>{cell}</TableCell>
+                            ))}
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </MuiTable>
+        </TableContainer>
+        </Paper>
+    )
+}
