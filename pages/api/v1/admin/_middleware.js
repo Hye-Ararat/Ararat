025651d5665638d81fce9a4e6a04504a6ef01d82
Joseph Maldjian <josephavm@icloud.com>
2022-01-10 18:38:50 -0600

    Add authorization to admin endpoints

--- pages/api/v1/admin/_middleware.js
+++ pages/api/v1/admin/_middleware.js
@@ -1,6 +1,35 @@
 import { NextResponse } from "next/server";
+import jwt from "@tsndr/cloudflare-worker-jwt";
 export async function middleware(req, res) {
-	if (req.url.includes("/api/v1/client/auth/login")) return NextResponse.next();
-    if (req.headers.get("authorization")) return NextResponse.next();
-    return NextResponse.redirect("/api/v1/unauthorized");
+    if (req.headers.get("authorization")) {
+        if (req.headers.get("authorization").split(" ")[1].includes("::")) {
+            //is node key
+            console.log(req.headers.get("authorization").split(" ")[1].split("::")[1])
+            try {
+                var allowed = await jwt.verify(req.headers.get("authorization").split(" ")[1].split("::")[1], process.env.ENC_KEY);
+            } catch (error){
+                console.log(error)
+                return NextResponse.redirect("/api/v1/unauthorized");
+            }
+            console.log(allowed)
+            if (!allowed) return NextResponse.redirect("/api/v1/unauthorized");
+        } else if (req.headers.get("authorization").split(" ")[1].includes(":")) {
+            //is api key
+            try {
+                var allowed = await jwt.verify(req.headers.get("authorization").split(" ")[1].split(":")[1], process.env.ENC_KEY);
+            }
+            catch {
+                return NextResponse.redirect("/api/v1/unauthorized");
+            }
+            if (!allowed) return NextResponse.redirect("/api/v1/unauthorized");
+        } else {
+            try {
+                var user_data = await jwt.verify(req.headers.get("authorization").split(" ")[1], process.env.ENC_KEY);
+            } catch (error) {
+                return NextResponse.redirect("/api/v1/unauthorized");
+            }
+            if (!user_data) return NextResponse.redirect("/api/v1/unauthorized");
+        }
+        return NextResponse.next();
+    }
 }
