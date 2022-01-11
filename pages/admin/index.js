2dce4d2888c8178c4d471cfde6bb72e5b245dfe3
Joseph Maldjian <josephavm@icloud.com>
2022-01-10 18:37:20 -0600

    Admin Overview Page

---
+++ pages/admin/index.js
@@ -0,0 +1,17 @@
+import Navigation from "../../components/admin/Navigation"
+
+export default function Admin() {
+    return (
+        <>
+            <p>Admin</p>
+        </>
+    )
+}
+
+Admin.getLayout = function getLayout(page) {
+    return (
+        <Navigation>
+            {page}
+        </Navigation>
+    )
+}
