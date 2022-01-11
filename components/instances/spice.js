95fd1f99f0b154a39ee243db5c001bd9a1e7ce25
Joseph Maldjian <josephavm@icloud.com>
2022-01-10 18:37:46 -0600

    Use improved state management

--- components/instances/spice.js
+++ components/instances/spice.js
@@ -1,11 +1,15 @@
-import useSWR from "swr";
-import axios from "axios";
+//import useSWR from "swr";
+//import axios from "axios";
+import { memo } from "react";
 
-export default function Spice(props) {
-	const fetcher = (url) => axios.get(url).then((res) => res.data);
-    const {data, error} = useSWR("/api/v1/client/instances/" + props.instance._id + "/console/ws", fetcher)
+function Spice(props) {
+	//const fetcher = (url) => axios.get(url).then((res) => res.data);
+    //const {data, error} = useSWR("/api/v1/client/instances/" + props.instance._id + "/console/ws", fetcher)
     return (
-        data ? <iframe height="550px" width="100%" frameBorder="0" src={`${props.instance.relationships.node.address.ssl ? "https://" : "http://"}${props.instance.relationships.node.address.hostname}:${props.instance.relationships.node.address.port}/api/v1/instances/61d1f283396fc0779ee1e2f8/console?token=${data}`} /> : ""
-
+        <iframe height="550px" width="100%" frameBorder="0" src={`${props.instance.relationships.node.address.ssl ? "https://" : "http://"}${props.instance.relationships.node.address.hostname}:${props.instance.relationships.node.address.port}/api/v1/instances/61d1f283396fc0779ee1e2f8/console`} />
     )
 }
+function areEqual(prevProps, nextProps) {
+    return JSON.stringify(prevProps) === JSON.stringify(nextProps);
+}
+export default memo(Spice, areEqual)
--- components/instances/terminal.js
+++ components/instances/terminal.js
@@ -1,101 +1,95 @@
 import { useEffect, useState, memo } from "react"
 import { Terminal } from "xterm"
 import { AttachAddon } from "xterm-addon-attach"
-import {FitAddon} from "xterm-addon-fit"
+import { FitAddon } from "xterm-addon-fit"
 import Spice from "./spice";
 import ResizeObserver from "react-resize-observer";
 import axios from "axios"
+import { InstanceStore } from "../../states/instance";
+import { Fade } from "@mui/material"
 const fitAddon = new FitAddon();
 function TermComponent(props) {
     let term;
-    let socket;
-    let controlws;
+    const instance = {
+        sockets: {
+            console: InstanceStore.useStoreState(state => state.sockets.console),
+            setConsole: InstanceStore.useStoreActions(state => state.sockets.setConsole),
+            control: InstanceStore.useStoreState(state => state.sockets.control),
+            setControl: InstanceStore.useStoreActions(state => state.sockets.setControl),
+        }
+    }
     useEffect(() => {
         if (props.instance.relationships.magma_cube != null) {
             if (props.instance.relationships.magma_cube.console == "xterm") {
-                axios.get("/api/v1/client/instances/" + props.instanceId + "/console/ws").then((res) => {
-                    res = res.data;
-                    term = new Terminal()
+                console.log("THIS IS GOING")
+                if (!instance.sockets.console) {
+                    instance.sockets.setConsole(new WebSocket(`${props.instance.relationships.node.address.ssl ? "wss" : "ws"}://${props.instance.relationships.node.address.hostname}:${props.instance.relationships.node.address.port}/api/v1/instances/${props.instance._id}/console`));
+                } else if (!instance.sockets.control) {
+                    instance.sockets.setControl(new WebSocket(`${props.instance.relationships.node.address.ssl ? "wss" : "ws"}://${props.instance.relationships.node.address.hostname}:${props.instance.relationships.node.address.port}/api/v1/instances/${props.instance._id}/control`))
+                } else {
+                    console.log("THIS IS GOING 2")
+                    instance.sockets.console.onopen = () => {
+                        axios.get("/api/v1/client/instances/" + props.instanceId + "/console/ws").then((res) => {
+                            res = res.data;
+                            instance.sockets.console.send(`{"event":"authenticate", "data": ${res}}`)
+                        })
+                        instance.sockets.control.onopen = () => {
+                            axios.get("/api/v1/client/instances/" + props.instanceId + "/console/ws").then((res) => {
+                                res = res.data;
+                                instance.sockets.console.send(`{"event":"authenticate", "data": ${res}}`)
+                                window.onresize = () => {
+                                    var dimensions = fitAddon.proposeDimensions();
+                                    instance.sockets.control.send(JSON.stringify({ event: "resize", data: { cols: dimensions.cols, rows: dimensions.rows } }))
+                                }
+                            })
+                        }
+                    }
+                    term = new Terminal();
+                    console.log("GOING STILL")
                     term.loadAddon(fitAddon);
                     term.open(document.getElementById("terminal"))
-                        fitAddon.fit()
-                    socket = new WebSocket(`${props.instance.relationships.node.address.ssl ? "wss" : "ws"}://${props.instance.relationships.node.address.hostname}:${props.instance.relationships.node.address.port}/api/v1/instances/${props.instance._id}/console`)
-                    socket.onopen = () => {
-                        socket.send(`{"event":"authenticate", "data": ${res}}`)
-                    }
-                    socket.onerror = (error) => {
-                        console.log(error)
-                    }
-                    const attachAddon = new AttachAddon(socket);
+                    fitAddon.fit()
+                    const attachAddon = new AttachAddon(instance.sockets.console);
                     term.loadAddon(attachAddon);
-                    controlws = new WebSocket(`${props.instance.relationships.node.address.ssl ? "wss" : "ws"}://${props.instance.relationships.node.address.hostname}:${props.instance.relationships.node.address.port}/api/v1/instances/${props.instance._id}/control`)
-                    controlws.onopen = () => {
-                        window.onresize = () => {
-                            var dimensions = fitAddon.proposeDimensions();
-                            if (controlws) {
-                                controlws.send(JSON.stringify({event: "resize", rows: dimensions.rows, cols: dimensions.cols}))
-                            } 
-                            fitAddon.fit()
-                        }
+                    instance.sockets.console.onclose = () => {
+                        instance.sockets.setConsole(null);
                     }
-                    var terms = document.getElementsByClassName("xterm-viewport");
-                    console.log(terms.length)
-                    setTimeout(() => {
-                        if (terms.length > 1) {
-                            window.location.reload()
-                        }
-                    }, 500)
-                })
-
+                }
+            }
+        } else {
+            console.log("no xterm a")
         }
-    } else {
-        console.log("no xterm a")
-    }
-    return () => {
-        console.log("closing EVERYTHING")
-        if (socket) {
-            console.log("closing socket")
-        socket.close()
+        return () => {
+            if (term) {
+                console.log("closing terminal")
+                term.dispose()
+            }
+            window.onresize = null
         }
-        if (term) {
-            console.log("closing terminal")
-            term.dispose()
-        }   
-        if (controlws) {
-            console.log("closing controlws")
-
-            controlws.close()
-        }
-        console.log("removing window.onresize")
-        window.onresize = null
-        console.log("removing terminal")
-    }
-    }, [props.instance])
-    useEffect(() => {
-        if (term && props.status != null) {
-        term.write("Server " + props.status)
-        }
-    }, [props.status])
+    }, [props.instance, instance.sockets.console, instance.sockets.control])
     return (
         <>
-        <link rel="stylesheet" type="text/css" href="/xterm.css" />
+            <link rel="stylesheet" type="text/css" href="/xterm.css" />
             {props.instance.relationships.magma_cube != null ?
                 props.instance.relationships.magma_cube.console == "vga" ?
-                    <Spice instance={props.instance}/>
+                    <Spice instance={props.instance} />
                     :
                     <>
-                    <ResizeObserver onResize={rect => {
-                        if (props.instance.relationships.magma_cube.console == "xterm") {
-                            var yes = document.getElementsByClassName("xterm-viewport");
-                            yes = yes[0];
-                            if (yes) {
-                            yes.style.width = `${rect.width}px`;
-                            yes.style.height = `${rect.height}px`;
+                        <ResizeObserver onResize={rect => {
+                            if (props.instance.relationships.magma_cube.console == "xterm") {
+                                var yes = document.getElementsByClassName("xterm-viewport");
+                                yes = yes[0];
+                                if (yes) {
+                                    yes.style.width = `${rect.width}px`;
+                                    yes.style.height = `${rect.height}px`;
+                                }
+                                fitAddon.fit()
                             }
-                        }
-                
-                   }} />
-                    <div id="terminal" className="thisIsLeTerminal" style={{width: "100%"}}></div>
+
+                        }} />
+                        <Fade in={instance.sockets.console}>
+                            <div id="terminal" className="thisIsLeTerminal" style={{ width: "100%" }}></div>
+                        </Fade>
                     </>
                 :
                 ""}
