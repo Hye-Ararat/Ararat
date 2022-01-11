64d6b1e24db38d6d0746b80805b006ae001bde2c
Joseph Maldjian <josephavm@icloud.com>
2022-01-10 18:39:10 -0600

    Remove uneccessary endpoints

--- pages/api/v1/admin/network/containers/[id].js
+++
@@ -1,9 +0,0 @@
-const {connectToDatabase} = require("../../../../../../util/mongodb");
-const {ObjectId} = require("mongodb");
-
-export default async function handler(req, res) {
-    const {method, query: {id}} = req;
-    const {db} = await connectToDatabase();
-    const network_container = await db.collection("network_containers").findOne({_id: ObjectId(id)})
-    network_container ? res.send(network_container) : res.status(404).send("Network Container does not exist")
-}
--- pages/api/v1/admin/network/forwards/[id].js
+++
@@ -1,9 +0,0 @@
-const {connectToDatabase} = require("../../../../../../util/mongodb");
-const {ObjectId} = require("mongodb");
-
-export default async function handler(req, res) {
-    const {method, query: {id}} = req;
-    const {db} = await connectToDatabase();
-    const network_forward = await db.collection("network_forwards").findOne({_id: ObjectId(id)})
-    network_forward ? res.send(network_forward) : res.status(404).send("Network Forward does not exist")
-}
