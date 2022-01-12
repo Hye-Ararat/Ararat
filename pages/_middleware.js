70cf4e20501ae7c1367696e5454bfe32fe3d4c0a
Joseph Maldjian <josephavm@icloud.com>
2022-01-12 16:03:39 -0600

    Update _middleware.js

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import jwt from "@tsndr/cloudflare-worker-jwt"
-
+import URL from "url";
 export async function middleware(req) {
     try {
         var u = req.url
