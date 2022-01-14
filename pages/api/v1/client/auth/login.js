3776848f61f94c6d9055fea2dac4d41f6c6d5514
Meow <erty2pop@gmail.com>
2022-01-14 09:30:10 -0500

    Add prettierrc and login error display. (#38)
    * add prettierrc
    
    * add login errors
    
    * Restore Theme
    
    Co-authored-by: Meow VPS <undefined@null.com>
    Co-authored-by: Joseph Maldjian <josephavm@icloud.com>

--- pages/api/v1/client/auth/login.js
+++ pages/api/v1/client/auth/login.js
@@ -10,22 +10,20 @@ export default async function handler(req, res) {
     case "POST": {
       var { db } = await connectToDatabase();
       let user_data = await db.collection("users").findOne({
-        email: req.body.email,
+        email: req.body.email
       });
-      if (user_data == null)
-        return res.status(401).json({ status: "error", data: "Incorrect email/password" });
+      if (user_data == null) return res.status(401).json({ status: "error", data: "Incorrect email or password." });
       const match = await bcrypt.compare(req.body.password, user_data.password);
-      if (!match)
-        return res.status(401).json({ status: "error", data: "Incorrect email/password" });
+      if (!match) return res.status(401).json({ status: "error", data: "Incorrect email or password." });
       let refresh_token = jwt.sign(
         {
           type: "refresh",
-          user: ObjectId(user_data._id).toString(),
+          user: ObjectId(user_data._id).toString()
         },
         process.env.ENC_KEY,
         {
           algorithm: "HS256",
-          expiresIn: "7d",
+          expiresIn: "7d"
         }
       );
       const user = {
@@ -36,25 +34,25 @@ export default async function handler(req, res) {
         admin: user_data.admin,
         email: user_data.email,
         preferences: user_data.preferences,
-        phone_number: user_data.phone_number,
+        phone_number: user_data.phone_number
       };
       let access_token = jwt.sign(user, process.env.ENC_KEY, {
         algorithm: "HS256",
-        expiresIn: "15m",
+        expiresIn: "15m"
       });
       res.json({
         status: "success",
         data: {
           refresh_token: refresh_token,
-          access_token: access_token,
-        },
+          access_token: access_token
+        }
       });
       break;
     }
     default: {
       res.status(400).send({
         status: "error",
-        data: "Method not allowed",
+        data: "Method not allowed."
       });
     }
   }
--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,13 +1,6 @@
-import {
-	Container,
-	FormControl,
-	Grid,
-	TextField,
-	Typography,
-	Button,
-} from "@mui/material";
+import { Container, FormControl, Grid, TextField, Typography, Button, Alert } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
-import {Check} from "@mui/icons-material"
+import { Check } from "@mui/icons-material";
 import logo from "../../public/logo.png";
 import Image from "next/image";
 import { Box } from "@mui/system";
@@ -19,78 +12,70 @@ import { useRouter } from "next/router";
 import nprogress, { render } from "nprogress";
 
 export default function Login() {
-	const router = useRouter();
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-    const [loggingIn, setLoggingIn] = useState(false);
-	const loginUser = async () => {
-		try {
-			setLoggingIn(true);
-			await login(email, password);
-		} catch (error) {
-			setLoggingIn(false);
-		}
-		setLoggingIn(false);
-		render(<Check color="success" />)
-		router.push("/");
-	}
-	return (
-		<>
-			<Head>
-				<title>Login | Ararat</title>
-			</Head>
-			<Container>
-				<Grid
-					container
-					direction="column"
-					justifyContent="center"
-					alignItems="center"
-					mt={30}
-				>
-					<Grid
-						container
-						direction="column"
-						justifyContent="center"
-						alignItems="center"
-					>
-						<Image src={logo} width={160} height={85.16} />
-						<Typography align="center" mt={1} variant="subtitle1">
-							Welcome to Ararat! Please login to continue.
-						</Typography>
-						<FormControl sx={{ m: 1, width: "40ch" }} variant="outlined">
-							<TextField
-								onChange={(e) => setEmail(e.target.value)}
-								margin="dense"
-								placeholder="Email"
-								variant="outlined"
-								type="email"
-							/>
-							<TextField
-								onChange={(e) => setPassword(e.target.value)}
-								margin="dense"
-								placeholder="Password"
-								variant="outlined"
-								type="password"
-							/>
-							<Box component="div" sx={{ mt: 1 }}>
-								<LoadingButton
-                                    loading={loggingIn}
-									onClick={loginUser}
-									sx={{ width: "100%" }}
-									variant="contained"
-									color="primary"
-								>
-									Login
-								</LoadingButton>
-							</Box>
-						</FormControl>
-						<Button variant="text">Forgot Password?</Button>
-						<Link href="/auth/signup">
-						<Button variant="text">Create Account</Button>
-						</Link>
-					</Grid>
-				</Grid>
-			</Container>
-		</>
-	);
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loggingIn, setLoggingIn] = useState(false);
+  const [loginErr, setLoginErr] = useState("");
+  const loginUser = async () => {
+    try {
+      setLoggingIn(true);
+      await login(email, password);
+    } catch (error) {
+      setLoggingIn(false);
+      setLoginErr(error);
+    }
+    setLoggingIn(false);
+    render(<Check color="success" />);
+    router.push("/");
+  };
+  return (
+    <>
+      <Head>
+        <title>Login | Ararat</title>
+      </Head>
+      <Container>
+        <Grid container direction="column" justifyContent="center" alignItems="center" mt={30}>
+          <Grid container direction="column" justifyContent="center" alignItems="center">
+            <Image src={logo} width={160} height={85.16} />
+            <Typography align="center" mt={1} variant="subtitle1">
+              Welcome to Ararat! Please login to continue.
+            </Typography>
+            <FormControl sx={{ m: 1, width: "40ch" }} variant="outlined">
+              {loginErr && <Alert severity="error">{loginErr}</Alert>}
+              <TextField
+                onChange={(e) => setEmail(e.target.value)}
+                margin="dense"
+                placeholder="Email"
+                variant="outlined"
+                type="email"
+              />
+              <TextField
+                onChange={(e) => setPassword(e.target.value)}
+                margin="dense"
+                placeholder="Password"
+                variant="outlined"
+                type="password"
+              />
+              <Box component="div" sx={{ mt: 1 }}>
+                <LoadingButton
+                  loading={loggingIn}
+                  onClick={loginUser}
+                  sx={{ width: "100%" }}
+                  variant="contained"
+                  color="primary"
+                >
+                  Login
+                </LoadingButton>
+              </Box>
+            </FormControl>
+            <Button variant="text">Forgot Password?</Button>
+            <Link href="/auth/signup">
+              <Button variant="text">Create Account</Button>
+            </Link>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
 }
