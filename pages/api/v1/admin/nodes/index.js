9e8cf4055f2cc15ce1dc86737de4f3d89a07e577
Joseph Maldjian <josephavm@icloud.com>
2022-01-17 16:00:22 -0600

    Finish Node Creation

--- pages/api/v1/admin/nodes/index.js
+++ pages/api/v1/admin/nodes/index.js
@@ -11,22 +11,29 @@ export default async function handler(req, res) {
 		case "POST": {
 			let { db } = await connectToDatabase();
 			try {
-			var node = {
-				name: req.body.name,
-				address: {
-					hostname: req.body.address.hostname,
-					port: req.body.address.port,
-				},
-				access_token: access_token,
-			};
-		} catch {
-			return res
-				.status(400)
-				.send({ status: "error", data: "Invalid request" });
-		}
+				var node = {
+					name: req.body.name,
+					address: {
+						hostname: req.body.address.hostname,
+						port: req.body.address.port,
+						ssl: req.body.address.ssl
+					},
+					limits: {
+						memory: req.body.limits.memory,
+						cpu: req.body.limits.cpu,
+						disk: req.body.limits.disk,
+					},
+					access_token: access_token,
+				};
+			} catch {
+				return res
+					.status(400)
+					.send({ status: "error", data: "Invalid request" });
+			}
 			try {
 				var node_insert = await db.collection("nodes").insertOne(node);
 			} catch (error) {
+				console.log(error)
 				return res.status(500).send({
 					status: "error",
 					data: "An error occured while creating the node",
@@ -43,31 +50,36 @@ export default async function handler(req, res) {
 				let cipher = crypto.createCipheriv("aes-256-ctr", process.env.ENC_KEY, iv)
 				var hashed_key = Buffer.concat([cipher.update(access_token), cipher.final()]);
 			} catch (error) {
+				console.log(error)
 				return res.status(500).send({
 					status: "error",
 					data: "An error occured while creating the node",
 				});
 			}
 			try {
-			db.collection("nodes").updateOne({
-				_id: node_insert.insertedId,
-			}, {
-				$set: {
-					access_token: access_token_identifier + "::" + hashed_key.toString("hex"),
-					access_token_iv: iv.toString("hex")}
-			})
-		} catch (error) {
-			return res.status(500).send({
-				status: "error",
-				data: "An error occured while creating the node"
-			})
-		}
-		return res.json({
-			status: "success",
-			data: {
-				access_token: access_token,
+				db.collection("nodes").updateOne({
+					_id: node_insert.insertedId,
+				}, {
+					$set: {
+						access_token: access_token_identifier + "::" + hashed_key.toString("hex"),
+						access_token_iv: iv.toString("hex")
+					}
+				})
+			} catch (error) {
+				console.log(error)
+				return res.status(500).send({
+					status: "error",
+					data: "An error occured while creating the node"
+				})
 			}
-		})
+			return res.json({
+				status: "success",
+				data: {
+					access_token: access_token,
+					id: node_insert.insertedId,
+					panel_url: process.env.URL
+				}
+			})
 
 			break;
 		}
