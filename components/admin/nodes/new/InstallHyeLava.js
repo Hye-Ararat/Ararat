e2b9fb67dbb0c5b08dfb7cb6b28827c4ce29fc40
Joseph Maldjian <josephavm@icloud.com>
2022-01-17 16:14:15 -0600

    Replace quotes with escape characters

--- components/admin/nodes/new/InstallHyeLava.js
+++ components/admin/nodes/new/InstallHyeLava.js
@@ -21,7 +21,7 @@ export default function InstallHyeLava(props) {
             <Command command={`apt -y install curl`} sx={{ mb: 2 }} />
             <Divider sx={{ mb: 1 }} />
             <Typography variant="h5" sx={{ mb: 1 }}>Installing Hye Lava</Typography>
-            <Typography variant="p" sx={{mb: 2}}>We provide an easy 1-off command to install Hye Lava easily. Just paste this command into your system and it's off to the races.</Typography>
+            <Typography variant="p" sx={{ mb: 2 }}>We provide an easy 1-off command to install Hye Lava easily. Just paste this command into your system and it&apos;s off to the races.</Typography>
             <Command command={`bash <(curl -s https://gist.githubusercontent.com/Hye-Dev/9db8116e51620a14792041454e609eaa/raw/d61995e5445e3d560e6909e7cd4e0ccf74a85a21/installLavaAlpha.sh)`} sx={{ mb: 2 }} />
             <Divider sx={{ mb: 1 }} />
             <Typography variant="h5" sx={{ mb: 1 }}>Configuring LXD</Typography>
@@ -29,7 +29,7 @@ export default function InstallHyeLava(props) {
             <Command command={"* soft nofile 1048576\n* hard nofile 1048576\nroot soft nofile 1048576\nroot hard nofile 1048576\n* soft memlock unlimited\n* hard memlock unlimited\nroot soft memlock unlimited\nroot hard memlock unlimited\n"} />
             <Typography variant="p">Add the following lines to /etc/sysctl.conf</Typography>
             <Command command={"fs.aio-max-nr = 524288\nfs.inotify.max_queued_events = 1048576\nfs.inotify.max_user_instances = 1048576\nfs.inotify.max_user_watches = 1048576\nkernel.dmesg_restrict = 1\nkernel.keys.maxbytes = 2000000\nkernel.keys.maxkeys = 2000\nnet.core.bpf_jit_limit = 3000000000\nnet.ipv4.neigh.default.gc_thresh3 = 8192\nnet.ipv6.neigh.default.gc_thresh3 = 8192\nvm.max_map_count = 262144\n"} />
-            <Typography variant="p">Reboot your node, and you're ready to continue to the next step</Typography>
+            <Typography variant="p">Reboot your node, and you&apos;re ready to continue to the next step</Typography>
         </>
     )
 }
--- components/admin/nodes/new/SetupNode.js
+++ components/admin/nodes/new/SetupNode.js
@@ -11,7 +11,7 @@ export default function SetupNode(props) {
     return (
         <>
             <Typography variant="h5" sx={{ mb: 1 }}>Setup Hye Lava</Typography>
-            <Typography variant="p" sx={{ mb: 2 }}>We provide an easy 1-off command to configure Hye Lava easily. Just paste this command in your Hye Lava root directory (normally /srv/lava) and it's off to the races.</Typography>
+            <Typography variant="p" sx={{ mb: 2 }}>We provide an easy 1-off command to configure Hye Lava easily. Just paste this command in your Hye Lava root directory (normally /srv/lava) and it&apos;s off to the races.</Typography>
             <Command command={`node index.js setup --api-key ${props.nodeKey} --node ${props.nodeID} --panel-url ${props.panelURL}/`} sx={{ mb: 2 }} />
         </>
     )
