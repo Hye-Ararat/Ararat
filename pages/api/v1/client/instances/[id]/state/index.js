2ec06eac0a556fc7c5a298e5ff8d86543db4b150
Joseph Maldjian <josephavm@icloud.com>
2022-01-17 12:09:44 -0600

    Fix API key splitting

--- pages/api/v1/client/instances/[id]/state/index.js
+++ pages/api/v1/client/instances/[id]/state/index.js
@@ -1,11 +1,11 @@
-const {post, get} = require("axios");
-import {connectToDatabase} from "../../../../../../../util/mongodb"
+const { post, get } = require("axios");
+import { connectToDatabase } from "../../../../../../../util/mongodb"
 import { ObjectId } from "mongodb";
 import { decode } from "jsonwebtoken";
 import crypto from "crypto";
 export default async function handler(req, res) {
-    const {query: {id}, method} = req;
-    var {db} = await connectToDatabase();
+    const { query: { id }, method } = req;
+    var { db } = await connectToDatabase();
     try {
         var user = decode(req.headers.authorization.split(" ")[1])
     } catch {
@@ -16,9 +16,9 @@ export default async function handler(req, res) {
     try {
         var instance = await db.collection("instances").findOne({
             _id: ObjectId(id),
-            [`users.${user.id}`]: {$exists: true}
+            [`users.${user.id}`]: { $exists: true }
         })
-    } catch (error){
+    } catch (error) {
         console.log(error)
         console.log("error 1")
         return res.status(500).json(
@@ -42,13 +42,13 @@ export default async function handler(req, res) {
         case "GET":
             try {
                 var decipher = crypto.createDecipheriv("aes-256-ctr", process.env.ENC_KEY, Buffer.from(node.access_token_iv, "hex"))
-                var access_token = Buffer.concat([decipher.update(Buffer.from(node.access_token, "hex")), decipher.final()])
+                var access_token = Buffer.concat([decipher.update(Buffer.from(node.access_token.split("::")[1], "hex")), decipher.final()])
                 var state = await post(`${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances/${id}/state`, req.body, {
                     headers: {
-                        Aunthorization: `Bearer ${access_token.toString()}`
+                        Authorization: `Bearer ${access_token.toString()}`
                     }
                 })
-            } catch (error){
+            } catch (error) {
                 console.log(error)
                 console.log("error 2")
                 return res.status(500).send("An error occured while sending the power action")
@@ -58,13 +58,13 @@ export default async function handler(req, res) {
         case "POST":
             try {
                 var decipher = crypto.createDecipheriv("aes-256-ctr", process.env.ENC_KEY, Buffer.from(node.access_token_iv, "hex"))
-                var access_token = Buffer.concat([decipher.update(Buffer.from(node.access_token, "hex")), decipher.final()])
+                var access_token = Buffer.concat([decipher.update(Buffer.from(node.access_token.split("::")[1], "hex")), decipher.final()])
                 var state = await post(`${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances/${id}/state`, req.body, {
                     headers: {
-                        Aunthorization: `Bearer ${access_token.toString()}`
+                        Authorization: `Bearer ${access_token.toString()}`
                     }
                 })
-            } catch (error){
+            } catch (error) {
                 console.log(error)
                 console.log("error 2")
                 return res.status(500).send("An error occured while fetching the power state")
