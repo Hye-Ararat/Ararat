b0ff721da745c515bb62736ea589cd7333f15d24
RyderAsking <clasherlodt@gmail.com>
2022-01-25 10:54:49 +0530

    Fix

--- pages/api/v1/client/auth/signup.js
+++ pages/api/v1/client/auth/signup.js
@@ -1,19 +1,16 @@
-const { connectToDatabase } = require('../../../../../util/mongodb');
+const { connectToDatabase } = require("../../../../../util/mongodb");
 import axios from "axios";
-import bcrypt from "bcryptjs"
-
-
+import bcrypt from "bcryptjs";
 
 export default async function handler(req, res) {
   const { method } = req;
   switch (method) {
-    case 'POST': {
+    case "POST": {
       var { db } = await connectToDatabase();
-      let user_data = await db.collection('users').findOne({
-        email: req.body.email,
+      let user_data = await db.collection("users").findOne({
+        email: req.body.email
       });
-      if (user_data)
-        return res.json({ status: 'error', data: 'That email already exists' });
+      if (user_data) return res.json({ status: "error", data: "That email already exists" });
       try {
         var salt = await bcrypt.genSalt(10);
         var hashedPassword = await bcrypt.hash(req.body.password, salt);
@@ -21,25 +18,24 @@ export default async function handler(req, res) {
         return res.status(500).json({ status: "error", data: "An error occured hile creating the user" });
       }
 
-
       const user = {
         username: req.body.username,
         first_name: req.body.name,
         last_name: req.body.surname,
         email: req.body.email,
         password: hashedPassword,
-        phone_number: null,
+        phone_number: null
       };
       await db.collection("users").insertOne(user);
       return res.json({
         status: "Success",
-        data: "User created successfully",
-      })
+        data: "User created successfully"
+      });
       break;
     }
     default: {
       res.status(400).send({
-        status: 'error',
+        status: "error"
       });
     }
   }
