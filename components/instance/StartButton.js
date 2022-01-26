688b87d862bb0e364959fa6064692568456dfdbb
RyderAsking <clasherlodt@gmail.com>
2022-01-26 11:36:57 +0530

    Better handling

--- components/instance/StartButton.js
+++ components/instance/StartButton.js
@@ -4,19 +4,25 @@ import axios from "axios";
 
 export default function StartButton(props) {
   const [starting, setStarting] = useState(false);
+
+  const start = () => {
+    setStarting(true);
+    axios
+      .post(`/api/v1/client/instances/${props.instance}/state`, {
+        state: "start"
+      })
+      .then((res) => {
+        setStarting(false);
+      });
+  };
+
   return (
     <LoadingButton
       loading={starting}
       color="success"
       variant="contained"
       sx={{ marginLeft: "auto", marginTop: "auto", marginBottom: "auto" }}
-      onClick={async () => {
-        setStarting(true);
-        await axios.post(`/api/v1/client/instances/${props.instance}/state`, {
-          state: "start"
-        });
-        setStarting(false);
-      }}
+      onClick={start}
     >
       Start
     </LoadingButton>
