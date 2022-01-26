39b874c63b0fe130ba6d42ea6d3a48e3a79a9817
RyderAsking <clasherlodt@gmail.com>
2022-01-26 11:33:00 +0530

    Cleaner code

--- components/instance/StartButton.js
+++ components/instance/StartButton.js
@@ -3,14 +3,22 @@ import { useState } from "react";
 import axios from "axios";
 
 export default function StartButton(props) {
-    const [starting, setStarting] = useState(false)
-    return(
-        <LoadingButton loading={starting} color="success" variant="contained" sx={{marginLeft: "auto", marginTop: "auto", marginBottom: "auto"}} onClick={async () => {
-            setStarting(true)
-            await axios.post(`/api/v1/client/instances/${props.instance}/state`, {
-                state: "start"
-            })
-            setStarting(false)
-
-        }}>Start</LoadingButton>    )
+  const [starting, setStarting] = useState(false);
+  return (
+    <LoadingButton
+      loading={starting}
+      color="success"
+      variant="contained"
+      sx={{ marginLeft: "auto", marginTop: "auto", marginBottom: "auto" }}
+      onClick={async () => {
+        setStarting(true);
+        await axios.post(`/api/v1/client/instances/${props.instance}/state`, {
+          state: "start"
+        });
+        setStarting(false);
+      }}
+    >
+      Start
+    </LoadingButton>
+  );
 }
