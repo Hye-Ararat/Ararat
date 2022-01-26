e82a2cfdae90ff3603538356db9765cc85b8cb7a
RyderAsking <clasherlodt@gmail.com>
2022-01-26 11:37:41 +0530

    Cleaner code

--- components/instance/StopButton.js
+++ components/instance/StopButton.js
@@ -3,24 +3,43 @@ import { useState } from "react";
 import axios from "axios";
 
 export default function StopButton(props) {
-    const [stopping, setStopping] = useState(false)
-    const [killing, setKilling] = useState(false)
-    const [showKill, setShowKill] = useState(false)
-    return(
-        showKill == false? <LoadingButton loading={stopping} color="error" variant="contained" sx={{marginLeft: "auto", marginTop: "auto", marginBottom: "auto"}} onClick={async () => {
-            setStopping(true)
-            await axios.post(`/api/v1/client/instances/${props.instance}/state`, {
-                state: "stop"
-            })
-            setShowKill(true)
-        }}>Stop</LoadingButton> : <LoadingButton loading={killing} color="error" variant="contained" sx={{marginLeft: "auto", marginTop: "auto", marginBottom: "auto"}} onClick={async () => {
-            setKilling(true)
-            setStopping(false)
-            await axios.post(`/api/v1/client/instances/${props.instance}/state`, {
-                state: "stop",
-                force: "true"
-            })
-            setKilling(false)
-            setShowKill(false)
-        }}>Kill</LoadingButton> )
+  const [stopping, setStopping] = useState(false);
+  const [killing, setKilling] = useState(false);
+  const [showKill, setShowKill] = useState(false);
+  return showKill == false ? (
+    <LoadingButton
+      loading={stopping}
+      color="error"
+      variant="contained"
+      sx={{ marginLeft: "auto", marginTop: "auto", marginBottom: "auto" }}
+      onClick={async () => {
+        setStopping(true);
+        await axios.post(`/api/v1/client/instances/${props.instance}/state`, {
+          state: "stop"
+        });
+        setShowKill(true);
+      }}
+    >
+      Stop
+    </LoadingButton>
+  ) : (
+    <LoadingButton
+      loading={killing}
+      color="error"
+      variant="contained"
+      sx={{ marginLeft: "auto", marginTop: "auto", marginBottom: "auto" }}
+      onClick={async () => {
+        setKilling(true);
+        setStopping(false);
+        await axios.post(`/api/v1/client/instances/${props.instance}/state`, {
+          state: "stop",
+          force: "true"
+        });
+        setKilling(false);
+        setShowKill(false);
+      }}
+    >
+      Kill
+    </LoadingButton>
+  );
 }
