880405e95ffa5ad72a9ad6799e2e25cfa69b8cc6
Joseph Maldjian <josephavm@icloud.com>
2022-01-27 10:40:03 -0600

    Add file saving

--- components/instance/files/FileEditor.js
+++ components/instance/files/FileEditor.js
@@ -1,249 +1,265 @@
 import dynamic from "next/dynamic";
 import { useEffect, useState } from 'react';
-import Monaco, {useMonaco} from "@monaco-editor/react";
+import Monaco, { useMonaco } from "@monaco-editor/react";
 import { Button, Container, Grid } from "@mui/material";
 import ResizeObserver from "react-resize-observer";
 import axios from "axios";
+import { LoadingButton } from "@mui/lab";
 
-export default function FileEditor({file, path, instance}) {    
-    const [data, setData] = useState(file);
-    function language() {
-        if (path.includes(".js")) {
-            return "javascript";
-        } else if (path.includes("sshd_config")){
-            return "ssh_config";
-        }         else if (path.includes(".conf")){
-            return "ini";
-        }  else {
-            return "plaintext";
-        }
+export default function FileEditor({ file, path, instance }) {
+  const [data, setData] = useState(file);
+  const [saving, setSaving] = useState(false);
+  function language() {
+    switch (path.split(".").pop()) {
+      case "js":
+        return "javascript";
+      case "py":
+        return "python";
+      case "sh":
+        return "shell";
+      case "rb":
+        return "ruby";
+      case "php":
+        return "php";
+      case "html":
+        return "html";
+      case "css":
+        return "css";
+      case "json":
+        return "json";
+      case "md":
+        return "markdown";
+      default:
+        return "plaintext";
     }
-    const monaco = useMonaco();
-    useEffect(() => {
-        if (monaco) {
-            monaco.editor.defineTheme("hye", {
-                "base": "vs-dark",
-                "inherit": true,
-                "rules": [
-                  {
-                    "background": "18212e",
-                    "token": ""
-                  },
-                  {
-                    "foreground": "6272a4",
-                    "token": "comment"
-                  },
-                  {
-                    "foreground": "f1fa8c",
-                    "token": "string"
-                  },
-                  {
-                    "foreground": "bd93f9",
-                    "token": "constant.numeric"
-                  },
-                  {
-                    "foreground": "bd93f9",
-                    "token": "constant.language"
-                  },
-                  {
-                    "foreground": "bd93f9",
-                    "token": "constant.character"
-                  },
-                  {
-                    "foreground": "bd93f9",
-                    "token": "constant.other"
-                  },
-                  {
-                    "foreground": "ffb86c",
-                    "token": "variable.other.readwrite.instance"
-                  },
-                  {
-                    "foreground": "ff79c6",
-                    "token": "constant.character.escaped"
-                  },
-                  {
-                    "foreground": "ff79c6",
-                    "token": "constant.character.escape"
-                  },
-                  {
-                    "foreground": "ff79c6",
-                    "token": "string source"
-                  },
-                  {
-                    "foreground": "ff79c6",
-                    "token": "string source.ruby"
-                  },
-                  {
-                    "foreground": "ff79c6",
-                    "token": "keyword"
-                  },
-                  {
-                    "foreground": "ff79c6",
-                    "token": "storage"
-                  },
-                  {
-                    "foreground": "8be9fd",
-                    "fontStyle": "italic",
-                    "token": "storage.type"
-                  },
-                  {
-                    "foreground": "50fa7b",
-                    "fontStyle": "underline",
-                    "token": "entity.name.class"
-                  },
-                  {
-                    "foreground": "50fa7b",
-                    "fontStyle": "italic underline",
-                    "token": "entity.other.inherited-class"
-                  },
-                  {
-                    "foreground": "50fa7b",
-                    "token": "entity.name.function"
-                  },
-                  {
-                    "foreground": "ffb86c",
-                    "fontStyle": "italic",
-                    "token": "variable.parameter"
-                  },
-                  {
-                    "foreground": "ff79c6",
-                    "token": "entity.name.tag"
-                  },
-                  {
-                    "foreground": "50fa7b",
-                    "token": "entity.other.attribute-name"
-                  },
-                  {
-                    "foreground": "8be9fd",
-                    "token": "support.function"
-                  },
-                  {
-                    "foreground": "6be5fd",
-                    "token": "support.constant"
-                  },
-                  {
-                    "foreground": "66d9ef",
-                    "fontStyle": " italic",
-                    "token": "support.type"
-                  },
-                  {
-                    "foreground": "66d9ef",
-                    "fontStyle": " italic",
-                    "token": "support.class"
-                  },
-                  {
-                    "foreground": "f8f8f0",
-                    "background": "ff79c6",
-                    "token": "invalid"
-                  },
-                  {
-                    "foreground": "f8f8f0",
-                    "background": "bd93f9",
-                    "token": "invalid.deprecated"
-                  },
-                  {
-                    "foreground": "cfcfc2",
-                    "token": "meta.structure.dictionary.json string.quoted.double.json"
-                  },
-                  {
-                    "foreground": "6272a4",
-                    "token": "meta.diff"
-                  },
-                  {
-                    "foreground": "6272a4",
-                    "token": "meta.diff.header"
-                  },
-                  {
-                    "foreground": "ff79c6",
-                    "token": "markup.deleted"
-                  },
-                  {
-                    "foreground": "50fa7b",
-                    "token": "markup.inserted"
-                  },
-                  {
-                    "foreground": "e6db74",
-                    "token": "markup.changed"
-                  },
-                  {
-                    "foreground": "bd93f9",
-                    "token": "constant.numeric.line-number.find-in-files - match"
-                  },
-                  {
-                    "foreground": "e6db74",
-                    "token": "entity.name.filename"
-                  },
-                  {
-                    "foreground": "f83333",
-                    "token": "message.error"
-                  },
-                  {
-                    "foreground": "eeeeee",
-                    "token": "punctuation.definition.string.begin.json - meta.structure.dictionary.value.json"
-                  },
-                  {
-                    "foreground": "eeeeee",
-                    "token": "punctuation.definition.string.end.json - meta.structure.dictionary.value.json"
-                  },
-                  {
-                    "foreground": "8be9fd",
-                    "token": "meta.structure.dictionary.json string.quoted.double.json"
-                  },
-                  {
-                    "foreground": "f1fa8c",
-                    "token": "meta.structure.dictionary.value.json string.quoted.double.json"
-                  },
-                  {
-                    "foreground": "50fa7b",
-                    "token": "meta meta meta meta meta meta meta.structure.dictionary.value string"
-                  },
-                  {
-                    "foreground": "ffb86c",
-                    "token": "meta meta meta meta meta meta.structure.dictionary.value string"
-                  },
-                  {
-                    "foreground": "ff79c6",
-                    "token": "meta meta meta meta meta.structure.dictionary.value string"
-                  },
-                  {
-                    "foreground": "bd93f9",
-                    "token": "meta meta meta meta.structure.dictionary.value string"
-                  },
-                  {
-                    "foreground": "50fa7b",
-                    "token": "meta meta meta.structure.dictionary.value string"
-                  },
-                  {
-                    "foreground": "ffb86c",
-                    "token": "meta meta.structure.dictionary.value string"
-                  }
-                ],
-                "colors": {
-                  "editor.foreground": "#f8f8f2",
-                  "editor.background": "#141c26",
-                  "editor.selectionBackground": "#233142",
-                  "editor.lineHighlightBackground": "#0e131a",
-                  "editorCursor.foreground": "#f8f8f0",
-                  "editorWhitespace.foreground": "#3B3A32",
-                  "editorIndentGuide.activeBackground": "#9D550FB0",
-                  "editor.selectionHighlightBorder": "#222218"
-                }
-              })
-            monaco.editor.setTheme("hye")
+  }
+  const monaco = useMonaco();
+  useEffect(() => {
+    if (monaco) {
+      monaco.editor.defineTheme("hye", {
+        "base": "vs-dark",
+        "inherit": true,
+        "rules": [
+          {
+            "background": "18212e",
+            "token": ""
+          },
+          {
+            "foreground": "6272a4",
+            "token": "comment"
+          },
+          {
+            "foreground": "f1fa8c",
+            "token": "string"
+          },
+          {
+            "foreground": "bd93f9",
+            "token": "constant.numeric"
+          },
+          {
+            "foreground": "bd93f9",
+            "token": "constant.language"
+          },
+          {
+            "foreground": "bd93f9",
+            "token": "constant.character"
+          },
+          {
+            "foreground": "bd93f9",
+            "token": "constant.other"
+          },
+          {
+            "foreground": "ffb86c",
+            "token": "variable.other.readwrite.instance"
+          },
+          {
+            "foreground": "ff79c6",
+            "token": "constant.character.escaped"
+          },
+          {
+            "foreground": "ff79c6",
+            "token": "constant.character.escape"
+          },
+          {
+            "foreground": "ff79c6",
+            "token": "string source"
+          },
+          {
+            "foreground": "ff79c6",
+            "token": "string source.ruby"
+          },
+          {
+            "foreground": "ff79c6",
+            "token": "keyword"
+          },
+          {
+            "foreground": "ff79c6",
+            "token": "storage"
+          },
+          {
+            "foreground": "8be9fd",
+            "fontStyle": "italic",
+            "token": "storage.type"
+          },
+          {
+            "foreground": "50fa7b",
+            "fontStyle": "underline",
+            "token": "entity.name.class"
+          },
+          {
+            "foreground": "50fa7b",
+            "fontStyle": "italic underline",
+            "token": "entity.other.inherited-class"
+          },
+          {
+            "foreground": "50fa7b",
+            "token": "entity.name.function"
+          },
+          {
+            "foreground": "ffb86c",
+            "fontStyle": "italic",
+            "token": "variable.parameter"
+          },
+          {
+            "foreground": "ff79c6",
+            "token": "entity.name.tag"
+          },
+          {
+            "foreground": "50fa7b",
+            "token": "entity.other.attribute-name"
+          },
+          {
+            "foreground": "8be9fd",
+            "token": "support.function"
+          },
+          {
+            "foreground": "6be5fd",
+            "token": "support.constant"
+          },
+          {
+            "foreground": "66d9ef",
+            "fontStyle": " italic",
+            "token": "support.type"
+          },
+          {
+            "foreground": "66d9ef",
+            "fontStyle": " italic",
+            "token": "support.class"
+          },
+          {
+            "foreground": "f8f8f0",
+            "background": "ff79c6",
+            "token": "invalid"
+          },
+          {
+            "foreground": "f8f8f0",
+            "background": "bd93f9",
+            "token": "invalid.deprecated"
+          },
+          {
+            "foreground": "cfcfc2",
+            "token": "meta.structure.dictionary.json string.quoted.double.json"
+          },
+          {
+            "foreground": "6272a4",
+            "token": "meta.diff"
+          },
+          {
+            "foreground": "6272a4",
+            "token": "meta.diff.header"
+          },
+          {
+            "foreground": "ff79c6",
+            "token": "markup.deleted"
+          },
+          {
+            "foreground": "50fa7b",
+            "token": "markup.inserted"
+          },
+          {
+            "foreground": "e6db74",
+            "token": "markup.changed"
+          },
+          {
+            "foreground": "bd93f9",
+            "token": "constant.numeric.line-number.find-in-files - match"
+          },
+          {
+            "foreground": "e6db74",
+            "token": "entity.name.filename"
+          },
+          {
+            "foreground": "f83333",
+            "token": "message.error"
+          },
+          {
+            "foreground": "eeeeee",
+            "token": "punctuation.definition.string.begin.json - meta.structure.dictionary.value.json"
+          },
+          {
+            "foreground": "eeeeee",
+            "token": "punctuation.definition.string.end.json - meta.structure.dictionary.value.json"
+          },
+          {
+            "foreground": "8be9fd",
+            "token": "meta.structure.dictionary.json string.quoted.double.json"
+          },
+          {
+            "foreground": "f1fa8c",
+            "token": "meta.structure.dictionary.value.json string.quoted.double.json"
+          },
+          {
+            "foreground": "50fa7b",
+            "token": "meta meta meta meta meta meta meta.structure.dictionary.value string"
+          },
+          {
+            "foreground": "ffb86c",
+            "token": "meta meta meta meta meta meta.structure.dictionary.value string"
+          },
+          {
+            "foreground": "ff79c6",
+            "token": "meta meta meta meta meta.structure.dictionary.value string"
+          },
+          {
+            "foreground": "bd93f9",
+            "token": "meta meta meta meta.structure.dictionary.value string"
+          },
+          {
+            "foreground": "50fa7b",
+            "token": "meta meta meta.structure.dictionary.value string"
+          },
+          {
+            "foreground": "ffb86c",
+            "token": "meta meta.structure.dictionary.value string"
+          }
+        ],
+        "colors": {
+          "editor.foreground": "#f8f8f2",
+          "editor.background": "#141c26",
+          "editor.selectionBackground": "#233142",
+          "editor.lineHighlightBackground": "#0e131a",
+          "editorCursor.foreground": "#f8f8f0",
+          "editorWhitespace.foreground": "#3B3A32",
+          "editorIndentGuide.activeBackground": "#9D550FB0",
+          "editor.selectionHighlightBorder": "#222218"
         }
-    }, [monaco])
-    return (
-        <div style={{cursor: "text", width: "100%"}}>
-        <Monaco height={"50vh"} width={"100%"} onChange={(data) => setData(data)} id="monaco-editor-parent" theme="hye" value={typeof(file) == "object" ? JSON.stringify(file) : file.length == 0 ? " " : file} style={{marginLeft: "auto", marginRight: "auto"}} language={language()} options={{cursorSmoothCaretAnimation: true, cursorBlinking: "smooth", smoothScrolling: true, automaticLayout: true}}/>
-        <Button sx={{mt: 1}}variant="contained" color="success" onClick={async () => {
-            console.log(data);
-            await axios.post(`http://81.205.168.8:3535/api/v1/instances/${instance}/files?path=${path}`, data, {
-                headers: {
-                    'Content-Type': 'text/plain',
-                }
-            });
-            
-        }}>Save</Button>
-        </div>
-    )
+      })
+      monaco.editor.setTheme("hye")
+    }
+  }, [monaco])
+  return (
+    <div style={{ cursor: "text", width: "100%" }}>
+      <Monaco height={"50vh"} width={"100%"} onChange={(data) => setData(data)} id="monaco-editor-parent" theme="hye" value={typeof (file) == "object" ? JSON.stringify(file) : file.length == 0 ? " " : file} style={{ marginLeft: "auto", marginRight: "auto" }} language={language()} options={{ cursorSmoothCaretAnimation: true, cursorBlinking: "smooth", smoothScrolling: true, automaticLayout: true }} />
+      <LoadingButton loading={saving} sx={{ mt: 1 }} variant="contained" color="success" onClick={async () => {
+        console.log(data);
+        setSaving(true);
+        await axios.post(`/api/v1/client/instances/${instance}/files?path=${path}`, data, {
+          headers: {
+            'Content-Type': 'text/plain',
+          }
+        });
+        setSaving(false);
+      }}>Save</LoadingButton>
+    </div>
+  )
 }
--- pages/api/v1/client/instances/[id]/files/index.js
+++ pages/api/v1/client/instances/[id]/files/index.js
@@ -1,76 +1,131 @@
-import { get } from "axios"
+import { get, post } from "axios"
 import { decode } from "jsonwebtoken"
 import { connectToDatabase } from "../../../../../../../util/mongodb";
 import { ObjectId } from "mongodb";
 import crypto from "crypto"
 export default async function handler(req, res) {
-    const { query: { id, path } } = req;
-    console.log(id);
-    try {
-        var user = decode(req.headers.authorization.split(" ")[1])
-    } catch {
-        return res.status(403).send("Unauthorized");
-    }
-    var { db } = await connectToDatabase();
-    try {
-        var instance = await db.collection("instances").findOne({
-            _id: ObjectId(id),
-            [`users.${user.id}`]: { $exists: true }
-        })
-    } catch (error) {
-        console.log(error)
-        console.log("error 1 oooh its shit fdksj;asldkjfakjsdl")
-        return res.status(500).send("An error occured while fetching the instance data")
-    }
-    if (!instance) {
-        return res.status(404).send("Instance does not exist")
-    }
-    try {
-        var node = await db.collection("nodes").findOne({
-            _id: ObjectId(instance.node)
-        })
-    } catch {
-        return res.status(500).send("An error occured while fetching the node data");
-    }
-    if (!node) {
-        return res.status(500).send("Node associated with server does not exist")
-    }
-    try {
-        var decipher = crypto.createDecipheriv("aes-256-ctr", process.env.ENC_KEY, Buffer.from(node.access_token_iv, "hex"))
-        var access_token = Buffer.concat([decipher.update(Buffer.from(node.access_token.split("::")[1], "hex")), decipher.final()])
-        console.log(access_token.toString())
-        console.log(`${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances/${id}/files?path=${path}`)
-        var files = await get(`${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances/${id}/files?path=/${path}`, {
-            headers: {
-                Authorization: `Bearer ${access_token.toString()}`
+    const { query: { id, path }, method } = req;
+    switch (method) {
+        case "GET":
+            console.log(id);
+            try {
+                var user = decode(req.headers.authorization.split(" ")[1])
+            } catch {
+                return res.status(403).send("Unauthorized");
             }
-        })
-    } catch (error) {
-        console.log(error)
-        console.log("error 2")
-        return res.status(500).send("An error occured while fetching the files")
-    }
-    try {
-        var sorted = files.data.list.sort((a, b) => {
-            if (a.type == "directory" && b.type == "file") {
-                return -1
-            } else if (a.type == "file" && b.type == "directory") {
-                return 1
-            } else {
-                if (a.name > b.name) {
-                    return -1
-                } else if (b.name > a.name) {
-                    return 1
-                } else {
-                    return 0
-                }
+            var { db } = await connectToDatabase();
+            try {
+                var instance = await db.collection("instances").findOne({
+                    _id: ObjectId(id),
+                    [`users.${user.id}`]: { $exists: true }
+                })
+            } catch (error) {
+                console.log(error)
+                console.log("error 1 oooh its shit fdksj;asldkjfakjsdl")
+                return res.status(500).send("An error occured while fetching the instance data")
+            }
+            if (!instance) {
+                return res.status(404).send("Instance does not exist")
+            }
+            try {
+                var node = await db.collection("nodes").findOne({
+                    _id: ObjectId(instance.node)
+                })
+            } catch {
+                return res.status(500).send("An error occured while fetching the node data");
+            }
+            if (!node) {
+                return res.status(500).send("Node associated with server does not exist")
+            }
+            try {
+                var decipher = crypto.createDecipheriv("aes-256-ctr", process.env.ENC_KEY, Buffer.from(node.access_token_iv, "hex"))
+                var access_token = Buffer.concat([decipher.update(Buffer.from(node.access_token.split("::")[1], "hex")), decipher.final()])
+                console.log(access_token.toString())
+                console.log(`${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances/${id}/files?path=${path}`)
+                var files = await get(`${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances/${id}/files?path=/${path}`, {
+                    headers: {
+                        Authorization: `Bearer ${access_token.toString()}`
+                    }
+                })
+            } catch (error) {
+                console.log(error)
+                console.log("error 2")
+                return res.status(500).send("An error occured while fetching the files")
+            }
+            try {
+                var sorted = files.data.list.sort((a, b) => {
+                    if (a.type == "directory" && b.type == "file") {
+                        return -1
+                    } else if (a.type == "file" && b.type == "directory") {
+                        return 1
+                    } else {
+                        if (a.name > b.name) {
+                            return -1
+                        } else if (b.name > a.name) {
+                            return 1
+                        } else {
+                            return 0
+                        }
 
+                    }
+                })
+            } catch {
+                return res.send(files.data)
             }
-        })
-    } catch {
-        return res.send(files.data)
+            files.data.list = sorted;
+            return res.send(files.data)
+            break;
+        case "POST":
+            console.log(id);
+            try {
+                var user = decode(req.headers.authorization.split(" ")[1])
+            } catch {
+                return res.status(403).send("Unauthorized");
+            }
+            var { db } = await connectToDatabase();
+            try {
+                var instance = await db.collection("instances").findOne({
+                    _id: ObjectId(id),
+                    [`users.${user.id}`]: { $exists: true }
+                })
+            } catch (error) {
+                console.log(error)
+                console.log("error 1 oooh its shit fdksj;asldkjfakjsdl")
+                return res.status(500).send("An error occured while fetching the instance data")
+            }
+            if (!instance) {
+                return res.status(404).send("Instance does not exist")
+            }
+            try {
+                var node = await db.collection("nodes").findOne({
+                    _id: ObjectId(instance.node)
+                })
+            } catch {
+                return res.status(500).send("An error occured while fetching the node data");
+            }
+            if (!node) {
+                return res.status(500).send("Node associated with server does not exist")
+            }
+            try {
+                var decipher = crypto.createDecipheriv("aes-256-ctr", process.env.ENC_KEY, Buffer.from(node.access_token_iv, "hex"))
+                var access_token = Buffer.concat([decipher.update(Buffer.from(node.access_token.split("::")[1], "hex")), decipher.final()])
+                console.log(access_token.toString())
+                console.log(`${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances/${id}/files?path=${path}`)
+                var files = await post(`${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances/${id}/files?path=/${path}`, req.body, {
+                    headers: {
+                        Authorization: `Bearer ${access_token.toString()}`,
+                        "Content-Type": "text/plain"
+                    }
+                })
+            } catch (error) {
+                console.log(error)
+                console.log("error 2")
+                return res.status(500).send("An error occured while uplooading")
+            }
+            return res.send("Success")
+            break;
+        default:
+            return res.status(405).send("Method not allowed")
     }
-    files.data.list = sorted;
-    res.send(files.data)
 
 }
