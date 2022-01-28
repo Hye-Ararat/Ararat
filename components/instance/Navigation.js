0f1ef1751e58986de3eb9db493deabed13fbd1b9
Joseph Maldjian <josephavm@icloud.com>
2022-01-28 13:34:07 -0600

    Add power actions, state, and external console to navigation

--- components/instance/Navigation.js
+++ components/instance/Navigation.js
@@ -11,7 +11,9 @@ import {
   ListItemText,
   Divider,
   Avatar,
-  SvgIcon
+  SvgIcon,
+  Grid,
+  Button
 } from "@mui/material";
 import {
   Inbox as InboxIcon,
@@ -37,9 +39,13 @@ import * as Icons from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-const fetcher = (url) => axios.get(url).then((res) => res.data);
+import StateIndicator from "./StateIndicator";
+import { InstanceStore } from "../../states/instance";
+import StopButton from "./StopButton";
+import StartButton from "./StartButton";
 
 export default function Navigation({ children, ...props }) {
+  const fetcher = (url) => axios.get(url).then((res) => res.data);
   console.log(props.page);
   const router = useRouter();
   const { id } = router.query;
@@ -54,6 +60,47 @@ export default function Navigation({ children, ...props }) {
       return data.data;
     }
   }
+  const instance = {
+    data: InstanceStore.useStoreState((state) => state.data),
+    setData: InstanceStore.useStoreActions((state) => state.setData),
+    sockets: {
+      monitor: InstanceStore.useStoreState((state) => state.sockets.monitor),
+      setMonitor: InstanceStore.useStoreActions((state) => state.sockets.setMonitor)
+    }
+  }
+
+  var { data: instanceData } = useSWR(`/api/v1/client/instances/${id}?include=["magma_cube", "node", "network"]`, fetcher);
+  useEffect(() => {
+    if (instance.data) {
+      console.log("exists");
+      console.log(instance.data);
+    } else {
+      console.log("doesnt");
+      instance.setData(instanceData);
+    }
+  }, [instance.data, instanceData])
+  useEffect(() => {
+    if (instance.data) {
+      console.log("yes");
+      console.log("yes2");
+      if (instance.sockets.monitor) {
+        instance.sockets.monitor.onopen = () => {
+          axios.get("/api/v1/client/instances/" + instance.data._id + "/monitor/ws").then((res) => {
+            instance.sockets.monitor.send(res.data.data.access_token);
+          });
+        };
+      } else {
+        instance.sockets.setMonitor(
+          new WebSocket(
+            `${instance.data.relationships.node.address.ssl ? "wss" : "ws"}://${instance.data.relationships.node.address.hostname
+            }:${instance.data.relationships.node.address.port}/api/v1/instances/${instance.data._id}/monitor`
+          )
+        );
+      }
+    } else {
+      console.log("no");
+    }
+  }, [instance.data, instance.sockets.monitor]);
   useEffect(() => {
     console.log("Nav redone");
   }, []);
@@ -64,8 +111,31 @@ export default function Navigation({ children, ...props }) {
         <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
           <Toolbar>
             <Typography variant="h6" noWrap component="div">
-              Ararat
+              {props.page ? instance.data ? instance.data.name : "Ararat" : "Ararat"}
             </Typography>
+            {props.page ? instance.data ?
+              <Grid container sx={{ ml: "auto" }} xs={3}>
+                <Box sx={{
+                  mt: "auto", mb: "auto", height: "100%"
+                }} onClick={() => {
+                  window.open("/instance/" + instance.data._id + "/console", 'popUpWindow', 'height=600,width=800,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
+                }} >
+                  <ConsoleIcon sx={{
+                    mt: "auto", width: "30px", height: "30px"
+                  }} />
+                </Box>
+                <StartButton instance={instance.data._id} />
+                <Button
+                  color="warning"
+                  variant="contained"
+                  sx={{ marginLeft: "auto", marginTop: "auto", marginBottom: "auto" }}
+                >
+                  Restart
+                </Button>
+                <StopButton instance={instance.data._id} />
+                <StateIndicator />
+              </Grid>
+              : '' : ""}
           </Toolbar>
         </AppBar>
         <Drawer
@@ -122,11 +192,11 @@ export default function Navigation({ children, ...props }) {
                 </ListItem>
               </Link>
               <Link href={`/instance/${encodeURIComponent(id)}`}>
-                <ListItem button selected={props.page == "backups" ? true : false}>
+                <ListItem button selected={props.page == "snapshots" ? true : false}>
                   <ListItemIcon>
                     <BackupIcon />
                   </ListItemIcon>
-                  <ListItemText primary="Backups" />
+                  <ListItemText primary="Snapshots" />
                 </ListItem>
               </Link>
               <Link href={`/instance/${encodeURIComponent(id)}`}>
@@ -172,20 +242,20 @@ export default function Navigation({ children, ...props }) {
               {Addons()
                 ? Addons() != "Loading"
                   ? Addons().map((addon) => {
-                      library.add();
-                      return (
-                        <Link href={`/instances/${encodeURIComponent(id)}${addon.route}`} key={addon._id}>
-                          <ListItem button>
-                            <ListItemIcon>
-                              <SvgIcon sx={{ width: 24, height: 24, ml: 0.2 }}>
-                                <FontAwesomeIcon icon={Icons[addon.icon]} />
-                              </SvgIcon>
-                            </ListItemIcon>
-                            <ListItemText primary={addon.name} />
-                          </ListItem>
-                        </Link>
-                      );
-                    })
+                    library.add();
+                    return (
+                      <Link href={`/instances/${encodeURIComponent(id)}${addon.route}`} key={addon._id}>
+                        <ListItem button>
+                          <ListItemIcon>
+                            <SvgIcon sx={{ width: 24, height: 24, ml: 0.2 }}>
+                              <FontAwesomeIcon icon={Icons[addon.icon]} />
+                            </SvgIcon>
+                          </ListItemIcon>
+                          <ListItemText primary={addon.name} />
+                        </ListItem>
+                      </Link>
+                    );
+                  })
                   : ""
                 : ""}
             </List>
