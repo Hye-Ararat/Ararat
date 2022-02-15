ca01cf618ded34078f70657be6e260e14b53fb9b
Joseph Maldjian <josephavm@icloud.com>
2022-02-14 22:49:26 -0600

    Switch to Prisma & new format

--- pages/admin/nodes/index.js
+++ pages/admin/nodes/index.js
@@ -5,6 +5,7 @@ import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { Box } from "@mui/system";
+import { PrismaClient } from "@prisma/client";
 export async function getServerSideProps({ req, res }) {
     if (!req.cookies.access_token) {
         return {
@@ -18,10 +19,8 @@ export async function getServerSideProps({ req, res }) {
         "Cache-Control",
         "public, s-maxage=10, stale-while-revalidate=59"
     );
-    var { connectToDatabase } = require("../../../util/mongodb")
-    var { db } = await connectToDatabase();
-    var { verify, decode } = require("jsonwebtoken");
-    var { ObjectId } = require("mongodb");
+    const prisma = new PrismaClient();
+    const { verify, decode } = require("jsonwebtoken");
     try {
         var valid_session = verify(req.cookies.access_token, process.env.ENC_KEY)
     } catch {
@@ -40,24 +39,28 @@ export async function getServerSideProps({ req, res }) {
             }
         }
     }
-    var user_data = decode(req.cookies.access_token)
+    const user_data = decode(req.cookies.access_token)
     console.log(user_data)
-    if (user_data.admin && user_data.admin.nodes && user_data.admin.nodes.read) {
-        var nodes = await db.collection('nodes').find({}).toArray();
-        console.log(nodes)
-        var safeNodes = [];
-        nodes.forEach(node => {
-            node.access_token = undefined;
-            node.access_token_iv = undefined;
-            node.id = node._id;
-            node._id = undefined;
-            safeNodes.push(node);
+    let nodes = [];
+    if (user_data.permissions.includes("list-nodes")) {
+        nodes = await prisma.node.findMany({
+            select: {
+                id: true,
+                name: true,
+                _count: true,
+                cpu: true,
+                memory: true,
+                disk: true,
+                hostname: true,
+                port: true,
+                ssl: true
+            }
         })
+        console.log(nodes)
     }
-    console.log(safeNodes)
     return {
         props: {
-            nodes: safeNodes ? JSON.parse(JSON.stringify(safeNodes)) : JSON.parse(JSON.stringify([])),
+            nodes: nodes ? JSON.parse(JSON.stringify(nodes)) : JSON.parse(JSON.stringify([])),
             user: JSON.parse(JSON.stringify(user_data))
         }
     }
@@ -68,8 +71,8 @@ export default function Nodes({ nodes, user }) {
     const router = useRouter()
     return (
         <>
-            <Grid direction="row" container><Typography variant="h4" sx={{ mb: 1 }}>Nodes</Typography> {user.admin && user.admin.nodes && user.admin.nodes.write ? <Link href={"/admin/nodes/new"}><Button sx={{ mt: "auto", mb: "auto", ml: "auto" }} variant="contained" color="primary">Create Node</Button></Link> : ""}</Grid>
-            {user.admin && user.admin.nodes && user.admin.nodes.read ?
+            <Grid direction="row" container><Typography variant="h4" sx={{ mb: 1 }}>Nodes</Typography> {user.permissions.includes("create-node") ? <Link href={"/admin/nodes/new"}><Button sx={{ mt: "auto", mb: "auto", ml: "auto" }} variant="contained" color="primary">Create Node</Button></Link> : ""}</Grid>
+            {user.permissions.includes("list-nodes") ?
                 <Paper sx={{ borderRadius: 1.5 }}>
                     <TableContainer sx={{ borderRadius: 1.5 }}>
                         <Table>
@@ -89,16 +92,16 @@ export default function Nodes({ nodes, user }) {
                                             }} style={{ cursor: "pointer" }}>{node.name}</TableCell>
                                             <TableCell onClick={() => {
                                                 router.push(`/admin/nodes/${node.id}`)
-                                            }} style={{ cursor: "pointer" }} align="left">{node.limits.memory}</TableCell>
+                                            }} style={{ cursor: "pointer" }} align="left">{node.memory}</TableCell>
                                             <TableCell onClick={() => {
                                                 router.push(`/admin/nodes/${node.id}`)
-                                            }} style={{ cursor: "pointer" }} align="left">{node.limits.disk}</TableCell>
+                                            }} style={{ cursor: "pointer" }} align="left">{node.disk}</TableCell>
                                             <TableCell onClick={() => {
                                                 router.push(`/admin/nodes/${node.id}`)
-                                            }} style={{ cursor: "pointer" }} align="left">{node.limits.cpu}</TableCell>
+                                            }} style={{ cursor: "pointer" }} align="left">{node.cpu}</TableCell>
                                             <TableCell onClick={() => {
                                                 router.push(`/admin/nodes/${node.id}`)
-                                            }} style={{ cursor: "pointer" }} align="left">{node.address.ssl ? "true" : "false"}</TableCell>
+                                            }} style={{ cursor: "pointer" }} align="left">{node.ssl ? "true" : "false"}</TableCell>
                                         </TableRow>
                                     )
                                 })}
