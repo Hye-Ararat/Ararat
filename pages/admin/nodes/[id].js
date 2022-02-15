ecc6ed61d71405e5ae62dd7f0456ef6eb55026e4
Joseph Maldjian <josephavm@icloud.com>
2022-02-14 22:48:55 -0600

    Switch to Prisma

--- pages/admin/nodes/[id].js
+++ pages/admin/nodes/[id].js
@@ -9,6 +9,7 @@ import { Box } from "@mui/system";
 import { useState } from "react";
 import ResizeObserver from "react-resize-observer";
 import Network from "../../../components/admin/nodes/[id]/Network";
+import prisma, { PrismaClient } from "@prisma/client";
 export async function getServerSideProps({ req, res, query }) {
     if (!req.cookies.access_token) {
         return {
@@ -22,10 +23,8 @@ export async function getServerSideProps({ req, res, query }) {
         "Cache-Control",
         "public, s-maxage=10, stale-while-revalidate=59"
     );
-    var { connectToDatabase } = require("../../../util/mongodb")
-    var { db } = await connectToDatabase();
     var { verify, decode } = require("jsonwebtoken");
-    var { ObjectId } = require("mongodb");
+    const prisma = new PrismaClient();
     try {
         var valid_session = verify(req.cookies.access_token, process.env.ENC_KEY)
     } catch {
@@ -46,35 +45,30 @@ export async function getServerSideProps({ req, res, query }) {
     }
     var user_data = decode(req.cookies.access_token)
     console.log(user_data)
-    if (user_data.admin && user_data.admin.nodes && user_data.admin.nodes.read) {
-        var node = await db.collection('nodes').findOne({
-            _id: ObjectId(query.id)
-        });
+    if (user_data.permissions.includes("view-node")) {
+        console.log(query.id)
+        console.time("e")
+        var node = await prisma.node.findUnique({
+            where: {
+                id: query.id
+            },
+            select: {
+                id: true,
+                _count: true,
+                name: true,
+                cpu: true,
+                disk: true,
+                hostname: true,
+                memory: true,
+                port: true,
+                ssl: true,
+                networks: user_data.permissions.includes("list-networks")
+            }
+        })
+        console.timeEnd("e")
         console.log(node)
-        node.access_token = undefined;
-        node.access_token_iv = undefined;
-        node.id = node._id;
-        node._id = undefined;
     }
-    if (user_data.admin && user_data.admin.networks && user_data.admin.networks.read) {
-        var networks = await db.collection('networks').find({
-            node: query.id
-        }).toArray();
-        console.log(networks)
-        if (node) {
-            if (!node.relationships) {
-                node.relationships = {}
-            }
-            node.relationships.networks = networks;
-        } else {
-            var node = {
-                relationships: {
-                    networks: networks
-                }
-            }
-        }
-    }
-        return {
+    return {
         props: {
             node: node ? JSON.parse(JSON.stringify(node)) : null,
             user: JSON.parse(JSON.stringify(user_data))
@@ -83,29 +77,29 @@ export async function getServerSideProps({ req, res, query }) {
 }
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
-  
+
     return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ p: 3 }}>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
-      </div>
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
     );
-  }
-  function a11yProps(index) {
+}
+function a11yProps(index) {
     return {
-      id: `scrollable-auto-tab-${index}`,
-      "aria-controls": `scrollable-auto-tabpanel-${index}`
+        id: `scrollable-auto-tab-${index}`,
+        "aria-controls": `scrollable-auto-tabpanel-${index}`
     };
-  }
+}
 export default function Nodes({ node, user }) {
     console.log(node)
     console.log(user)
@@ -113,19 +107,19 @@ export default function Nodes({ node, user }) {
     const [width, setWidth] = useState();
     const handleChange = (event, newValue) => {
         setTab(newValue);
-      };
+    };
     return (
         <>
             {node ?
                 <>
                     <Typography variant="h4" sx={{ mb: 1 }}>{node.name}</Typography>
-                    <Box sx={{borderBottom: 1, borderColor: "divider"}}>
+                    <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
                         <Tabs value={tab} onChange={handleChange} scrollButtons="auto">
-                            <Tab label="Overview" {...a11yProps(0)} wrapped/>
-                            <Tab label="Settings" {...a11yProps(1)} wrapped/>
-                            <Tab label="Configuration" {...a11yProps(2)} wrapped/>
-                            <Tab label="Network" {...a11yProps(3)} wrapped/>
-                            <Tab label="Instances" {...a11yProps(4)} wrapped/>
+                            <Tab label="Overview" {...a11yProps(0)} wrapped />
+                            <Tab label="Settings" {...a11yProps(1)} wrapped />
+                            <Tab label="Configuration" {...a11yProps(2)} wrapped />
+                            <Tab label="Network" {...a11yProps(3)} wrapped />
+                            <Tab label="Instances" {...a11yProps(4)} wrapped />
                         </Tabs>
                     </Box>
                     <TabPanel value={tab} index={0}>
@@ -138,7 +132,7 @@ export default function Nodes({ node, user }) {
                         Yes
                     </TabPanel>
                     <TabPanel value={tab} index={3}>
-                        <Network user={user} node={node}/>
+                        <Network user={user} node={node} />
                     </TabPanel>
                     <TabPanel value={tab} index={4}>
                         Yes
