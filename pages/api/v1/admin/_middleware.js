902d6e0d3d88dbd115da3ead08cf1d849c3a5eb4
Joseph Maldjian <josephavm@icloud.com>
2022-02-16 11:33:48 -0600

    cleanup admin middleware

--- pages/api/v1/admin/_middleware.js
+++ pages/api/v1/admin/_middleware.js
@@ -1,35 +1,12 @@
 import { NextResponse } from "next/server";
-import jwt from "@tsndr/cloudflare-worker-jwt";
+import { verify } from "@tsndr/cloudflare-worker-jwt";
 export async function middleware(req, res) {
-    if (req.headers.get("authorization")) {
-        if (req.headers.get("authorization").split(" ")[1].includes("::")) {
-            //is node key
-            console.log(req.headers.get("authorization").split(" ")[1].split("::")[1])
-            try {
-                var allowed = await jwt.verify(req.headers.get("authorization").split(" ")[1].split("::")[1], process.env.ENC_KEY);
-            } catch (error){
-                console.log(error)
-                return NextResponse.redirect("/api/v1/unauthorized");
-            }
-            console.log(allowed)
-            if (!allowed) return NextResponse.redirect("/api/v1/unauthorized");
-        } else if (req.headers.get("authorization").split(" ")[1].includes(":")) {
-            //is api key
-            try {
-                var allowed = await jwt.verify(req.headers.get("authorization").split(" ")[1].split(":")[1], process.env.ENC_KEY);
-            }
-            catch {
-                return NextResponse.redirect("/api/v1/unauthorized");
-            }
-            if (!allowed) return NextResponse.redirect("/api/v1/unauthorized");
-        } else {
-            try {
-                var user_data = await jwt.verify(req.headers.get("authorization").split(" ")[1], process.env.ENC_KEY);
-            } catch (error) {
-                return NextResponse.redirect("/api/v1/unauthorized");
-            }
-            if (!user_data) return NextResponse.redirect("/api/v1/unauthorized");
-        }
-        return NextResponse.next();
-    }
+    if (!req.headers.get("authorization")) return NextResponse.redirect("/api/v1/unauthorized");
+    if (!req.headers.get("authorization").split(" ")[1]) return NextResponse.redirect("/api/v1/unauthorized");
+    let key;
+    if (req.headers.get("authorization").split(" ")[1].includes("::")) key = req.headers.get("authorization").split(" ")[1].split("::")[1];
+    if (req.headers.get("authorization").split(" ")[1].includes(":")) key = req.headers.get("authorization").split(" ")[1].split(":")[1];
+    if (!key) key = req.headers.get("authorization").split(" ")[1];
+    if (!verify(key, process.env.ENC_KEY)) return NextResponse.redirect("/api/v1/unauthorized");
+    return NextResponse.next();
 }
