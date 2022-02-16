dd912af5c46f5c39e3d9d55205c3503b7e294cbc
Joseph Maldjian <josephavm@icloud.com>
2022-02-16 12:44:30 -0600

    Move instance creation to prisma, use new format, and overall clanup

--- pages/api/v1/admin/instances/index.js
+++ pages/api/v1/admin/instances/index.js
@@ -1,75 +1,135 @@
-import { decode } from "jsonwebtoken"
-import { ObjectId } from "mongodb";
-import { connectToDatabase } from "../../../../../util/mongodb";
-import crypto from "crypto";
-import axios from "axios";
+import { createDecipheriv } from "crypto";
+import { post } from "../../../../../lib/requestNode";
+import decodeToken from "../../../../../lib/decodeToken";
+import prisma from "../../../../../lib/prisma";
 
 export default async function handler(req, res) {
-    console.log(req.body)
-    try {
-        var token = decode(req.headers.authorization.split(" ")[1]);
-    } catch {
-        return res.status(403).send("Unauthorized");
-    }
+    const { method, query: { id, include } } = req;
+    const permissions = decodeToken(req.headers["authorization"].split(" ")).permissions
+    switch (method) {
+        case "GET":
 
-    if (!token.admin.instances.write) {
-        return res.status(403).send("Not allowed to access this resource");
-    }
+            break;
+        case "POST":
+            //Validate permissions
+            if (!permissions.includes("create-instance")) return res.status(403).send("Not allowed to access this resource");
 
+            //Validate body
+            if (!req.body.name || !req.body.node || !req.body.devices || !req.body.memory || !req.body.images || !req.body.users) return res.status(400).send("Missing required fields");
 
-    const { db } = await connectToDatabase();
-
-    try {
-        var node = await db.collection("nodes").findOne({
-            _id: ObjectId(req.body.node)
-        })
-    } catch {
-        return res.status(500).send("Internal Server Error");
-    }
-    if (!node) {
-        return res.status(400).send("Node specified does not exist");
-    }
-
-    try {
-        var decipher = crypto.createDecipheriv("aes-256-ctr", process.env.ENC_KEY, Buffer.from(node.access_token_iv, "hex"))
-        var access_token = Buffer.concat([decipher.update(Buffer.from(node.access_token.split("::")[1], "hex")), decipher.final()])
-    } catch {
-        return res.status(500).send("Internal Server Error");
-    }
-
-
-    try {
-        var instance = await db.collection("instances").insertOne({
-            name: req.body.name,
-            node: req.body.node,
-            magma_cube: req.body.magma_cube,
-            limits: req.body.limits,
-            users: req.body.users,
-            environment: req.body.environment,
-            devices: req.body.devices,
-            type: req.body.type,
-        })
-    } catch {
-        return res.status(500).send("Internal Server Error");
-    }
-    var config = {
-        id: instance.insertedId,
-        devices: req.body.devices,
-        limits: req.body.limits,
-        type: req.body.type == "n-vps" ? "container" : "virtual-machine",
-        image: req.body.magma_cube.image
-    }
-    console.log(config)
-    try {
-        axios.post(`${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances`, config, {
-            headers: {
-                "Authorization": `Bearer ${access_token.toString()}`
+            //Insert instance into database
+            let instance;
+            try {
+                instance = await prisma.instance.create({
+                    data: {
+                        name: req.body.name,
+                        cpu: req.body.cpu,
+                        memory: req.body.memory,
+                        cpuPriority: req.body.cpuPriority,
+                        memoryEnforce: req.body.memoryEnforce,
+                        node: {
+                            connect: {
+                                id: req.body.node
+                            }
+                        },
+                        image: {
+                            connect: {
+                                id: req.body.image
+                            }
+                        }
+                    },
+                    include: {
+                        image: true
+                    }
+                })
+            } catch (error) {
+                return res.status(500).send(error);
             }
-        })
-    } catch {
-        return res.status(500).send("Internal Server Error");
+
+            //Add instance devices
+            let devices = {};
+            req.body.devices.forEach(async device => {
+                await prisma.instanceDevice.create({
+                    data: {
+                        instance: {
+                            connect: {
+                                id: instance.id
+                            }
+                        },
+                        name: device.name,
+                        type: device.type,
+                        metadata: device.metadata
+                    }
+                })
+                devices[device.name] = {
+                    type: device.type,
+                    ...device.metadata
+                }
+            })
+
+            //Add instance users
+            req.body.users.forEach(async user => {
+                await prisma.instanceUser.create({
+                    data: {
+                        instance: {
+                            connect: {
+                                id: instance.id
+                            }
+                        },
+                        user: {
+                            connect: {
+                                id: user.id
+                            }
+                        }
+                    }
+                })
+                user.permissions.forEach(async permission => {
+                    await prisma.instanceUserPermission.create({
+                        data: {
+                            permission: permission,
+                            instanceUser: {
+                                connect: {
+                                    id: user.id
+                                }
+                            }
+                        }
+                    })
+                })
+            })
+
+            //Create instance on node
+            const config = {
+                id: instance.id,
+                devices: devices,
+                limits: {
+                    cpu: {
+                        limit: req.body.cpu,
+                        priority: req.body.cpuPriority
+                    },
+                    memory: {
+                        limit: req.body.memory,
+                        enforce: req.body.memoryEnforce
+                    },
+                    disk: {
+                        priority: req.body.diskPriority
+                    }
+                },
+                type: instance.image.type
+            }
+
+            try {
+                await post(node, "/api/v1/instances", config);
+            } catch (error) {
+                await prisma.instance.delete({
+                    where: {
+                        id: instance.id
+                    }
+                })
+                return res.status(500).send("An error occured while creating the instance");
+            }
+            return res.status(202).send("Success");
+            break;
+        default:
+            return res.status(405).send("Method not allowed");
     }
-    return res.status(200).send("Startd creating instance");
-
-
 }
