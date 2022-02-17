4836a414f9b9ce1282831b4f79af08fdbc945eb2
Joseph Maldjian <josephavm@icloud.com>
2022-02-17 11:43:45 -0600

    Move over to prisma and cleanup

--- pages/api/v1/admin/instances/[id]/backups/[backup]/state.js
+++ pages/api/v1/admin/instances/[id]/backups/[backup]/state.js
@@ -1,30 +1,16 @@
-import { verify } from "jsonwebtoken";
-import { connectToDatabase } from "../../../../../../../../util/mongodb";
-import { ObjectId } from "mongodb";
+import prisma from "../../../../../../../../lib/prisma";
 
 export default async function handler(req, res) {
     const { method, query: { id, backup } } = req;
-    if (!req.headers.authorization.includes("::")) {
-        return res.status(403).send("Not allowed to access this resource")
-    }
-    let allowed;
-    try {
-        allowed = verify(req.headers.authorization.split("::")[1], process.env.ENC_KEY);
-    } catch (error) {
-        allowed = false;
-    }
-    if (!allowed) return res.status(403).send("Not allowed to access this resource")
-    const { db } = await connectToDatabase();
-    try {
-        await db.collection("backups").updateOne({
-            _id: ObjectId(backup),
-        }, {
-            $set: { pending: req.body.state }
-        })
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send(error);
-    }
-    return res.status(200).send("Backup State Updated");
+    if (!req.headers.authorization.includes("::")) return res.status(403).send("Not allowed to access this resource")
+    const updatedBackup = await prisma.instanceBackup.update({
+        where: {
+            id: backup
+        },
+        data: {
+            pending: false
+        }
+    })
+    return res.status(200).send(updatedBackup);
 
 }
--- pages/api/v1/admin/instances/[id]/index.js
+++ pages/api/v1/admin/instances/[id]/index.js
@@ -1,8 +1,9 @@
 import { ObjectId } from "mongodb";
-import { connectToDatabase } from "../../../../../../util/mongodb";
 import crypto from "crypto";
 import axios from "axios";
 import prisma from "../../../../../../lib/prisma";
+import decodeToken from "../../../../../../lib/decodeToken";
+import { del } from "../../../../../../lib/requestNode";
 export default async function handler(req, res) {
   const {
     method,
@@ -19,7 +20,21 @@ export default async function handler(req, res) {
         include: {
           backups: permissions.includes("list-backups"),
           snapshots: permissions.includes("list-snapshots"),
-          users: permissions.includes("list-users"),
+          users: {
+            select: {
+              id: permissions.includes("list-users"),
+              user: {
+                select: {
+                  firstName: permissions.includes("list-users"),
+                  lastName: permissions.includes("list-users"),
+                  email: permissions.includes("list-users"),
+                  id: permissions.includes("list-users"),
+                  username: permissions.includes("list-users"),
+                  permissions: permissions.includes("view-user"),
+                }
+              },
+            }
+          },
           node: permissions.includes("view-node"),
           devices: true,
           image: {
@@ -44,93 +59,34 @@ export default async function handler(req, res) {
       return res.status(200).send(instance);
       break;
     case "DELETE":
-      if (!instance) {
-        return res.status(404).send("Instance Does Not Exist");
-      }
+      if (!permissions.includes("delete-instance")) return res.status(403).send("Not allowed to access this resource");
+      const instanceToDelete = await prisma.instance.findUnique({
+        where: {
+          id: id
+        },
+        include: {
+          node: true
+        }
+      })
       try {
-        var node = await db.collection("nodes").findOne({
-          _id: ObjectId(instance.node)
-        });
+        await del(instanceToDelete.node, `/api/v1/instances/${id}`);
       } catch (error) {
-        console.log(error);
-        return res.status(500).send("An error occured while deleting the instance");
+        return res.status(500).send(error);
       }
 
       try {
-        var decipher = crypto.createDecipheriv(
-          "aes-256-ctr",
-          process.env.ENC_KEY,
-          Buffer.from(node.access_token_iv, "hex")
-        );
-        var access_token = Buffer.concat([
-          decipher.update(Buffer.from(node.access_token.split("::")[1], "hex")),
-          decipher.final()
-        ]);
-      } catch (error) {
-        console.log(error);
-        return res.status(500).send("An error occured while deleting the instance");
-      }
-
-      try {
-        await axios.delete(
-          `${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances/${id}`,
-          {
-            headers: {
-              Authorization: `Bearer ${access_token}`
-            }
+        await prisma.instance.delete({
+          where: {
+            id: id
           }
-        );
+        })
       } catch (error) {
-        console.log(error);
-        return res.status(500).send("An error occured while deleting the instance");
+        return res.status(500).send(error)
       }
-
-      await db.collection("instances").deleteOne({
-        _id: ObjectId(id)
-      });
-
-      return res.status(200).send("Success");
+      return res.status(204).send();
       break;
     case "PATCH":
-      if (!instance) {
-        return res.status(404).send("Instance Does Not Exist");
-      }
-      try {
-        var node = await db.collection("nodes").findOne({
-          _id: ObjectId(instance.node)
-        });
-      } catch (error) {
-        console.log(error);
-        return res.status(500).send("An error occured while deleting the instance");
-      }
-
-      try {
-        var decipher = crypto.createDecipheriv(
-          "aes-256-ctr",
-          process.env.ENC_KEY,
-          Buffer.from(node.access_token_iv, "hex")
-        );
-        var access_token = Buffer.concat([
-          decipher.update(Buffer.from(node.access_token.split("::")[1], "hex")),
-          decipher.final()
-        ]);
-      } catch (error) {
-        console.log(error);
-        return res.status(500).send("An error occured while editing the instance");
-      }
-
-      try {
-        node.findOneAndUpdate({
-          _id: ObjectId(instance.node)
-        }, {
-          $set: {
-            name: req.body.name,
-          }
-        });
-      } catch (error) {
-        console.log(error);
-        return res.status(500).send("An error occured while editing the instance");
-      }
+      //Update Instance
       break;
 
     default:
