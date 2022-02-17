245e1bf271f3c1bb965aa30d1bb02d9c499b3103
Joseph Maldjian <josephavm@icloud.com>
2022-02-17 11:45:35 -0600

    add magma cube fetching

---
+++ pages/api/v1/admin/magma_cubes/[id]/index.js
@@ -0,0 +1,38 @@
+import decodeToken from "../../../../../../lib/decodeToken";
+import prisma from "../../../../../../lib/prisma";
+
+export default async function handler(req, res) {
+  const {
+    method,
+    query: { id },
+  } = req;
+  const permissions = decodeToken(req.headers["authorization"].split(" ")[1]).permissions;
+  switch (method) {
+    case "GET":
+      if (!permissions.includes("view-magmaCube")) return res.status(403).send("Not allowed to access this resource");
+      const magmaCube = await prisma.magmaCube.findUnique({
+        where: {
+          id: id
+        },
+        include: {
+          images: {
+            select: {
+              amd64: permissions.includes("view-image"),
+              arm64: permissions.includes("view-image"),
+              console: permissions.includes("view-image"),
+              entrypoint: permissions.includes("view-image"),
+              id: permissions.includes("list-images"),
+              name: permissions.includes("list-images"),
+              stateless: permissions.includes("view-image"),
+              type: permissions.includes("view-image"),
+            }
+          }
+        }
+      })
+      if (!magmaCube) return res.status(403).send("Not allowed to access this resource");
+      return res.status(200).send(magmaCube);
+      break;
+    default:
+      return res.status(400).send("Method not allowed");
+  }
+}
