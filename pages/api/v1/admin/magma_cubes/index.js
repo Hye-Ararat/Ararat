6f77cf36112eadfd4f1e0d353f197aafb6bd8bb0
Joseph Maldjian <josephavm@icloud.com>
2022-02-18 09:02:16 -0600

    Cleanup magma cube creation

--- pages/api/v1/admin/magma_cubes/index.js
+++ pages/api/v1/admin/magma_cubes/index.js
@@ -1,34 +1,14 @@
-import { decode } from "jsonwebtoken"
-import { connectToDatabase } from "../../../../../util/mongodb";
+import decodeToken from "../../../../../lib/decodeToken";
+import prisma from "../../../../../lib/prisma";
+
+
 export default async function handler(req, res) {
-    console.log(req.body)
-    if (req.headers["authorization"].split(" ")[1].includes("::")) {
-        //is node key
-    } else if (req.headers["authorization"].split(" ")[1].includes(":")) {
-        try {
-            var token_data = decode(req.headers["authorization"].split(" ")[1].split(":")[1]);
-        } catch {
-            return res.status(403).send("Not allowed to access this resource");
+    const permissions = decodeToken(req.headers["authorization"].split(" ")[1]).permissions;
+    if (!permissions.includes("create-magmaCube")) return res.status(403).send("Not allowed to access this resource");
+    const cube = await prisma.magmaCube.create({
+        data: {
+            name: req.body.name
         }
-        if (!token_data.admin.magma_cubes.write) {
-            return res.status(403).send("Not allowed to access this resource");
-        }
-    } else {
-        try {
-            var token_data = decode(req.headers["authorization"].split(" ")[1]);
-        } catch {
-            return res.status(403).send("Not allowed to access this resource");
-        }
-        if (!token_data.admin.magma_cubes.write) {
-            return res.status(403).send("Not allowed to access this resource");
-        }
-    }
-    var {db} = await connectToDatabase();
-    try {
-        var cube = await db.collection("magma_cubes").insertOne(req.body);
-    } catch {
-        return res.status(500).send("Internal Server Error");
-    }
-    return res.status(200).send("Cube successfully added");
-    res.send("hello")
+    })
+    return res.status(200).send(cube);
 }
