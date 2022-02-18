e1879436881b0b51406daad75011b858503b06fd
Joseph Maldjian <josephavm@icloud.com>
2022-02-18 13:49:51 -0600

    Clean up access token refreshing

--- pages/api/v1/client/auth/refresh_access_token.js
+++ pages/api/v1/client/auth/refresh_access_token.js
@@ -1,66 +1,53 @@
-import { verify, sign } from "jsonwebtoken";
-import { connectToDatabase } from "../../../../../util/mongodb";
-import { ObjectId } from "mongodb";
+import { verify, sign, decode } from "jsonwebtoken";
+import prisma from "../../../../../lib/prisma";
+
+
 export default async function handler(req, res) {
 	const { method } = req;
 	switch (method) {
 		case "POST":
-			if (!req.body.refresh_token)
-				return res
-					.status(400)
-					.json({ status: "error", data: "Refresh token is required" });
-			try {
-				var refresh_token = verify(
-					req.body.refresh_token,
-					process.env.ENC_KEY
-				);
-			} catch (error){
+			if (!req.body.refresh_token) return res.status(400).json("Refresh token is required");
+			const valid = verify(req.body.refresh_token, process.env.ENC_KEY);
+			if (!valid) {
 				res.setHeader('Set-Cookie', `refresh_token=; Path=/; Expires=${new Date(0).toUTCString()}`);
 				return res.status(403).send("Unauthorized");
 			}
-			if (!refresh_token) {
-			res.setHeader('Set-Cookie', `refresh_token=; Path=/; Expires=${new Date(0).toUTCString()}`);
-				return res.status(403).send("Unauthorized");
-			}
-			var { db } = await connectToDatabase();
-			try {
-				var user_data = await db.collection("users").findOne({
-					_id: ObjectId(refresh_token.user),
-				});
-			} catch {
-				res
-					.status(500)
-					.json({
-						status: "error",
-						data: "An error occured whiel generating the access token",
-					});
-			}
-			if (!user_data)
-				return res.status(403).json({ status: "error", data: "Unauthorized" });
-			let user = {
-				id: user_data._id,
-				username: user_data.username,
-				last_name: user_data.lastname,
-				first_name: user_data.firstname,
-				admin: user_data.admin,
-				email: user_data.email,
-				preferences: user_data.preferences,
-				phone_number: user_data.phone_number,
+
+
+			const userID = decode(req.body.refresh_token).id;
+			const user = await prisma.user.findUnique({
+				where: {
+					id: userID
+				},
+				include: {
+					permissions: {
+						select: {
+							permission: true
+						}
+					}
+				}
+			});
+
+
+			let permissions = [];
+			user.permissions.forEach(async permission => permissions.push(permission.permission));
+
+			const tokenData = {
+				id: user.id,
+				username: user.username,
+				email: user.email,
+				firstName: user.firstName,
+				lastName: user.lastName,
+				permissions: permissions,
 			};
-			let access_token = sign(user, process.env.ENC_KEY, {
+
+			const access_token = sign(tokenData, process.env.ENC_KEY, {
 				algorithm: "HS256",
 				expiresIn: "15m",
 			});
-			return res.json({
-				status: "success",
-				data: {
-					access_token: access_token,
-				},
-			});
+			return res.status(200).send(access_token);
 			break;
 		default:
-			return res
-				.status(405)
-				.json({ status: "error", data: "Method not allowed" });
+			return res.status(405).send("Method not allowed");
 	}
 }
