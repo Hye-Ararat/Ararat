26a22ceba3b16edbcb3486ae9a7ba37c2c6da9d6
Joseph Maldjian <josephavm@icloud.com>
2022-02-18 10:49:35 -0600

    Make compatible with Next.JS 12.1

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,14 +1,13 @@
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
-    var path = req.nextUrl.pathname;
-
+    const path = req.nextUrl.pathname;
     if (path.includes("/api/v1")) return NextResponse.next();
     if (req.cookies.refresh_token && path.includes("/auth")) {
-        return NextResponse.redirect("/");
+        return NextResponse.redirect(new URL("/", req.url));
     }
     if (req.cookies.refresh_token || path.includes("/auth")) {
         return NextResponse.next();
     }
-    return NextResponse.redirect("/auth/login", 307);
+    return NextResponse.redirect(new URL("/auth/login", req.url), 307);
 }  
