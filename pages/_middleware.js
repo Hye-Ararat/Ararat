425d698a699d1ae3320490a2a9949c66bc412a12
Joseph Maldjian <josephavm@icloud.com>
2022-02-18 10:57:32 -0600

    Simplify middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,11 +3,7 @@ import { NextResponse } from "next/server";
 export async function middleware(req) {
     const path = req.nextUrl.pathname;
     if (path.includes("/api/v1")) return NextResponse.next();
-    if (req.cookies.refresh_token && path.includes("/auth")) {
-        return NextResponse.redirect(new URL("/", req.url));
-    }
-    if (req.cookies.refresh_token || path.includes("/auth")) {
-        return NextResponse.next();
-    }
+    if (req.cookies.refresh_token && path.includes("/auth")) return NextResponse.redirect(new URL("/", req.url));
+    if (req.cookies.refresh_token || path.includes("/auth")) return NextResponse.next();
     return NextResponse.redirect(new URL("/auth/login", req.url), 307);
 }  
