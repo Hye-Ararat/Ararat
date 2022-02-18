4c5b81aa0f9bafac35ff08c00baa48177a05f724
Joseph Maldjian <josephavm@icloud.com>
2022-02-18 13:36:09 -0600

    Update _middleware.js

--- pages/api/v1/client/_middleware.js
+++ pages/api/v1/client/_middleware.js
@@ -1,31 +1,15 @@
 import jwt from "@tsndr/cloudflare-worker-jwt";
 import { NextResponse } from "next/server";
 export async function middleware(req, res) {
-	if (
-		req.nextUrl.pathname.includes("/api/v1/client/auth")
-	)
-		return NextResponse.next();
-	if (req.headers.get("authorization")) {
-		if (req.headers.get("authorization").split(" ")[1].includes(":")) {
-			//Is API Key
-		} else if (req.headers.get("authorization").split(" ")[1].includes("::")) {
-			//Is Node API Key
-		} else {
-			try {
-				var user_data = await jwt.verify(
-					req.headers.get("authorization").split(" ")[1],
-					process.env.ENC_KEY
-				);
-			} catch (error) {
-				return NextResponse.redirect("/api/v1/unauthorized");
-			}
-			if (!user_data) return NextResponse.redirect("/api/v1/unauthorized");
-		}
-		return NextResponse.next();
-	} else if (req.nextUrl.searchParams.get("authorization")) {
-		//req.headers.set("authorization", req.nextUrl.searchParams.get("authorization"));
-		req.headers["authorization"] = "Bearer " + req.nextUrl.searchParams.get("authorization")
-		return NextResponse.next();
+	if (req.nextUrl.pathname.includes("/api/v1/client/auth")) return NextResponse.next();
+	if (req.nextUrl.searchParams.get("authorization")) req.headers["authorization"] = "Bearer " + req.nextUrl.searchParams.get("authorization");
+	let key;
+	if (req.headers.get("authorization").split(" ")[1].includes(":")) key = req.headers.get("authorization").split(" ")[1].split(":")[1];
+	if (!key) key = req.headers.get("authorization").split(" ")[1];
+	try {
+		if (! await jwt.verify(key, process.env.ENC_KEY)) return new Response("Unauthorized", { status: 401 });
+	} catch (error) {
+		return new Response("Unauthorized", { status: 401 });
+
 	}
-	return NextResponse.redirect("/api/v1/unauthorized");
 }
