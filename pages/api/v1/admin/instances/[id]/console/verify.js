a835474998a7d2bb39440530151ecde85e302871
Joseph Maldjian <josephavm@icloud.com>
2022-02-18 09:01:19 -0600

    Use predefined paramaters

--- pages/api/v1/admin/instances/[id]/console/verify.js
+++ pages/api/v1/admin/instances/[id]/console/verify.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
     if (!req.body.access_token.includes(":::")) return res.status(403).send("Not allowed to access this resource");
     if (!verify(req.body.access_token.split(":::")[1], process.env.ENC_KEY)) return res.status(403).send("Unauthorized");
     const token_data = decodeToken(req.body.access_token.split(" ")[1]);
-    if (token_data.instance_id != req.query.id) return res.status(403).send("Unauthorized");
+    if (token_data.instance_id != id) return res.status(403).send("Unauthorized");
     if (token_data.type != "console_access_token") return res.status(403).send("Unauthorized");
     return res.status(204).send();
 }
