7d82912c2edc2830715e920b45bc07751a1be91c
Joseph Maldjian <josephavm@icloud.com>
2022-02-22 14:35:04 -0600

    Fix token checking/decoding not woirking for node keys

--- pages/api/v1/admin/_middleware.js
+++ pages/api/v1/admin/_middleware.js
@@ -4,8 +4,8 @@ export async function middleware(req) {
     if (!req.headers.get("authorization")) return new Response("Unauthorized", { status: 401 });
     if (!req.headers.get("authorization").split(" ")[1]) return new Response("Unauthorized", { status: 401 });
     let key;
-    if (req.headers.get("authorization").split(" ")[1].includes("::")) key = req.headers.get("authorization").split(" ")[1].split("::")[1];
-    if (req.headers.get("authorization").split(" ")[1].includes(":")) key = req.headers.get("authorization").split(" ")[1].split(":")[1];
+    if (req.headers.get("authorization").split(" ")[1].includes("::") && !key) key = req.headers.get("authorization").split(" ")[1].split("::")[1];
+    if (req.headers.get("authorization").split(" ")[1].includes(":") && !key) key = req.headers.get("authorization").split(" ")[1].split(":")[1];
     if (!key) key = req.headers.get("authorization").split(" ")[1];
     try {
         if (!await jwt.verify(key, process.env.ENC_KEY)) return new Response("Unauthorized", { status: 401 });
