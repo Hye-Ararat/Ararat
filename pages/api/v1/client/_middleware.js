d38cc3e1eda6fc02fb3d66119e93e3c83a31497e
Joseph Maldjian <josephavm@icloud.com>
2022-02-23 13:28:39 -0600

    verify extra specific-function keys in middleware

--- pages/api/v1/client/_middleware.js
+++ pages/api/v1/client/_middleware.js
@@ -4,7 +4,8 @@ export async function middleware(req, res) {
 	if (req.nextUrl.pathname.includes("/api/v1/client/auth")) return NextResponse.next();
 	if (req.nextUrl.searchParams.get("authorization")) req.headers["authorization"] = "Bearer " + req.nextUrl.searchParams.get("authorization");
 	let key;
-	if (req.headers.get("authorization").split(" ")[1].includes(":")) key = req.headers.get("authorization").split(" ")[1].split(":")[1];
+	if (req.headers.get("authorization").split(" ")[1].includes(":::")) key = req.headers.get("authorization").split(" ")[1].split(":::")[1];
+	if (req.headers.get("authorization").split(" ")[1].includes(":") && !key) key = req.headers.get("authorization").split(" ")[1].split(":")[1];
 	if (!key) key = req.headers.get("authorization").split(" ")[1];
 	try {
 		if (!await jwt.verify(key, process.env.ENC_KEY)) return new Response("Unauthorized", { status: 401 });
