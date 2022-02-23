899819af1d0020b106736f2a0f12a998a006e5d6
Joseph Maldjian <josephavm@icloud.com>
2022-02-23 10:54:28 -0600

    Move backup downloading to prisma and cleanup code

--- pages/api/v1/client/instances/[id]/backups/[backup]/download.js
+++ pages/api/v1/client/instances/[id]/backups/[backup]/download.js
@@ -1,86 +1,59 @@
-import { verify } from "jsonwebtoken";
-import { ObjectId } from "mongodb";
-import { connectToDatabase } from "../../../../../../../../util/mongodb";
+import getInstancePermissions from "../../../../../../../../lib/client/getInstancePermissions";
+import decodeToken from "../../../../../../../../lib/decodeToken";
+import getNodeAccessToken from "../../../../../../../../lib/getNodeAccessToken";
+import prisma from "../../../../../../../../lib/prisma";
 import axios from "axios";
-import crypto from "crypto"
 
 export default async function handler(req, res) {
-    const { method, query: { id, backup } } = req;
-    console.log(req.headers)
-    if (req.query.authorization) {
-        req.headers["authorization"] = "Bearer " + req.query.authorization
-    }
-    let user;
-    try {
-        user = verify(req.headers.authorization.split(" ")[1], process.env.ENC_KEY);
-    } catch (error) {
-        return res.status(401).send("Unauthorized");
-    }
-    if (!user) return res.status(401).send("Unauthorized");
+    const { query: { id, backup } } = req;
+    const tokenData = decodeToken(req.headers["authorization"].split(" ")[1]);
 
-    const { db } = await connectToDatabase();
+    const backupToDownload = await prisma.instanceBackup.findUnique({
+        where: {
+            id: backup
+        },
+        include: {
+            instance: {
+                select: {
+                    users: {
+                        select: {
+                            id: true,
+                            permissions: true,
+                            user: {
+                                select: {
+                                    id: true
+                                }
+                            }
+                        }
+                    },
+                    node: true
+                }
+            }
+        }
+    });
+    if (!backupToDownload) return res.status(404).send("Backup not found");
 
-    let instance;
-    let node;
-    let backupData;
+    const permissions = getInstancePermissions(tokenData.id, backupToDownload.instance);
 
-    try {
-        instance = await db.collection("instances").findOne({
-            _id: ObjectId(id),
-            [`users.${user.id}`]: { $exists: true }
-        })
-    } catch (error) {
-        return res.status(500).send(error);
-    }
+    if (!permissions.includes("download-backup")) return res.status(403).send("Not allowed to access this resource");
 
-    if (!instance) return res.status(404).send("Instance not found");
-
-    try {
-        backupData = await db.collection("backups").findOne({
-            _id: ObjectId(backup),
-            instance: instance._id.toHexString()
-        })
-    } catch (error) {
-        return res.status(500).send(error);
-    }
-
-    if (!backupData) return res.status(404).send("Backup not found");
-
-    try {
-        node = await db.collection("nodes").findOne({
-            _id: ObjectId(instance.node)
-        })
-    } catch (error) {
-        return res.status(500).send(error);
-    }
-
-    if (!node) return res.status(404).send("Node not found");
-
+    const accessToken = getNodeAccessToken(backupToDownload.instance.node.accessTokenIV, backupToDownload.instance.node.accessToken);
 
     let download;
-    let access_token;
-
-    try {
-        const decipher = crypto.createDecipheriv("aes-256-ctr", process.env.ENC_KEY, Buffer.from(node.access_token_iv, "hex"))
-        access_token = Buffer.concat([decipher.update(Buffer.from(node.access_token.split("::")[1], "hex")), decipher.final()])
-
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send(error);
-    }
-
     try {
         download = await axios({
-            url: `${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances/${id}/backups/${backupData._id.toHexString()}`, responseType: "stream", method: "get", headers: {
-                Authorization: `Bearer ${access_token.toString()}`
+            url: `${backupToDownload.instance.node.ssl ? "https://" : "http://"}${backupToDownload.instance.node.hostname}:${backupToDownload.instance.node.port}/api/v1/instances/${id}/backups/${backup}`, responseType: "stream", method: "get", headers: {
+                Authorization: `Bearer ${accessToken}`
             }
         })
-    } catch (error) {
-        return res.status(500).send(error);
+    } catch {
+        return res.status(500).send("Internal Server Error");
     }
+
     res.writeHead(200, {
         "Content-Type": "application/octet-stream",
-        "Content-Disposition": `attachment; filename="${backupData.name}.tar.gz"`,
-    })
+        "Content-Disposition": `attachment; filename="${backupToDownload.name}.tar.gz"`,
+    });
+
     return download.data.pipe(res);
 }
