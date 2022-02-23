57d75ae5ac575fc677d7da6d2d11304fe843799d
Joseph Maldjian <josephavm@icloud.com>
2022-02-23 13:33:49 -0600

    cleanup monitor token verification

--- pages/api/v1/client/instances/[id]/monitor/verify.js
+++ pages/api/v1/client/instances/[id]/monitor/verify.js
@@ -1,63 +1,9 @@
-import { connectToDatabase } from "../../../../../../../util/mongodb";
-import { verify } from "jsonwebtoken";
 import bcrypt from "bcryptjs";
+import decodeToken from "../../../../../../../lib/decodeToken";
 
 export default async function handler(req, res) {
-  const {
-    query: { id },
-    method,
-  } = req;
-  switch (method) {
-    case "POST":
-      let { db } = await connectToDatabase();
-      if (!req.body.access_token)
-        return res
-          .status(400)
-          .send({ status: "error", data: "Access Token is required" });
-      if (!req.body.access_token.includes(":::"))
-        return res.status(403).send({ status: "error", data: "Unauthorized" });
-      try {
-        var token_data = await verify(
-          req.body.access_token.split(":::")[1],
-          process.env.ENC_KEY
-        );
-      } catch (error) {
-        return res.status(403).send({ status: "error", data: "Unauthorized" });
-      }
-      if (token_data.instance_id != id)
-        return res.status(403).send({ status: "error", data: "Unauthorized" });
-      if (token_data.type != "monitor_access_token")
-        return res.status(403).send({ status: "error", data: "Unauthorized" });
-      var sessions = await db
-        .collection("sessions")
-        .find({
-          instance_id: id,
-          type: "monitor_access_token",
-        })
-        .toArray();
-      let match = false;
-      async function run() {
-        return new Promise((resolve, reject) => {
-          sessions.forEach(async (session) => {
-            if (!match) {
-              let is_match = await bcrypt.compare(
-                req.body.access_token,
-                session.access_token
-              );
-              if (is_match) {
-                match = true;
-                return resolve(true);
-              }
-            }
-          });
-        });
-      }
-      await run();
-      if (!match)
-        return res.status(403).send({ status: "error", data: "Unauthorized" });
-      return res.json({ status: "success", data: "Access Token Is Valid" });
-      break;
-    default:
-      res.status(400).json({ status: "error", data: "Method not allowed" });
-  }
+  const { query: { id } } = req;
+  const tokenData = decodeToken(req.headers["authorization"].split(" ")[1]);
+  if (tokenData.type != "monitor" || tokenData.instance != id) return res.status(403).send("Not allowed to access this resource");
+  return res.status(204).send();
 }
