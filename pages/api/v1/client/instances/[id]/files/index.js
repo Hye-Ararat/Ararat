34663ae499a604ee3c942defa96232b0bc9c283d
Joseph Maldjian <josephavm@icloud.com>
2022-02-23 13:25:09 -0600

    Move to prisma and cleanup code

--- pages/api/v1/client/instances/[id]/files/index.js
+++ pages/api/v1/client/instances/[id]/files/index.js
@@ -1,59 +1,51 @@
-import axios, { get, post } from "axios"
 import { decode, verify } from "jsonwebtoken"
 import { connectToDatabase } from "../../../../../../../util/mongodb";
 import { ObjectId } from "mongodb";
 import crypto from "crypto"
+import decodeToken from "../../../../../../../lib/decodeToken";
+import prisma from "../../../../../../../lib/prisma";
+import getInstancePermissions from "../../../../../../../lib/client/getInstancePermissions";
+import { del, get, post } from "../../../../../../../lib/requestNode";
 export default async function handler(req, res) {
     const { query: { id, path }, method } = req;
+    const tokenData = decodeToken(req.headers["authorization"].split(" ")[1]);
+
+    const instance = await prisma.instance.findUnique({
+        where: {
+            id: id
+        },
+        include: {
+            users: {
+                select: {
+                    permissions: true,
+                    user: {
+                        select: {
+                            id: true,
+                        }
+                    }
+                }
+            },
+            node: true
+        }
+    });
+    if (!instance) return res.status(404).send("Instance not found");
+
+    const permissions = getInstancePermissions(tokenData.id, instance);
+
     switch (method) {
         case "GET":
-            console.log(id);
+            if (!permissions.includes("list-files") && !permissions.includes("view-file")) return res.status(403).send("Not allowed to access this resource");
+
+            let files;
             try {
-                var user = decode(req.headers.authorization.split(" ")[1])
+                files = await get(instance.node, `/api/v1/instances/${instance.id}/files?path=${path}`);
             } catch {
-                return res.status(403).send("Unauthorized");
+                return res.status(500).send("Internal Server Error");
             }
-            var { db } = await connectToDatabase();
+
+            let sorted;
             try {
-                var instance = await db.collection("instances").findOne({
-                    _id: ObjectId(id),
-                    [`users.${user.id}`]: { $exists: true }
-                })
-            } catch (error) {
-                console.log(error)
-                console.log("error 1 oooh its shit fdksj;asldkjfakjsdl")
-                return res.status(500).send("An error occured while fetching the instance data")
-            }
-            if (!instance) {
-                return res.status(404).send("Instance does not exist")
-            }
-            try {
-                var node = await db.collection("nodes").findOne({
-                    _id: ObjectId(instance.node)
-                })
-            } catch {
-                return res.status(500).send("An error occured while fetching the node data");
-            }
-            if (!node) {
-                return res.status(500).send("Node associated with server does not exist")
-            }
-            try {
-                var decipher = crypto.createDecipheriv("aes-256-ctr", process.env.ENC_KEY, Buffer.from(node.access_token_iv, "hex"))
-                var access_token = Buffer.concat([decipher.update(Buffer.from(node.access_token.split("::")[1], "hex")), decipher.final()])
-                console.log(access_token.toString())
-                console.log(`${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances/${id}/files?path=${path}`)
-                var files = await get(`${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances/${id}/files?path=/${path}`, {
-                    headers: {
-                        Authorization: `Bearer ${access_token.toString()}`
-                    }
-                })
-            } catch (error) {
-                console.log(error)
-                console.log("error 2")
-                return res.status(500).send("An error occured while fetching the files")
-            }
-            try {
-                var sorted = files.data.list.sort((a, b) => {
+                sorted = files.data.list.sort((a, b) => {
                     if (a.type == "directory" && b.type == "file") {
                         return -1
                     } else if (a.type == "file" && b.type == "directory") {
@@ -73,116 +65,37 @@ export default async function handler(req, res) {
                 res.headers = {
                     "Content-Type": "text/plain"
                 }
+
+                if (!permissions.includes("view-file")) return res.status(403).send("Not allowed to access this resource");
+
                 return res.send(JSON.stringify(files.data))
             }
+
+            if (!permissions.includes("list-files")) return res.status(403).send("Not allowed to access this resource");
+
             files.data.list = sorted;
             return res.send(files.data)
             break;
         case "POST":
-            console.log(id);
+            if (!permissions.includes("create-file")) return res.status(403).send("Not allowed to access this resource");
+
             try {
-                var user = decode(req.headers.authorization.split(" ")[1])
+                await post(instance.node, `/api/v1/instances/${id}/files?path=${path}`, req.body)
             } catch {
-                return res.status(403).send("Unauthorized");
+                return res.status(500).send("Internal Server Error")
             }
-            var { db } = await connectToDatabase();
-            try {
-                var instance = await db.collection("instances").findOne({
-                    _id: ObjectId(id),
-                    [`users.${user.id}`]: { $exists: true }
-                })
-            } catch (error) {
-                console.log(error)
-                console.log("error 1 oooh its shit fdksj;asldkjfakjsdl")
-                return res.status(500).send("An error occured while fetching the instance data")
-            }
-            if (!instance) {
-                return res.status(404).send("Instance does not exist")
-            }
-            try {
-                var node = await db.collection("nodes").findOne({
-                    _id: ObjectId(instance.node)
-                })
-            } catch {
-                return res.status(500).send("An error occured while fetching the node data");
-            }
-            if (!node) {
-                return res.status(500).send("Node associated with server does not exist")
-            }
-            try {
-                var decipher = crypto.createDecipheriv("aes-256-ctr", process.env.ENC_KEY, Buffer.from(node.access_token_iv, "hex"))
-                var access_token = Buffer.concat([decipher.update(Buffer.from(node.access_token.split("::")[1], "hex")), decipher.final()])
-                console.log(access_token.toString())
-                console.log(`${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances/${id}/files?path=${path}`)
-                if (!req.body.fs_type) {
-                    var files = await post(`${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances/${id}/files?path=/${path}`, req.body, {
-                        headers: {
-                            Authorization: `Bearer ${access_token.toString()}`,
-                            "Content-Type": "text/plain"
-                        }
-                    })
-                } else {
-                    var folder = await axios.patch(`${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances/${id}/files?path=/${path}`, null, {
-                        headers: {
-                            Authorization: `Bearer ${access_token.toString()}`,
-                        }
-                    })
-                }
-            } catch (error) {
-                console.log(error)
-                console.log("error 2")
-                return res.status(500).send("An error occured while uplooading")
-            }
-            return res.send("Success")
+            return res.status(204).send();
             break;
         case "DELETE":
-            var user;
-            try {
-                user = verify(req.headers.authorization.split(" ")[1], process.env.ENC_KEY)
-            } catch (error) {
-                return res.status(403).send("Unauthorized");
-            }
-            var { db } = await connectToDatabase();
-
-            var instance;
-            try {
-                instance = await db.collection("instances").findOne({
-                    _id: ObjectId(id),
-                    [`users.${user.id}`]: { $exists: true }
-                })
-            } catch (error) {
-                return res.status(500).send("An error occured while fetching the instance data")
-            }
-
-            if (!instance) return res.status(404).send("Instance does not exist");
-
-            var node;
-            try {
-                node = await db.collection("nodes").findOne({
-                    _id: ObjectId(instance.node)
-                })
-            } catch (error) {
-                return res.status(500).send("An error occured while fetching the node data");
-            }
-
-            if (!node) return res.status(500).send("Node associated with server does not exist");
+            if (!permissions.includes("delete-files")) return res.status(403).send("Not allowed to access this resource");
 
             try {
-                var decipher = crypto.createDecipheriv("aes-256-ctr", process.env.ENC_KEY, Buffer.from(node.access_token_iv, "hex"))
-                var access_token = Buffer.concat([decipher.update(Buffer.from(node.access_token.split("::")[1], "hex")), decipher.final()])
-            } catch (error) {
-                return res.status(500).send("An error occured while deleting the file")
+                await del(instance.node, `/api/v1/instances/${id}/files?path=${path}`);
+            } catch {
+                return res.status(500).send("Internal Server Error");
             }
-            try {
-                await axios.delete(`${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances/${id}/files?path=/${path}`, {
-                    headers: {
-                        Authorization: `Bearer ${access_token.toString()}`
-                    }
-                }
-                )
-            } catch (error) {
-                return res.status(500).send("An error occured while deleting the file")
-            }
+            return res.status(204).send();
+
             break;
         default:
             return res.status(405).send("Method not allowed")
