554faefc1325573bd1cb8e81f36a641661cbf7fd
Joseph Maldjian <josephavm@icloud.com>
2022-02-25 10:46:30 -0600

    move to prisma and cleanup code

--- pages/api/v1/client/instances/[id]/state/index.js
+++ pages/api/v1/client/instances/[id]/state/index.js
@@ -1,79 +1,51 @@
-const { post, get } = require("axios");
-import { connectToDatabase } from "../../../../../../../util/mongodb"
-import { ObjectId } from "mongodb";
-import { decode } from "jsonwebtoken";
-import crypto from "crypto";
+import decodeToken from "../../../../../../../lib/decodeToken";
+import prisma from "../../../../../../../lib/prisma";
+import { get, post } from "../../../../../../../lib/requestNode";
+
 export default async function handler(req, res) {
-    const { query: { id }, method } = req;
-    var { db } = await connectToDatabase();
+    const { query: { id }, body: { state } } = req;
+
+    const tokenData = decodeToken(req.headers["authorization"].split(" ")[1]);
+
+    const instance = await prisma.instance.findUnique({
+        where: {
+            id: id
+        },
+        include: {
+            node: true,
+            users: {
+                select: {
+                    permissions: true,
+                    user: {
+                        select: {
+                            id: true,
+                        }
+                    }
+                }
+            }
+        }
+    })
+
+    if (!instance) return res.status(404).send("Instance not found");
+
+    const permissions = getInstancePermissions(tokenData.id, instance);
+
+    if (!state) return res.status(400).send("State is required");
+    if (state != "start" || state != "stop" || state != "kill" || state != "restart") return res.status(400).send("Invalid State");
+
+    if (state == "start" && !permissions.includes("start-instance")) return res.status(403).send("Not allowed to access this resource");
+    if (state == "stop" && !permissions.includes("stop-instance")) return res.status(403).send("Not allowed to access this resource");
+    if (state == "kill" && !permissions.includes("kill-instance")) return res.status(403).send("Not allowed to access this resource");
+    if (state == "restart" && !permissions.includes("restart-instance")) return res.status(403).send("Not allowed to access this resource");
+
     try {
-        var user = decode(req.headers.authorization.split(" ")[1])
+        await post(instance.node, `/api/v1/instances/${id}/state`, { state });
     } catch {
-        return res.status(403).send("Unauthorized");
-    }
-    console.log(user)
-    console.log(id)
-    try {
-        var instance = await db.collection("instances").findOne({
-            _id: ObjectId(id),
-            [`users.${user.id}`]: { $exists: true }
-        })
-    } catch (error) {
-        console.log(error)
-        console.log("error 1")
-        return res.status(500).json(
-            "An error occured while sending the state action")
-    }
-    if (!instance) {
-        return res.status(404).send("Instance does not exist")
-    }
-    try {
-        var node = await db.collection("nodes").findOne({
-            _id: ObjectId(instance.node)
-        })
-    } catch {
-        return res.status(500).send("An error occured while fetching the node data");
-    }
-    if (!node) {
-        return res.status(500).send("Node associated with server does not exist")
+        return res.status(500).send("Internal Server Error");
     }
 
-    switch (method) {
-        case "GET":
-            try {
-                var decipher = crypto.createDecipheriv("aes-256-ctr", process.env.ENC_KEY, Buffer.from(node.access_token_iv, "hex"))
-                var access_token = Buffer.concat([decipher.update(Buffer.from(node.access_token.split("::")[1], "hex")), decipher.final()])
-                var state = await post(`${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances/${id}/state`, req.body, {
-                    headers: {
-                        Authorization: `Bearer ${access_token.toString()}`
-                    }
-                })
-            } catch (error) {
-                console.log(error)
-                console.log("error 2")
-                return res.status(500).send("An error occured while sending the power action")
-            }
-            return res.send("Success")
-            break;
-        case "POST":
-            try {
-                var decipher = crypto.createDecipheriv("aes-256-ctr", process.env.ENC_KEY, Buffer.from(node.access_token_iv, "hex"))
-                var access_token = Buffer.concat([decipher.update(Buffer.from(node.access_token.split("::")[1], "hex")), decipher.final()])
-                var state = await post(`${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances/${id}/state`, req.body, {
-                    headers: {
-                        Authorization: `Bearer ${access_token.toString()}`
-                    }
-                })
-            } catch (error) {
-                console.log(error)
-                console.log("error 2")
-                return res.status(500).send("An error occured while fetching the power state")
-            }
-            return res.send(state.data)
-            break;
-        default:
-            return res.status(405).send("Method not allowed")
-            break;
-    }
-
+    let instanceState;
+    instanceState = await get(instance.node, `/api/v1/instances/${id}/state`).catch(() => instanceState = null);
+    if (instanceState) return res.status(200).send(instanceState);
+    if (!instanceState) return res.status(204).send();
 }
