6284181db39b25b0ba2cb98144704a74df34ef52
Joseph Maldjian <josephavm@icloud.com>
2022-02-25 10:50:20 -0600

    Fix bug where this endpoint finds nothing

--- pages/api/v1/client/instances/[id]/index.js
+++ pages/api/v1/client/instances/[id]/index.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
 
     const instance = await prisma.instance.findUnique({
         where: {
-            id: userData.id,
+            id: id,
         },
         include: {
             users: {
