9403b9159701539a3b3af604d1f18b19930ceb6c
Joseph Maldjian <josephavm@icloud.com>
2022-02-25 10:29:28 -0600

    Move to prisma and cleanup code

--- pages/api/v1/client/instances/[id]/snapshots/[snapshot]/index.js
+++ pages/api/v1/client/instances/[id]/snapshots/[snapshot]/index.js
@@ -1,76 +1,52 @@
-import { verify } from "jsonwebtoken";
-import { connectToDatabase } from "../../../../../../../../util/mongodb";
-import crypto from "crypto"
-import axios from "axios";
-import { ObjectId } from "mongodb";
+import decodeToken from "../../../../../../../../lib/decodeToken";
+import prisma from "../../../../../../../../lib/prisma";
+import getInstancePermissions from "../../../../../../../../lib/client/getInstancePermissions";
+import { del } from "../../../../../../../../lib/requestNode";
+
 export default async function handler(req, res) {
-    const { method, query: { id, snapshot } } = req;
-    console.log("a;ldfkjs;ldfj;asldkfjalsdkfjk")
-    let user;
-    try {
-        user = verify(req.headers.authorization.split(" ")[1], process.env.ENC_KEY);
-    } catch (error) {
-        console.log("error")
-        return res.status(401).send("Unauthorized");
-    }
+    const { query: { id, snapshot } } = req;
+    const tokenData = decodeToken(req.headers["authorization"].split(" ")[1]);
 
-    if (!user) return res.status(401).send("Unauthorized");
-
-    const { db } = await connectToDatabase();
-
-    let instance;
-    let node;
-
-    try {
-        instance = await db.collection("instances").findOne({
-            _id: ObjectId(id),
-            [`users.${user.id}`]: { $exists: true }
-        })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).send(error);
-    }
-
-    if (!instance) return res.status(404).send("Instance not found");
-
-    try {
-        node = await db.collection("nodes").findOne({
-            _id: ObjectId(instance.node)
-        })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).send(error);
-    }
-
-    if (!node) return res.status(404).send("Node not found");
-
-    let access_token;
-    try {
-        const decipher = crypto.createDecipheriv("aes-256-ctr", process.env.ENC_KEY, Buffer.from(node.access_token_iv, "hex"));
-        access_token = Buffer.concat([decipher.update(Buffer.from(node.access_token.split("::")[1], "hex")), decipher.final()]);
-    } catch (error) {
-        console.log(error)
-        return res.status(500).send(error);
-    }
-
-    try {
-        await axios.delete(`${node.address.ssl ? "https://" : "http://"}${node.address.hostname}:${node.address.port}/api/v1/instances/${id}/snapshots/${snapshot}`, {
-            headers: {
-                "Authorization": `Bearer ${access_token.toString()}`
+    const snapshotToDelete = await prisma.instanceSnapshot.findUnique({
+        where: {
+            id: snapshot
+        },
+        include: {
+            instance: {
+                select: {
+                    users: {
+                        select: {
+                            permissions: true,
+                            user: {
+                                select: {
+                                    id: true
+                                }
+                            }
+                        }
+                    },
+                    node: true
+                },
             }
-        })
-    } catch (error) {
-        console.log("error")
-        return res.status(500).send(error);
-    }
+        }
+    })
+
+    if (!snapshotToDelete) return res.status(404).send("Snapshot not found");
+
+    const permissions = getInstancePermissions(tokenData.id, snapshotToDelete.instance);
+
+    if (!permissions.includes("delete-snapshot")) return res.status(403).send("Not allowed to access this resource");
 
     try {
-        await db.collection("snapshots").deleteOne({
-            _id: ObjectId(snapshot)
-        })
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send(error);
+        await del(snapshotToDelete.instance.node, `/api/v1/instances/${id}/snapshots/${snapshot}`);
+    } catch {
+        return res.status(403).send("Not allowed to access this resource");
     }
-    return res.status(200).send("Success");
+
+    await prisma.instanceSnapshot.delete({
+        where: {
+            id: snapshot
+        }
+    })
+
+    return res.status(204).send();
 }
