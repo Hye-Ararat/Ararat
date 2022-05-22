ca9368f7384f4aee6928892767f29a9e64258db3
Joseph Maldjian <josephavm@icloud.com>
2022-05-22 17:52:48 -0500

    Add Footer

---
+++ components/footer.js
@@ -0,0 +1,5 @@
+export default function Footer() {
+    return (
+        <div style={{ textAlign: "center", marginTop: 10 }}><a href="https://hye.gg" style={{ color: "grey" }}>&copy; 2022 Hye Hosting LLC</a></div>
+    )
+}
