6cfd0ec0a514f187aec83ecf16fa665a06a7db10
Joseph Maldjian <josephavm@icloud.com>
2022-05-22 18:00:08 -0500

    New Node Creation Wizard

---
+++ components/nodes/CreateNode.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import SelectHost from "./create/host";
+import Install from "./create/install";
+import NodeInfo from "./create/nodeInfo";
+import SSL from "./create/ssl";
+
+export default function CreateNode() {
+    const [page, setPage] = useState("host");
+    const [nodeName, setNodeName] = useState(null);
+    const [nodeAddress, setNodeAddress] = useState(null);
+    const [nodePort, setNodePort] = useState(3000);
+    const [lxdPort, setLxdPort] = useState(8443);
+    const [sftpPort, setSftpPort] = useState(2222);
+    const [sslCertPath, setSslCertPath] = useState(null);
+    const [sslKeyPath, setSslKeyPath] = useState(null);
+
+    return (
+        <>
+            {page == "host" ?
+                <SelectHost setPage={setPage} />
+                : ""}
+            {page == "nodeInfo" ?
+                <NodeInfo setPage={setPage} nodeName={nodeName} setNodeName={setNodeName} nodeAddress={nodeAddress} setNodeAddress={setNodeAddress} nodePort={nodePort} setNodePort={setNodePort} lxdPort={lxdPort} setLxdPort={setLxdPort} sftpPort={sftpPort} setSftpPort={setSftpPort} />
+                : ""}
+            {page == "SSL" ?
+                <>
+                    <SSL setPage={setPage} sslKeyPath={sslKeyPath} setSslKeyPath={setSslKeyPath} sslCertPath={sslCertPath} setSslCertPath={setSslCertPath} />
+                </>
+                : ""}
+            {page == "install" ?
+                <Install sftpPort={sftpPort} nodeAddress={nodeAddress} lxdPort={lxdPort} nodePort={nodePort} setPage={setPage} sslCertPath={sslCertPath} sslKeyPath={sslKeyPath} nodeName={nodeName} />
+                : ""}
+        </>
+    )
+
+}
---
+++ components/nodes/create/host.js
@@ -0,0 +1,90 @@
+import { Dialog, Grid, DialogTitle, DialogContent, DividerPropsVariantOverrides, Typography, Divider, Modal, Box, Button } from "@mui/material"
+import { Cloud, Nightlight, CloudOutlined, CorporateFare, Security, SupportAgent, SignalCellularAlt } from "@mui/icons-material"
+import { useState } from "react";
+export default function SelectHost({ setPage }) {
+    const [hovering, setHovering] = useState(false);
+    const [sure, setSure] = useState(false);
+
+    return (
+        <>
+            <Modal open={sure}>
+                <Box sx={{
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    width: 550,
+                    bgcolor: 'background.paper',
+                    border: '2px solid #000',
+                    boxShadow: 24,
+                    p: 4,
+                    borderRadius: 3
+                }}>
+                    <Grid container sx={{ mt: 1 }}>
+                        <img src="/logo.png" width="120px" style={{ marginLeft: "auto", marginRight: 10 }} />
+                        <Typography variant="h3" sx={{ mr: "auto", mb: 1, fontWeight: "bold", fontFamily: "Poppins" }}>Cloud</Typography>
+                    </Grid>
+                    <Grid container>
+                        <Typography sx={{ mr: "auto", ml: "auto" }}>Are you sure you want to miss out?</Typography>
+                    </Grid>
+                    <Divider sx={{ mt: 2, mb: 2 }} />
+                    <Grid container direction="row">
+                        <Grid container direction="column" sx={{ maxWidth: "33%" }}>
+                            <Security sx={{ fontSize: "40px", mr: "auto", ml: "auto" }} />
+                            <Typography variant="h6" align="center">Anti-DDoS</Typography>
+                        </Grid>
+                        <Grid container direction="column" sx={{ maxWidth: "33%" }}>
+                            <SupportAgent sx={{ fontSize: "40px", mr: "auto", ml: "auto" }} />
+                            <Typography variant="h6" align="center">24/7 Support</Typography>
+                        </Grid>
+                        <Grid container direction="column" sx={{ maxWidth: "33%" }}>
+                            <SignalCellularAlt sx={{ fontSize: "40px", mr: "auto", ml: "auto" }} />
+                            <Typography variant="h6" align="center">99.9% Uptime</Typography>
+                        </Grid>
+
+                    </Grid>
+                    <Grid container direction="column" sx={{ width: "300px", mr: "auto", ml: "auto", mt: 2 }}>
+                        <Button variant="contained" color="info" sx={{ mt: 2 }} onClick={() => {
+                            setSure(false);
+                            setPage("cloudPlan");
+                        }} autoFocus>Use Hye Cloud</Button>
+                        <Button sx={{ mt: 1, color: "gray" }} onClick={() => {
+                            setSure(false);
+                            setPage("nodeInfo")
+                        }}>Miss Out</Button>
+                    </Grid>
+                </Box>
+            </Modal>
+            <Dialog open={true}>
+                <Grid container>
+                    <DialogTitle sx={{ mr: "auto", ml: "auto" }}>Where do you want to host this node?</DialogTitle>
+                </Grid>
+                <div onMouseLeave={() => setHovering(false)} onMouseOver={() => setHovering(true)} onClick={() => {
+                    setPage("cloudPlan");
+                }} style={{ cursor: "pointer" }}>
+                    <Grid container sx={{ backgroundImage: hovering ? "linear-gradient(transparent, rgba(255, 255, 255, 0.15), transparent)" : "linear-gradient(transparent, rgba(255, 255, 255, 0.12), transparent)", p: 3 }} direction="column">
+                        <Grid container direction="row" sx={{ maxWidth: "30px" }}>
+                            <Cloud sx={{ fontSize: "60px", zIndex: "30", mt: 2 }} />
+                            <Nightlight sx={{ fontSize: "60px", color: "#ffb800", position: "absolute", ml: 3, zIndex: "1", rotate: "-30deg" }} />
+                            <Cloud sx={{ fontSize: "30px", position: "absolute", ml: 8, mt: 3 }} />
+                        </Grid>
+                        <Typography align="center" fontWeight={500} variant="h6" sx={{ position: "absolute", left: "38.5%", bottom: "56%", fontFamily: "Poppins", fontWeight: "bold" }}>Hye Cloud</Typography>
+                        <CloudOutlined sx={{ position: "absolute", left: "30%", bottom: "39.5%", fontSize: "180px", color: "transparent", fill: "white" }} />
+                        <Cloud sx={{ fontSize: "65px", position: "absolute", ml: 0, mb: 5, right: "15%", top: "13%" }} />
+                        <Cloud sx={{ fontSize: "85px", zIndex: "30", position: "absolute", right: "2%", top: "30%" }} />
+                    </Grid>
+                </div>
+
+                <DialogContent>
+                    <Divider sx={{ m: 1 }} />
+                    <Grid container direction="column" onClick={() => {
+                        setSure(true);
+                    }} sx={{ cursor: "pointer" }}>
+                        <Typography fontWeight={500} variant="h6" sx={{ mr: "auto", ml: "auto", fontFamily: "Poppins" }}>My Own Infrastructure</Typography>
+                        <CorporateFare sx={{ fontSize: "70px", mr: "auto", ml: "auto" }} />
+                    </Grid>
+                </DialogContent>
+            </Dialog >
+        </>
+    )
+}
