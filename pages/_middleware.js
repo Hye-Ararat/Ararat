6beb59f1506d8230233c62beb66f062a299cf20a
Joseph Maldjian <josephavm@icloud.com>
2022-05-22 18:04:19 -0500

    Allow LXD authorization bypass

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,6 +4,6 @@ export async function middleware(req) {
     const path = req.nextUrl.pathname;
     if (path.includes("/api/v1")) return NextResponse.next();
     if (req.cookies.refresh_token && path.includes("/auth")) return NextResponse.redirect(new URL("/", req.url));
-    if (req.cookies.refresh_token || path.includes("/auth")) return NextResponse.next();
+    if (req.cookies.refresh_token || path.includes("/auth") || path.includes("lxd-a")) return NextResponse.next();
     return NextResponse.redirect(new URL("/auth/login", req.url), 307);
 }  
