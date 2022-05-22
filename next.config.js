df0fd077e9efff5fc49c5434006fb298f871e7a2
Joseph Maldjian <josephavm@icloud.com>
2022-05-22 18:03:18 -0500

    WASM compat

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, { dev }) => {
+    return {
+      ...config,
+      experiments: {
+        asyncWebAssembly: true,
+        layers: true
+      }
+    }
+  }
 }
