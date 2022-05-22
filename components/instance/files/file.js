5180691ac076f975cffc15a895ec6aa1c42c7da3
Joseph Maldjian <josephavm@icloud.com>
2022-05-22 17:53:47 -0500

    Optimize for mobile and add additional information

--- components/instance/files/file.js
+++ components/instance/files/file.js
@@ -1,9 +1,10 @@
 import { faFile, faFolder } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Grid, Typography, Checkbox, Menu, MenuItem } from "@mui/material"
+import { Grid, Typography, Checkbox, Menu, MenuItem, Fade, useMediaQuery, useTheme } from "@mui/material"
 import prettyBytes from "pretty-bytes"
 import { Fragment, useState } from "react";
 import { MoreHoriz } from "@mui/icons-material";
+import moment from "moment";
 
 
 export default function File({ file, allChecked }) {
@@ -22,22 +23,29 @@ export default function File({ file, allChecked }) {
 
     return (
         <>
-            <Grid item container xs={.6}>
-                <Checkbox checked={checked || allChecked} id={file.name} sx={{ mt: "auto", mb: "auto" }} onClick={handleClick} />
+            {useMediaQuery(useTheme().breakpoints.up("sm")) ?
+                <Grid item container xs={.6} sm={1.5} md={.7} lg={.6}>
+                    <Checkbox checked={checked || allChecked} id={file.Name} sx={{ mt: "auto", mb: "auto" }} onClick={handleClick} />
+                </Grid>
+                : ""}
+            <Grid item container xs={6.5} sm={4} md={5} direction="row">
+                <FontAwesomeIcon icon={file.IsDir ? faFolder : faFile} style={{ marginTop: "auto", marginBottom: "auto", marginRight: 6, color: "grey" }} />
+                <Typography noWrap sx={{ mt: "auto", mb: "auto", mr: 3, fontWeight: "bold" }} variant="body2">{file.Name}</Typography>
             </Grid>
-            <Grid item container xs={6} direction="row">
-                <FontAwesomeIcon icon={file.type == "directory" ? faFolder : faFile} style={{ marginTop: "auto", marginBottom: "auto", marginRight: 6, color: "grey" }} />
-                <Typography sx={{ mt: "auto", mb: "auto", mr: 3, fontWeight: "bold" }} variant="body2">{file.name}</Typography>
-            </Grid>
-            <Grid item container xs={1}>
-                <Typography sx={{ mt: "auto", mb: "auto" }} variant="body2">{file.size != null || file.size != undefined ? prettyBytes(parseInt(file.size)) : ""}</Typography>
+            {useMediaQuery(useTheme().breakpoints.up("sm")) ?
+                <Grid item container xs={1}>
+                    <Typography sx={{ mt: "auto", mb: "auto" }} variant="body2">{file.Size != null || file.Size != undefined ? prettyBytes(parseInt(file.Size)) : ""}</Typography>
+                </Grid>
+                : ""}
+            <Grid item container xs={4} sm={3} sx={{ ml: "auto" }}>
+                <Typography noWrap sx={{ mt: "auto", mb: "auto" }} variant="body2">{file.ModTime != null || file.ModTime != undefined ? moment().diff(moment(file.ModTime), 'days') < 7 ? moment(file.ModTime).fromNow() : moment(file.ModTime).format("MMMM Do YYYY, h:mm A") : ""}</Typography>
             </Grid>
             <Grid item container xs={1} sx={{ ml: "auto", mt: "auto", mb: "auto" }}>
-                <MoreHoriz id={`options${file.name}`} sx={{ ml: "auto", mr: 2 }} onClick={() => {
+                <MoreHoriz id={`options${file.Name}`} sx={{ ml: "auto", mr: 2 }} onClick={() => {
                     setOpen(!open);
                 }} />
                 <Menu open={open} onClose={() => setOpen(false)} anchorEl={() => {
-                    return document.getElementById("options" + file.name);
+                    return document.getElementById("options" + file.Name);
                 }}>
                     <MenuItem>Delete</MenuItem>
                 </Menu>
