57114f7a5a2070a0be4885aba0dca818c666ce7c
Joseph Maldjian <josephavm@icloud.com>
2022-05-22 18:05:53 -0500

    New Instance Pages

---
+++ pages/instance/[id]/disks.js
@@ -0,0 +1,231 @@
+import { Box, Button, Grid, Modal, Typography, TextField, Paper, Chip, Checkbox, Tooltip, Zoom, Dialog, DialogTitle, DialogContent, useMediaQuery, useTheme, Autocomplete } from "@mui/material";
+import { Delete as DeleteIcon, Edit, History as HistoryIcon } from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import Navigation from "../../../components/instance/Navigation"
+import { InstanceStore } from "../../../states/instance"
+import { useRouter } from "next/router";
+import axios, { post } from "axios";
+import Footer from "../../../components/footer";
+import hyexd from "hyexd";
+import prisma from "../../../lib/prisma";
+import getNodeEnc from "../../../lib/getNodeEnc";
+import prettyBytes from "pretty-bytes";
+
+export async function getServerSideProps({ req, res, query }) {
+    if (!req.cookies.access_token) {
+        return {
+            redirect: {
+                destination: "/auth/login",
+                permanent: false,
+            }
+        }
+    }
+    res.setHeader("Cache-Control", "public, s-maxage=10, stale-while-revalidate=59");
+    const { decode } = require("jsonwebtoken");
+    const user_data = decode(req.cookies.access_token)
+
+    const instance = await prisma.instance.findUnique({
+        where: {
+            id: query.id
+        },
+        include: {
+            node: true
+        }
+    })
+    let disks;
+    const client = new hyexd("https://" + instance.node.address + ":" + instance.node.lxdPort, {
+        certificate: Buffer.from(Buffer(getNodeEnc(instance.node.encIV, instance.node.certificate)).toString(), "base64").toString("ascii"),
+        key: Buffer.from(Buffer(getNodeEnc(instance.node.encIV, instance.node.key)).toString(), "base64").toString("ascii")
+    })
+    let instState = await client.instance(instance.id).state;
+    instState = instState.metadata;
+    disks = await client.instance(instance.id).data;
+    disks = disks.metadata.devices;
+
+    disks = Object.keys(disks).map(key => {
+        if (disks[key].type === "disk") {
+            return {
+                ...disks[key],
+                name: key,
+                usage: instState.disk[key].usage
+            }
+        } else {
+            return -1;
+        }
+    }
+    ).filter(x => x !== -1);
+    return {
+        props: { disks, instanceId: query.id, nodeID: instance.nodeId }
+    }
+
+}
+
+export default function Disks({ disks, instanceId, nodeID }) {
+    const [pools, setPools] = useState([]);
+    const [volumes, setVolumes] = useState([]);
+    const [creatingDisk, setCreatingDisk] = useState(false);
+    const [tempDiskName, setTempDiskName] = useState(null);
+    const [tempDiskPath, setTempDiskPath] = useState(null);
+    const [tempDiskPool, setTempDiskPool] = useState(null);
+    const [tempDiskVolume, setTempDiskVolume] = useState(null)
+    const router = useRouter();
+    useEffect(() => {
+        axios.get(`/api/v1/nodes/${nodeID}/storage-pools`).then(res => {
+            let tempPools = [];
+            let tempVolumes = [];
+            res.data.metadata.forEach(pool => {
+                tempPools.push({
+                    label: pool.name,
+                    value: pool.name
+                });
+                axios.get(`/api/v1/nodes/${nodeID}/storage-pools/${pool.name}/volumes`).then((dat) => {
+                    console.log(dat.data.metadata);
+                    dat.data.metadata.filter(dev => dev.type == "custom").forEach(volume => {
+                        tempVolumes.push({
+                            ...volume,
+                            pool: pool.name,
+                            label: volume.name,
+                            value: volume.name
+
+                        })
+                    })
+                    setVolumes(tempVolumes);
+                })
+                setPools(tempPools);
+                console.log(tempPools)
+            })
+        })
+    }, [])
+    return (
+        <>
+            <Dialog open={creatingDisk} onClose={() => setCreatingDisk(false)}>
+                <DialogTitle>
+                    <Typography variant="h6" fontFamily="Poppins" align="center">Attach Volume</Typography>
+                </DialogTitle>
+                <DialogContent>
+                    <Grid container direction="row">
+                        <Typography fontSize="20px" sx={{ mr: 5, fontWeight: 600, mt: "auto", mb: "auto" }}>Disk Name</Typography>
+                        <TextField onChange={(e) => setTempDiskName(e.target.value)} variant="standard" placeholder="root" value={tempDiskName} sx={{ ml: "auto", mb: "auto" }} />
+                    </Grid>
+                    <Grid container direction="row" sx={{ mt: 2 }}>
+                        <Typography fontSize="20px" sx={{ mr: 5, fontWeight: 600, mt: "auto", mb: "auto" }}>Mount Point</Typography>
+                        <TextField onChange={(e) => setTempDiskPath(e.target.value)} variant="standard" value={tempDiskPath} sx={{ ml: "auto", mb: "auto" }} />
+                    </Grid>
+                    <Grid container direction="row" sx={{ mt: 2 }}>
+                        <Typography fontSize="20px" sx={{ mr: 5, fontWeight: 600, mt: "auto", mb: "auto" }}>Storage Pool</Typography>
+                        {pools.length > 0 ?
+                            <Autocomplete sx={{ mb: "auto", ml: "auto" }} onChange={(e, value) => setTempDiskPool(value.value)} value={tempDiskPool} options={pools} renderInput={(params) => <TextField {...params} variant="standard" placeholder="default" sx={{ width: "248px" }} />} />
+                            : ""}
+                    </Grid>
+                    <Grid container direction="row" sx={{ mt: 2 }}>
+                        <Typography fontSize="20px" sx={{ mr: 5, fontWeight: 600, mt: "auto", mb: "auto" }}>Volume</Typography>
+                        {pools.length > 0 ?
+                            <Autocomplete sx={{ mb: "auto", ml: "auto" }} onChange={(e, value) => setTempDiskVolume(value.value)} value={tempDiskVolume} options={volumes.filter(vol => vol.pool == tempDiskPool)} renderInput={(params) => <TextField {...params} variant="standard" placeholder="default" sx={{ width: "248px" }} />} />
+                            : ""}
+                    </Grid>
+                </DialogContent>
+
+                <Button variant="contained" color="success" onClick={async () => {
+                    try {
+                        await axios.get(`/api/v1/instances/${instanceId}`).then(async (dat) => {
+                            console.log(dat.data)
+                            console.log("THE DATA")
+                            await axios.patch(`/api/v1/instances/${instanceId}`, {
+                                devices: {
+                                    ...dat.data.metadata.devices,
+                                    [tempDiskName]: {
+                                        type: "disk",
+                                        source: tempDiskVolume,
+                                        pool: tempDiskPool,
+                                        path: tempDiskPath
+                                    }
+                                }
+                            })
+                        })
+                    } catch (error) {
+                        console.log(error)
+                    }
+                    setTempDiskName(null);
+                    setCreatingDisk(false);
+                    router.replace(router.asPath);
+                }}>Attach Volume</Button>
+            </Dialog>
+            <Grid container>
+                <Typography variant="h6">Disks</Typography>
+                <Button variant="contained" color="info" sx={{ ml: "auto", mt: "auto", mb: "auto" }} onClick={() => {
+                    setCreatingDisk(true);
+                }}>Attach Volume</Button>
+            </Grid>
+            <Grid container direction="row" sx={{ mb: 1, display: { xs: "none", sm: "none", md: "flex" } }}>
+                <Grid container xs={.6}>
+                    <Checkbox sx={{ mt: "auto", mb: "auto", display: { xs: "none", sm: "none", md: "inherit" } }} />
+                </Grid>
+                <Grid container xs={3} sm={3} md={2} lg={2} xl={1.5}>
+                    <Typography noWrap sx={{ mt: "auto", mb: "auto" }}>Name</Typography>
+                </Grid>
+                <Grid container xs={2} md={1.5} xl={1}>
+                    <Typography sx={{ mt: "auto", mb: "auto" }}>Size</Typography>
+                </Grid>
+                <Grid container xs={2} md={1.5} xl={1}>
+                    <Typography sx={{ mt: "auto", mb: "auto" }}>Mount</Typography>
+                </Grid>
+                <Grid container xs={2} md={1.5} xl={1}>
+                    <Typography sx={{ mt: "auto", mb: "auto" }}>Pool</Typography>
+                </Grid>
+            </Grid>
+            {disks.length == 0 ? <Typography variant="h6">No Disks</Typography> : ""}
+            {disks.map((disk) => {
+                console.log(disk)
+                return (
+                    <Paper sx={{ mt: 1, pt: 1, pb: 1 }} key={disk.name}>
+                        <Grid container direction="row">
+                            <Grid item container xs={.6}>
+                                <Checkbox sx={{ mt: "auto", mb: "auto", display: { xs: "none", sm: "none", md: "flex" } }} />
+                            </Grid>
+                            <Grid container xs={3} sm={3} md={2} lg={2} xl={1.5} sx={{ mt: "auto", mb: "auto" }}>
+                                <Typography fontWeight="bold">{disk.name}</Typography>
+                            </Grid>
+                            <Grid container xs={2} md={1.5} xl={1} sx={{ mt: "auto", mb: "auto" }}>
+                                <Typography>{prettyBytes(disk.usage).replace(" ", "") + " / " + (disk.size ? disk.size : "∞")}</Typography>
+                            </Grid>
+                            <Grid container xs={2} md={1.5} xl={1} sx={{ mt: "auto", mb: "auto" }}>
+                                <Typography>{disk.path}</Typography>
+                            </Grid>
+                            <Grid container xs={2} md={1.5} xl={1} sx={{ mt: "auto", mb: "auto" }}>
+                                <Typography>{disk.pool}</Typography>
+                            </Grid>
+                            <Grid container xs={3} md={4} xl={1.8} sx={{ ml: "auto" }} direction="row">
+                                <Tooltip title="Delete" TransitionComponent={Zoom}>
+                                    <Button onClick={() => {
+                                        axios.get("/api/v1/instances/" + instanceId).then(async (dat) => {
+                                            let devs = dat.data.metadata.devices;
+                                            delete devs[disk.name];
+                                            await axios.put("/api/v1/instances/" + instanceId, {
+                                                ...dat.data.metadata,
+                                                devices: devs
+                                            })
+                                        });
+                                    }} sx={{ ml: "auto", mr: 2, mt: "auto", mb: "auto" }} variant="contained" color="error"><DeleteIcon /></Button>
+                                </Tooltip>
+                                <Tooltip title="Edit" TransitionComponent={Zoom}>
+                                    <Button sx={{ mr: 2, mt: "auto", mb: "auto" }} variant="contained" color="warning"><Edit /></Button>
+                                </Tooltip>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                )
+            })}
+        </>
+    )
+}
+
+Disks.getLayout = (page) => {
+    return (
+        <InstanceStore.Provider>
+            <Navigation page="disks">
+                {page}
+                <Footer />
+            </Navigation>
+        </InstanceStore.Provider>
+    )
+}
--- pages/instance/[id]/index.js
+++ pages/instance/[id]/index.js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { Grid, Paper, Typography, Chip, Button } from "@mui/material";
+import { Grid, Paper, Typography, Chip, Button, useMediaQuery, useTheme } from "@mui/material";
 import useSWR from "swr";
 import { InstanceStore } from "../../../states/instance";
 import axios from "axios";
@@ -9,19 +9,57 @@ import StartButton from "../../../components/instance/StartButton";
 import StopButton from "../../../components/instance/StopButton";
 import Navigation from "../../../components/instance/Navigation";
 import StateIndicator from "../../../components/instance/StateIndicator";
+import decodeToken from "../../../lib/decodeToken";
+import prisma from "../../../lib/prisma";
+import Footer from "../../../components/footer";
+import { Box } from "@mui/system";
 const ResourceCharts = dynamic(() => import("../../../components/instance/ResourceCharts"), {
     ssr: false
 });
-const Terminal = dynamic(() => import("../../../components/instances/terminal"), {
+const Console = dynamic(() => import("../../../components/instance/Console"), {
     ssr: false
 });
 
-export default function Instance({ data }) {
+export async function getServerSideProps({ req, res, query }) {
+    if (!req.cookies.access_token) {
+        return {
+            redirect: {
+                destination: "/auth/login",
+                permanent: false,
+            },
+        }
+    }
+    res.setHeader(
+        "Cache-Control",
+        "public, s-maxage=10, stale-while-revalidate=59"
+    );
+
+    const user_data = decodeToken(req.cookies.access_token);
+    const instance = await prisma.instance.findUnique({
+        where: {
+            id: query.id,
+        },
+        include: {
+            users: true
+        }
+    })
+    if (instance.users.some(user => user.userId === user_data.id)) {
+        return { props: { instance } }
+    } else {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        }
+    }
+}
+export default function Instance({ instance }) {
     const router = useRouter();
     const { id } = router.query;
     console.log(id);
     const fetcher = (url) => axios.get(url).then((res) => res.data);
-    const instance = {
+    const instanceState = {
         data: InstanceStore.useStoreState((state) => state.data),
         setData: InstanceStore.useStoreActions((state) => state.setData),
         containerState: InstanceStore.useStoreState((state) => state.containerState),
@@ -33,84 +71,93 @@ export default function Instance({ data }) {
     };
 
     var { data: instanceData } = useSWR(
-        `/api/v1/client/instances/${id}?include=["magma_cube", "node", "network"]`,
+        `/api/v1/instances/${id}`,
         fetcher
     );
 
     useEffect(() => {
-        if (id) {
-            if (instance.data) {
-                console.log("exists");
-                console.log(instance.data);
-            } else {
-                console.log("doesnt");
-                instance.setData(instanceData);
+        if (id && !instanceState.data) {
+            if (instanceData) {
+                console.log(instanceData)
+                instanceState.setData(instanceData.metadata);
             }
         }
-    }, [instance.data, instanceData]);
+    }, [instance, instanceData, id]);
+
     useEffect(() => {
-        if (instance.data) {
-            console.log("yes");
-            console.log("yes2");
-            if (instance.sockets.monitor) {
-                instance.sockets.monitor.onopen = () => {
-                    axios.get("/api/v1/client/instances/" + instance.data._id + "/monitor/ws").then((res) => {
-                        instance.sockets.monitor.send(res.data.data.access_token);
-                    });
-                };
-            } else {
-                instance.sockets.setMonitor(
+        console.log(instanceState.data)
+        if (instanceState.data) {
+            if (!instanceState.sockets.monitor) {
+                instanceState.sockets.setMonitor(
                     new WebSocket(
-                        `${instance.data.relationships.node.address.ssl ? "wss" : "ws"}://${instance.data.relationships.node.address.hostname
-                        }:${instance.data.relationships.node.address.port}/api/v1/instances/${instance.data._id}/monitor`
+                        `${"ws"}://${instanceState.data.node.address}:${instanceState.data.node.port}/v1/instances/${instanceState.data.id}/state`
                     )
                 );
             }
-        } else {
-            console.log("no");
         }
-    }, [instance.data, instance.sockets.monitor]);
+    }, [instanceState.data, instanceState.sockets.monitor]);
 
     return (
         <>
-            <link
-                rel="stylesheet"
-                href="https://cdnjs.cloudflare.com/ajax/libs/xterm/3.14.5/xterm.min.css"
-                integrity="sha512-iLYuqv+v/P4u9erpk+KM83Ioe/l7SEmr7wB6g+Kg1qmEit8EShDKnKtLHlv2QXUp7GGJhmqDI+1PhJYLTsfb8w=="
-                crossOrigin="anonymous"
-                referrerpolicy="no-referrer"
-            />
             <Paper>
                 <Grid container direction="row" sx={{ p: 2 }}>
-                    <Grid item xs={3}>
-                        <Typography variant="h6">{instance.data ? instance.data.name : ""}</Typography>
+                    <Grid item xs={12} sm={12} md={5} container direction="row">
+                        {useMediaQuery(useTheme().breakpoints.up("md")) ?
+                            <Grid container xs={.5} sm={.8} md={.3} lg={.2} sx={{ mt: "auto", mb: "auto", mr: 1.5 }}>
+                                <StateIndicator />
+                            </Grid> :
+                            <Grid container xs={8} sx={{ mr: "auto", ml: "auto" }}>
+                                <Box sx={{ mr: "auto", ml: "auto" }}>
+                                    <Grid container>
+                                        <Grid container xs={.5} sm={.8} md={.3} lg={.2} sx={{ mt: "auto", mb: "auto", mr: 1 }}>
+                                            <StateIndicator />
+                                        </Grid>
+                                        <Typography align="center" variant="h6" sx={{ mt: "auto", mb: "auto" }}>{instance.name}</Typography>
+                                    </Grid>
+                                </Box>
+                            </Grid>}
+
+                        {useMediaQuery(useTheme().breakpoints.up("md")) ?
+
+                            <Typography variant="h6" sx={{ mt: "auto", mb: "auto" }}>{instance.name}</Typography>
+                            : ""}
                     </Grid>
-                    <Grid container item xs={8} md={5} lg={3.7} xl={3} sx={{ marginLeft: "auto" }}>
+                    {useMediaQuery(useTheme().breakpoints.up("md")) ? <Grid container item xs={12} sm={12} md={4.5} lg={3} xl={2.5} sx={{ marginLeft: "auto" }}>
                         <StartButton instance={id} />
                         <StopButton instance={id} />
                         <Button
+                            disabled={instanceState.monitor.status ? instanceState.monitor.status != "Running" : true}
                             color="warning"
                             variant="contained"
                             sx={{ marginLeft: "auto", marginTop: "auto", marginBottom: "auto" }}
                         >
                             Restart
                         </Button>
-                        <StateIndicator />
-                    </Grid>
+                    </Grid> : <Grid container item xs={12} sm={12} md={4.5} lg={3} xl={2.5} sx={{ marginLeft: "auto" }}>
+                        <StartButton center instance={id} />
+                        <StopButton center instance={id} />
+                        <Button
+                            disabled={instanceState.monitor.status ? instanceState.monitor.status != "Running" : true}
+                            color="warning"
+                            variant="contained"
+                            sx={{ marginLeft: "auto", marginTop: "auto", marginBottom: "auto", marginRight: "auto" }}
+                        >
+                            Restart
+                        </Button>
+                    </Grid>}
                 </Grid>
             </Paper>
             <Grid container xs={12} sx={{ mt: 2 }}>
                 <Grid item xs={12} sx={{ minHeight: "400px" }} container>
-                    {instance ? console.log("asldkfj;lsdkfj") : ""}
-                    {instance.containerState ? instance.data ?
-                        <Terminal status={instance.containerState} instance={instance.data} instanceId={id} />
+                    {instanceState.data && instanceState.monitor ?
+                        <Console />
                         :
-                        "" : ""
+                        ""
                     }
                 </Grid>
             </Grid>
             <Grid container xs={12}>
-                {instance.containerState ?
+                {instanceState.monitor.status ?
                     <ResourceCharts />
                     : ""}
             </Grid>
@@ -121,7 +168,7 @@ export default function Instance({ data }) {
 Instance.getLayout = function getLayout(page) {
     return (
         <InstanceStore.Provider>
-            <Navigation>{page}</Navigation>
+            <Navigation>{page}<Footer /></Navigation>
         </InstanceStore.Provider>
     );
 };
