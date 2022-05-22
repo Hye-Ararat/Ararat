185a496aba467d9d75497eae6aaa5193a2c2f38c
Joseph Maldjian <josephavm@icloud.com>
2022-05-22 14:05:28 -0500

    Update Authentication Endpoints

--- scripts/api/v1/auth/login.js
+++ scripts/api/v1/auth/login.js
@@ -3,20 +3,20 @@ import nookies from "nookies";
 function login(email, password) {
   return new Promise(async (resolve, reject) => {
     try {
-      var auth_response = await axios.post(`/api/v1/client/auth/login`, {
+      var auth_response = await axios.post(`/api/v1/auth/login`, {
         email: email,
         password: password
       });
     } catch (error) {
-      if (error.response.status == 401) return reject(String(error.response.data.data));
+      if (error.response.status == 401) return reject(String(error.response.data.error));
       else return reject("An error occured while sending the request.");
     }
     auth_response = auth_response.data;
-    nookies.set(null, "access_token", auth_response.access_token, {
+    nookies.set(null, "access_token", auth_response.metadata.accessToken, {
       expires: new Date(new Date().getTime() + 15 * 60 * 1000),
       path: "/"
     });
-    nookies.set(null, "refresh_token", auth_response.refresh_token, {
+    nookies.set(null, "refresh_token", auth_response.metadata.refreshToken, {
       expires: new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000),
       path: "/"
     });
