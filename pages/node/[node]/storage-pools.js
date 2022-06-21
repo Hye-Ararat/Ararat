54ac373123603ef81bc0badfebf11897485974b3
Joseph Maldjian <josephavm@icloud.com>
2022-06-21 10:16:49 -0500

    Node Storage Pools Page (begin)

--- pages/node/[node]/storage-pools.js
+++ pages/node/[node]/storage-pools.js
@@ -1,14 +1,15 @@
-import { Grid, Typography } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
 import { useRouter } from "next/router";
 import Navigation from "../../../components/node/navigation";
-import SideLayout from "../../../components/sideLayout";
+import SideLayout, { reformatItemList } from "../../../components/sideLayout";
 import prisma from "../../../lib/prisma";
 import { NodeStore } from "../../../states/node";
 import hyexd from "hyexd";
 import getNodeEnc from "../../../lib/getNodeEnc";
+import { useState } from "react";
 
 
-export async function getServerSideProps({req, res, query}) {
+export async function getServerSideProps({ req, res, query }) {
     if (!req.cookies.access_token) {
         return {
             redirect: {
@@ -28,23 +29,87 @@ export async function getServerSideProps({req, res, query}) {
         certificate: Buffer.from(Buffer.from(getNodeEnc(node.encIV, node.certificate)).toString(), "base64").toString("ascii"),
         key: Buffer.from(Buffer.from(getNodeEnc(node.encIV, node.key)).toString(), "base64").toString("ascii"),
     })
-    storagePools = (await client.storagePools())
+    storagePools = (await client.storagePools(true)).metadata
     console.log(storagePools)
     return {
         props: {
             node,
+            storagePools
         }
     }
 }
 
-export default function StoragePools({ node }) {
+export default function StoragePools({ node, storagePools }) {
     const router = useRouter();
+    const [creatingStoragePool, setCreatingStoragePool] = useState(false);
+    const [selectedStoragePool, setSelectedStoragePool] = useState(null)
     return (
-        <Grid container direction="row" sx={{ mb: 2 }}>
-            <Grid container xs={4}>
-                <Typography variant="h6" sx={{ mt: "auto", mb: "auto" }}>Storage Pools</Typography>
+        <>
+            <Grid container direction="row" sx={{ mb: 2 }}>
+                <Grid container xs={4}>
+                    <Typography variant="h6" sx={{ mt: "auto", mb: "auto" }}>Storage Pools</Typography>
+                </Grid>
+                <Grid container xs={4} sm={4} md={3} sx={{ ml: "auto", mt: "auto", mb: "auto" }}>
+                    <Button color="success" variant="contained" sx={{ ml: "auto" }} onClick={() => setCreatingStoragePool(true)}>
+                        Create Storage Pool
+                    </Button>
+                </Grid>
             </Grid>
-        </Grid>
+            <SideLayout
+                listItems={reformatItemList(storagePools, "name", "name", "driver", "used_by")}
+                thirdItemFormatter={(item) => {
+                    return (
+                        "Used by " + (storagePools.find((pool) => pool.name == item).used_by.length) + " devices"
+                    )
+                }}
+                FarAction={(id) => {
+                    return (
+                        <Button sx={{ ml: "auto", mt: "auto", mb: "auto" }} variant="contained" color="error">
+                            Delete
+                        </Button>)
+                }}
+                FarSection={({ id }) => {
+                    return (
+                        <Grid container direction="column" xs={6} sx={{ mt: "auto", mb: "auto", ml: "auto" }}>
+                            <Typography variant="h6" align="center">{storagePools.find(pool => pool.name == id).config["size"]}</Typography>
+                            <Typography align="center">Size</Typography>
+                        </Grid>
+                    )
+                }}
+                sections={[
+                    {
+                        title: "Volumes",
+                        action: (item) => {
+                            return (
+                                <Button
+                                    onClick={() => ""}
+                                    sx={{ ml: "auto", mt: "auto", mb: "auto" }}
+                                    variant="contained"
+                                    color="info"
+                                >
+                                    Create Volume
+                                </Button>)
+                        },
+                        formatter: (item) => {
+                            return (
+                                <Grid container direction="column">
+                                    <Typography fontWeight={500} align="center">
+                                        No Volumes
+                                    </Typography>
+                                    <Button
+                                        onClick={() => ""}
+                                        sx={{ mr: "auto", ml: "auto", mt: 1 }}
+                                        variant="contained"
+                                        color="info"
+                                    >
+                                        Create One
+                                    </Button>
+                                </Grid>)
+                        }
+                    }
+                ]}
+            />
+        </>
     )
 }
 
