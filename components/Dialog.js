abc20c82eaece418535ade8c0de2933dd2ceb344
Joseph Maldjian <josephavm@icloud.com>
2022-09-09 17:01:08 -0500

    Universal Dialog Component

---
+++ components/Dialog.js
@@ -0,0 +1,14 @@
+import { DialogContent, DialogTitle, Typography } from "@mui/material";
+
+export default function Dialog({ title, open, children }) {
+    return (
+        <Dialog open={open}>
+            <DialogTitle>
+                <Typography variant="h6" align="center" fontFamily="Poppins">{title}</Typography>
+            </DialogTitle>
+            <DialogContent>
+                {children}
+            </DialogContent>
+        </Dialog>
+    )
+}
