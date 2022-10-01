9af81784c49e814d1adb153b753ad2a315583fdc
Joseph Maldjian <josephavm@icloud.com>
2022-10-01 10:31:54 -0500

    in par

--- pages/api/v1/nodes/[id]/system.js
+++ pages/api/v1/nodes/[id]/system.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
         }
     })
     if (!node) return res.status(404).send(errorResponse("Node does not exist", 404))
-    let sysinfo = await axios.get(node.ssl ? "https://" : "http://" + node.address + ":" + node.port + "/v1/system")
+    let sysinfo = await axios.get((node.ssl ? "https://" : "http://") + node.address + ":" + node.port + "/v1/system")
     if (sysinfo.data.arch == "x64") sysinfo.data.arch = "amd64";
     return res.status(200).send(standardResponse(200, sysinfo.data, 200))
 }
