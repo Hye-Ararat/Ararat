6befcb0eac2a70630995337c0391facd2a6fad1d
Joseph Maldjian <joseph.maldjian@hye.gg>
2022-10-04 23:24:13 +0000

    use new permissions system

--- pages/api/v1/images/servers/[server]/index.js
+++ pages/api/v1/images/servers/[server]/index.js
@@ -1,52 +1,18 @@
 import decodeToken from "../../../../../../lib/decodeToken";
-import { getUserPermissions } from "../../../../../../lib/getUserPermissions";
-import prisma from "../../../../../../lib/prisma";
+import Permissions from "../../../../../../lib/permissions/index.js";
+import { errorResponse } from "../../../../../../lib/responses.js";
 
 export default async function handler(req, res) {
     const tokenData = decodeToken(req.headers["authorization"].split(" ")[1]);
     const { query: { server } } = req;
-    let image_server = null;
-    if ((await getUserPermissions(tokenData.id)).includes("list-images")) {
-        image_server = await prisma.imageServer.findUnique({
-            where: {
-                id: server
-            }
-        });
-    } else {
-        let tempImageServer = await prisma.imageServer.findUnique({
-            where: {
-                id: server,
-            },
-            include: {
-                users: {
-                    select: {
-                        userId: true
-                    }
-                }
-            }
-        })
-        let found = false;
-        let iterations = 0;
-        while (!found && iterations < tempImageServer.users.length) {
-            if (tempImageServer.users[iterations].userId == tokenData.id) {
-                found = true;
-            }
-            iterations += 1;
+    let permissions = new Permissions(tokenData.id);
+    if (!await permissions.imageServer(server).useImages) return res.status(403).send(errorResponse("You are not allowed to use this image server.", 403))
+    let image_server = await prisma.imageServer.findUnique({
+        where: {
+            id: server
         }
-        if (found) {
-            image_server = tempImageServer;
-        }
-    }
-    if (!image_server) {
-        return res.status(403).send({
-            status_code: 403,
-            status: "Forbidden",
-            type: "error",
-            "error": "You are not allowed to access this resource"
-        })
-    }
-    delete image_server.users;
-
+    })
+    if (!image_server) return res.status(404).send(errorResponse("Image server does not exist.", 404))
     return res.status(200).send({
         status_code: 200,
         status: "Success",
