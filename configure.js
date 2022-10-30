7d766f751991201edddceeb5e62d3d972150f64b
zachary#0842 <codingwithice3fire@gmail.com>
2022-10-30 06:11:56 -0400

    Revert Changes

--- configure.js
+++ configure.js
@@ -92,11 +92,11 @@ const sleep = (ms) => {
         execSync('sudo apt-get install -y nginx', { stdio: [0, 1, 2] })
         execSync('rm /etc/nginx/sites-enabled/default', { stdio: [0, 1, 2] })
         execSync('sudo apt-get install -y certbot python3-certbot-nginx', { stdio: [0, 1, 2] })
-        execSync(`sudo certbot certonly --nginx -d ${domain.value} --agree-tos --register-unsafely-without-email -n`, { stdio: [0, 1, 2] });
         execSync('wget -O /etc/nginx/sites-enabled/ararat.conf https://raw.githubusercontent.com/Hye-Ararat/Ararat/master/ararat.conf', { stdio: [0, 1, 2] });
         let conf = fs.readFileSync("/etc/nginx/sites-enabled/ararat.conf", "utf8");
         conf = conf.replaceAll("example.com", `${domain.value}`);
         fs.writeFileSync("/etc/nginx/sites-enabled/ararat.conf", conf);
+        execSync(`sudo certbot --nginx -d ${domain.value} --agree-tos --no-redirect --register-unsafely-without-email -n`, { stdio: [0, 1, 2] });
         execSync('systemctl restart nginx', { stdio: [0, 1, 2] });
     }
     const url = await prompts({
