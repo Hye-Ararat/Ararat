9b6e8aadc6e42b5ae91b31d7a62b54e35ac9614b
Joseph Maldjian <joseph.maldjian@hye.gg>
2023-01-18 17:33:41 +0000

    list nodes

--- app/(dashboard)/nodes/page.tsx
+++ app/(dashboard)/nodes/page.tsx
@@ -1,13 +1,24 @@
+import prisma from "../../../lib/prisma.js";
+
 import {Typography, Button, Grid} from "../../../components/base";
 import CreateNode from "./CreateNode";
 
 export default async function Nodes() {
+    const nodes = await prisma.node.findMany({})
+    console.log(nodes)
     return (
+        <>
         <Grid container direction="row">
         <Typography variant="h4">Nodes</Typography>
  
                    <CreateNode />
 
         </Grid>
+        {nodes.map((node) => {
+            return (
+                <p key={node.name}>{node.name}</p>
+            )
+        })}
+        </>
     )
 }
