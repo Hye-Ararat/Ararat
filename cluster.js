49f8ed5caed1e5882c043742c0b6e0df11529c4a
Hye-Dev <joseph.maldjian@hye.gg>
2023-01-20 16:28:44 +0000

    Add Cluster Configuration

--- cluster.js
+++ cluster.js
@@ -1,17 +1,68 @@
 const axios = require("axios").default;
-const {readFileSync, writeFileSync} = require("fs");
-const {execSync}= require("child_process");
+const ws = require("ws").WebSocket;
 
 (async function cluster() {
-    const requestClient = new axios.Axios({
-        socketPath: "/var/snap/lxd/common/lxd/unix.socket"
+    let addr = process.argv[2];
+    let name = process.argv[3];
+      const requestClient = new axios.Axios({
+        socketPath: "/var/snap/lxd/common/lxd/unix.socket",
+        headers: {
+            "Content-Type": "application/json",
+        }
     })
     let data = await requestClient.get("/1.0");
     console.log(JSON.parse(data.data))
-    let preseed = readFileSync("./preseed.yaml", "utf8");
-    writeFileSync("./preseed.yaml", preseed.replace("addressHere", "103.70.39.84:8443").replace("serverNameHere", "Development"))
-    let yes = execSync("cat preseed.yaml | lxd init --preseed")
-    console.log(yes.toString())
+    console.log("💻 Setting Listen Config");
+    let config;
+    try {
+        config = await requestClient.put("/1.0", JSON.stringify({
+            "config": {
+                "cluster.https_address": `${addr}:8443`,
+                "core.https_address": `${addr}:8443`
+            }
+        }))
+    } catch (error) {
+        return console.log(error)
+    }
+    console.log(config.data);
+    console.log("💻 Connecting to Events Socket");
+    const WebSocket = new ws("ws+unix:///var/snap/lxd/common/lxd/unix.socket:/1.0/events");
+    WebSocket.on("open", () => {
+        console.log("WebSocket Open")
+    })
+    WebSocket.on("message", (data) => {
+        let message = JSON.parse(data.toString());
+        try {
+            if (message.type == "operation") {
+                if (message.metadata.context.description == "Creating bootstrap node") {
+                    if (message.metadata.message == "Success for operation") {
+                        console.log("💻 Cluster Created");
+                        WebSocket.close();
+                        process.exit(0);
+                    }
+                }
+            }   
+        } catch (error) {
+            
+        }
+    })
 
-console.log("e")
+    console.log("💻 Setting Cluster Config");
+    let clusterConfig;
+    try {
+        clusterConfig = await requestClient.put("/1.0/cluster", JSON.stringify({
+            "server_name": `${name}`,
+            "enabled": true,
+            "member_config": null,
+            "cluster_address": "",
+            "cluster_certificate": "",
+            "server_address": "",
+            "cluster_password": ""
+        })
+        )
+    } catch (error) {
+        return console.log(error)
+    }
+    console.log(clusterConfig.data);
+
 })()
