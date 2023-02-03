112aad803784974526c6578b906967bd4b6e565a
Hye-Dev <joseph.maldjian@hye.gg>
2023-02-03 18:58:36 +0000

    backdrop

--- components/base.ts
+++ components/base.ts
@@ -1,5 +1,5 @@
 "use client";
-import { Typography, Button, Grid, Dialog, DialogTitle, DialogContent, Divider, Stepper, Step, StepLabel, DialogActions, TextField, CircularProgress, Paper, Select, MenuItem, Menu, Input, InputLabel, InputAdornment, Checkbox, Grow, Zoom, Breadcrumbs } from "@mui/material";
+import { Typography, Button, Grid, Dialog, DialogTitle, DialogContent, Divider, Stepper, Step, StepLabel, DialogActions, TextField, CircularProgress, Paper, Select, MenuItem, Menu, Input, InputLabel, InputAdornment, Checkbox, Grow, Zoom, Breadcrumbs, Backdrop } from "@mui/material";
 
 
-export {Typography, Button, Grid, Dialog, DialogTitle, DialogContent, Divider, Stepper, Step, StepLabel, DialogActions, TextField, CircularProgress, Paper, Select, MenuItem, Menu, Input, InputLabel, InputAdornment, Checkbox, Grow, Zoom, Breadcrumbs}
+export {Typography, Button, Grid, Dialog, DialogTitle, DialogContent, Divider, Stepper, Step, StepLabel, DialogActions, TextField, CircularProgress, Paper, Select, MenuItem, Menu, Input, InputLabel, InputAdornment, Checkbox, Grow, Zoom, Breadcrumbs, Backdrop}
