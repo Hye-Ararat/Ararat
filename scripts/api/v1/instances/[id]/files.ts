e79101ebf6a482ef9f4e883ccfcc62b27a455581
Joseph Maldjian <joseph.maldjian@hye.gg>
2023-02-21 20:32:02 +0000

    file creation

--- scripts/api/v1/instances/[id]/files.ts
+++ scripts/api/v1/instances/[id]/files.ts
@@ -1,6 +1,7 @@
 import nookies from "nookies";
+import { post } from "../../../../lib/requestClient";
 
-export default async function getInstanceFile(id: string, path: string, authorization: string) {
+export async function getInstanceFile(id: string, path: string, authorization: string) {
     return new Promise(async (resolve, reject) => {
         fetch(`${typeof document != 'undefined' ? "" : "http://127.0.0.1:3000"}/api/v1/instances/${id}/files?path=${path}` , {
             method: "GET",
@@ -18,3 +19,20 @@ export default async function getInstanceFile(id: string, path: string, authoriz
         })      
     })
   }
+
+export async function createInstanceFile(id: string, path: string, data?: string) {
+    let token = nookies.get(null).authorization;
+    let length = data ? data.length : 0;
+    return new Promise(async (resolve, reject) => {
+        let config: RequestInit = {
+            headers: {
+                "Content-Type": "text/plain",
+                "Content-Length": length.toString()
+            },
+            cache: "no-cache",
+        }
+        if (data) config.body = data;
+        let dat = await post(`/api/v1/instances/${id}/files?path=${path}`, config, token);
+        resolve(dat)
+    })
+}
