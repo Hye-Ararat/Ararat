f7a3d58a3fbdc743e91416539c13a6eb7088644f
Joseph Maldjian <joseph.maldjian@hye.gg>
2023-03-07 23:26:31 +0000

    New Fallbacks (hardcoded routes as of now)

--- next.config.js
+++ next.config.js
@@ -26,6 +26,26 @@ module.exports = {
         {
           source: "/api/v1/:path*",
           destination: "http://127.0.0.1:3001/api/v1/:path*"
+        },
+        {
+          source: "/api/authentication/auth/:path*",
+          destination: "http://127.0.0.1:3002/api/authentication/auth:path*"
+        },
+        {
+          source: "/interaction/:path*",
+          destination: "http://127.0.0.1:3002/interaction/:path*"
+        },
+        {
+          source: "/api/authentication/:path*",
+          destination: "http://127.0.0.1:3002/:path*"
+        },
+        {
+          source: "/candid/:path*",
+          destination: "http://127.0.0.1:8082/:path*"
+        },
+        {
+          source: "/1.0/:path*",
+          destination: "https://us-dal-1.hye.gg:8443/1.0/:path*"
         }
       ]
     }
