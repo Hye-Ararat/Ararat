18265e678919b064725d7d50156b3dfc8f0a97b9
Joseph Maldjian <joseph.maldjian@hye.gg>
2023-04-12 22:26:05 +0000

    Add OIDC server and LXD fallbacks

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+import caddyConfig from "./caddyConfig.json"
+
 module.exports = {
   reactStrictMode: false,
   experimental: {
@@ -28,12 +30,32 @@ module.exports = {
           destination: "http://127.0.0.1:3001/api/v1/:path*"
         },
         {
+          source: "/realms/Ararat/:path*",
+          destination: "http://127.0.0.1:8080/realms/Ararat/:path*"
+        },
+        {
+          source: "/resources/:path*",
+          destination: "http://127.0.0.1:8080/resources/:path*"
+        },
+        {
+          source: "/api/authentication/device/auth/:path*",
+          destination: "http://127.0.0.1:3002/api/authentication/device/auth/:path*"
+        },
+        {
+          source: "/api/authentication/device/:path*",
+          destination: "http://127.0.0.1:3002/api/authentication/device/:path*"
+        },
+        {
           source: "/api/authentication/auth/:path*",
           destination: "http://127.0.0.1:3002/api/authentication/auth/:path*"
         },
         {
           source: "/api/authentication/token/:path*",
           destination: "http://127.0.0.1:3002/api/authentication/token:path*"
+        },
+        {
+          source: "/api/authentication/jwks/:path*",
+          destination: "http://127.0.0.1:3002/api/authentication/jwks:path*"
         },
         {
           source: "/interaction/:path*",
@@ -49,7 +71,7 @@ module.exports = {
         },
         {
           source: "/1.0/:path*",
-          destination: "https://us-dal-1.hye.gg:8443/1.0/:path*"
+          destination: `https://${caddyConfig.apps.http.servers.ararat.routes[0].match[0].host}:8443/1.0/:path*`
         }
       ]
     }
