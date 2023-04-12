dee5e9cadbc80134288332c77901972fa47ee39f
Joseph Maldjian <joseph.maldjian@hye.gg>
2023-04-12 22:32:27 +0000

    LXD client

---
+++ lib/lxd.ts
@@ -0,0 +1,10 @@
+import {connectOIDC, connectUnix} from "lxdjs";
+import caddyConfig from "../caddyConfig.json";
+
+export default function lxd(access_token) {
+    return connectOIDC(`https:/${caddyConfig.apps.http.servers.ararat.routes[0].match[0].host}`, access_token);
+}
+
+export function lxdUnix() {
+    return connectUnix("/var/snap/lxd/common/lxd/unix.socket")
+}
