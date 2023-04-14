4260e9fa89245ab4e2937f592ba93129efcf9230
Joseph Maldjian <joseph.maldjian@hye.gg>
2023-04-14 14:06:41 +0000

    update match

--- lib/lxd.ts
+++ lib/lxd.ts
@@ -2,7 +2,7 @@ import {connectOIDC, connectUnix} from "lxdjs";
 import caddyConfig from "../caddyConfig.json";
 
 export default function lxd(access_token) {
-    return connectOIDC(`https:/${caddyConfig.apps.http.servers.ararat.routes[0].match[0].host}`, access_token);
+    return connectOIDC(`https://${caddyConfig.apps.http.servers.ararat.routes[0].match[0].host}`, access_token);
 }
 
 export function lxdUnix() {
