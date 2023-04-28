f7990cf8ef4ef8d83433f887e90dd87e217699c9
Joseph Maldjian <joseph.maldjian@hye.gg>
2023-04-28 18:32:03 +0000

    add censored state

--- lib/accessHandler/instance.ts
+++ lib/accessHandler/instance.ts
@@ -2,7 +2,7 @@ import { InstanceFull } from "lxdjs/dist/lib/lxd/instance";
 import { NextRequest } from "next/server";
 import { lxdUnix } from "../lxd";
 import prisma from "../prisma";
-import { notAllowed, sendData } from "./responses";
+import { allowed, notAllowed, sendData } from "./responses";
 import AccessHandler from ".";
 
 export default class Instance {
@@ -23,9 +23,10 @@ export default class Instance {
     }
 
     async hasPermission(permission: string) {
-        if (await this.hasPermission(permission)) return true;
+        if (await this.accessClient.hasPermission(permission)) return true;
         //get instance and check here instead
         let instance = await lxdUnix().instances.instance(this.id).metadata();
+        console.log(instance)
         let sub = this.payload["sub"];
         if (instance.config[`user.users.${sub}`]) {
             let permissions = JSON.parse(instance.config[`user.users.${sub}`]);
@@ -39,13 +40,17 @@ export default class Instance {
         let sub = this.payload["sub"];
         let path = request.nextUrl.pathname;
         let search = request.nextUrl.search;
+        console.log(path)
+        console.log(this.id)
         if (path == `/1.0/instances/${this.id}`) {
+            console.log("tes2")
             let instance;
             if (search.includes("recursion=1")) {
                 instance = await lxdUnix().instances.instance(this.id).metadata(true);
             } else {
                 instance = await lxdUnix().instances.instance(this.id).metadata();
             }
+            console.log("YESSSS")
             if (instance.config[`user.users.${sub}`]) {
                 if (search.includes("recursion=1")) {
                     let permissions = JSON.parse(instance.config[`user.users.${sub}`]);
@@ -56,6 +61,31 @@ export default class Instance {
                 return sendData(instance);
             }
         }
+        console.log(path)
+        if (path == `/1.0/instances/${this.id}/state`) {
+            console.log("YES ROUTE")
+            if (request.method == "PUT") {
+                console.log("CHECKING")
+                if (await this.hasPermission("change-state_instance")) return allowed();
+            }
+            if (request.method == "GET") {
+                if (await this.hasPermission("get-state_instance")) return allowed();
+                let state = await lxdUnix().instances.instance(this.id).getState();
+                delete state.cpu;
+                delete state.memory;
+                delete state.network;
+                delete state.disk;
+                delete state.processes;
+                delete state.pid;
+                return sendData(state);
+            }
+        }
+        if (path == `/1.0/instances/${this.id}/console`) {
+            if (request.method == "POST") {
+                if (await this.hasPermission("connect-console_instance")) return allowed();
+            }
+        }
+
         return notAllowed();
     }
 }
