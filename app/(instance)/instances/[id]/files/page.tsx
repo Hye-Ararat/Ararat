db920a8824bdb6f81a40acb2d619018b1a9a2569
Joseph Maldjian <joseph.maldjian@hye.gg>
2023-05-03 01:11:15 +0000

    use new API

--- app/(instance)/instances/[id]/files/page.tsx
+++ app/(instance)/instances/[id]/files/page.tsx
@@ -5,9 +5,41 @@ import {cookies} from "next/headers";
 import FileEditor from "./FileEditor";
 import NewFile from "./NewFile"
 import UploadFile from "./UploadFile";
+import lxd from "../../../../../lib/lxd";
+import FileView from "./FileView";
 
 export default async function Files({params, searchParams}) {
-    let files = await getInstanceFile(params.id, searchParams.path || "/", cookies().get("authorization").value);
+    let id = params.id;
+    let accessToken = cookies().get("access_token").value;
+    let client = lxd(accessToken);
+    let instance = client.instances.instance(id);
+    let instanceData = await instance.metadata(true);
+    let files = await instance.getFile(searchParams.path || "/");
+    console.log(files.metadata)
+    console.log(files.data)
+    let fileMetadata = [];
+    if (files.metadata.type == "directory") {
+    let e = new Promise((resolve, reject) => {
+        if (files.metadata.type == "directory") {
+            files.data.forEach(async (file, index) => {
+                let filePath = ((searchParams.path ? searchParams.path : "") + "/" + file).replace("//", "/");
+                console.log(filePath, "THE FILE PATH")
+                let fileData = await instance.getFile(filePath);
+                console.log(fileData, "this is the data")
+                if (fileData.metadata.type == "file") {
+                    let size = fileData.data.length;
+                    fileData.metadata["size"] = size;
+                }
+                fileData.metadata["name"] = file;
+              
+                fileMetadata.push(fileData.metadata)
+                console.log(fileMetadata)
+                if (index == files.data.length - 1) return resolve()
+            })
+        }  
+    })
+    await e;
+}
     return (
         <>
         <Grid container>
@@ -15,12 +47,11 @@ export default async function Files({params, searchParams}) {
         <UploadFile id={params.id} path={searchParams.path || "/"} />
         <NewFile id={params.id} path={searchParams.path || "/"} />
         </Grid>
-        {typeof files == "string" ?
-        
-        <FileEditor file={files} path={searchParams.path} instance={params.id} /> 
+        {files.metadata.type == "file" ?
+        <FileView instanceData={instanceData} file={files} path={searchParams.path} instance={params.id} accessToken={accessToken} />
     
         : 
-         <FilesTable id={params.id} path={searchParams.path || "/"} files={files} />}
+         <FilesTable accessToken={accessToken} id={params.id} path={searchParams.path || "/"} files={fileMetadata} />}
 
         </>
     )
