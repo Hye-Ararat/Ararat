65e3fba602375ef66795ff1529441c247262c982
Joseph Maldjian <josephavm@icloud.com>
2023-06-28 22:06:22 -0500

    feat: implement OIDC authentication

---
+++ src/authentication/configuration.js
@@ -0,0 +1,124 @@
+const fs = require("fs");
+const generateKeyPair = require("jose").generateKeyPair;
+const exportJWK = require("jose").exportJWK;
+
+async function setupKey() {
+    console.log("Checking for key...");
+    if (fs.existsSync("./key")) {
+        console.log("Key found!");
+        return;
+    };
+    console.log("No key found, generating...");
+    const keypair = await generateKeyPair("RS256");
+    const jwk = await exportJWK(keypair.privateKey);
+    fs.writeFileSync("./key", JSON.stringify(jwk));
+    console.log("Key generated!");
+    return;
+}
+setupKey();
+
+
+let url = "localhost:3000"
+
+const configuration = {
+    //adapter: PrismaAdapter,
+    scopes: ['openid', 'profile', 'email', 'offline_access'],
+    clients: [
+        {
+            client_id: 'lxd',
+            client_secret: 'test',
+            redirect_uris: ["http://localhost:5556/auth/lxd/callback", `http://${url}/api/authentication/callback`],
+            client_name: "Hye Ararat",
+            logo_uri: "https://linuxcontainers.org/lxd/docs/latest/_images/containers.png",
+            scope: "openid profile email offline_access",
+            token_endpoint_auth_method: "none",
+            grant_types: ["authorization_code", "urn:ietf:params:oauth:grant-type:device_code", "refresh_token", "client_credentials"]
+        }
+    ],
+    routes: {
+        authorization: '/oidc/auth',
+        backchannel_oidc: '/oidc/backchannel',
+        code_verification: '/oidc/device',
+        device_authorization: '/oidc/device/auth',
+        end_session: '/oidc/session/end',
+        introspection: '/oidc/token/introspection',
+        jwks: '/oidc/jwks',
+        pushed_authorization_request: '/oidc/request',
+        registration: '/oidc/reg',
+        revocation: '/oidc/token/revocation',
+        token: '/oidc/token',
+        userinfo: '/oidc/me'
+    },
+    cookies: {
+        keys: [process.env.ENC_KEY],
+    },
+    jwks: {
+        keys: [JSON.parse(fs.readFileSync("./key", "utf8"))]
+    },
+    features: {
+        devInteractions: {
+            enabled: false
+        },
+        deviceFlow: {
+            enabled: true,
+        },
+        clientCredentials: {
+            enabled: true,
+        },
+        introspection: {
+            enabled: true,
+        },
+        resourceIndicators: {
+            enabled: true,
+            defaultResource() {
+                return `http://${url}`;
+            },
+            getResourceServerInfo(ctx, resourceIndicator, client) {
+                return ({
+                    scope: 'openid profile email offline_access',
+                    audience: 'lxd',
+                    accessTokenTTL: 1 * 60 * 60, // 2 hours
+                    accessTokenFormat: 'jwt'
+                });
+            },
+            useGrantedResource(ctx, model) {
+                return true;
+            }
+        },
+    },
+    interactions: {
+        url(ctx, interaction) {
+            try {
+            console.log(interaction, "INTERACTION")
+            let interactionId = interaction.returnTo.split("/").pop();
+            console.log(interactionId, "INTERACTION ID")
+            console.log(interaction.prompt.name, "INTERACTION PROMPT NAME")
+            if (interaction.prompt.name == "login") {
+                return `http://${url}/authentication/login?interaction=${interactionId}`;
+            }
+            if (interaction.prompt.name == "consent") {
+                return `http://${url}/authentication/authorize?interaction=${interactionId}`;
+            }
+        } catch (err) {
+            console.log("ERROR", err)
+        }
+        }
+    },
+    ttl: {
+        Grant: 1 * 60 * 60, // 1 hour
+    },
+    findAccount(ctx, sub, token) {
+        console.log(sub, "THIS IS THE SUB")
+        return {
+            accountId: sub,
+            async claims(use, scope, claims, rejected) {
+                console.log(use, scope, claims, rejected, "THIS IS THE CLAIMS")
+                return {
+                    sub: sub,
+                }
+            }
+        }
+    }
+};
+
+module.exports = configuration;
