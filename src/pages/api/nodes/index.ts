d09ba121c3f2b85857467c3ed8fd199b0de25bbd
Wolfo <klokko06@gmail.com>
2023-07-04 08:53:12 +0000

    Add types to node creation route

--- src/pages/api/nodes/index.ts
+++ src/pages/api/nodes/index.ts
@@ -1,6 +1,7 @@
 import prisma from "@/lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     let body = req.body;
     const newNode = await prisma.node.create({
         data: {
