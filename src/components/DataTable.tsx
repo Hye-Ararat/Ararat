00b1a8e4b90c076b62f6531ad3f609f88e8e3bc8
Wolfo <klokko06@gmail.com>
2023-07-06 08:16:56 +0000

    Beatify DataTable component

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,11 +1,11 @@
 import { useMantineTheme, Group, Avatar, Badge, Anchor, ActionIcon, ScrollArea, Table, Text } from "@mantine/core";
 import { IconPencil, IconTrash } from "@tabler/icons-react";
-import { Children, ReactNode } from "react";
+import { Children, MouseEventHandler, ReactNode } from "react";
 
 export function DataTable({ children, headings }: { children: ReactNode, headings?: string[] }) {
     return (
         <ScrollArea>
-            <Table sx={{ minWidth: 800, borderSpacing: "0 10px", borderCollapse: "separate" }} verticalSpacing="md">
+            <Table sx={{ minWidth: 800, borderSpacing: "0 15px", borderCollapse: "separate" }} verticalSpacing="lg">
                 {headings ? <thead>
                     <tr key={"head"}>
                         {headings.map(heading => <th style={{ border: 0 }}>{heading}</th>)}
@@ -17,13 +17,14 @@ export function DataTable({ children, headings }: { children: ReactNode, heading
     );
 }
 
-export function DataTableRow({ children }: { children: ReactNode }) {
-    var firstStyle = { borderTopLeftRadius: "10px", borderBottomLeftRadius: "10px", backgroundColor: "#1a1b1e", marginTop: 30, border: 0 }
-    var lastStyle = { borderTopRightRadius: "10px", borderBottomRightRadius: "10px", backgroundColor: "#1a1b1e", marginTop: 30, border: 0 }
-    var normalStyle = { backgroundColor: "#1a1b1e", marginTop: 30, border: 0 }
+export function DataTableRow({ children, active,onClick }: { children: ReactNode, active?: boolean, onClick?: MouseEventHandler<HTMLTableDataCellElement> }) {
+    if (!active) active = false;
+    var firstStyle = { borderTopLeftRadius: "10px", borderBottomLeftRadius: "10px", backgroundColor: active ? "#28292e" : "#1a1b1e", marginTop: 30, border: 0 }
+    var lastStyle = { borderTopRightRadius: "10px", borderBottomRightRadius: "10px", backgroundColor: active ? "#28292e" : "#1a1b1e", marginTop: 30, border: 0 }
+    var normalStyle = { backgroundColor: active ? "#28292e" : "#1a1b1e", marginTop: 30, border: 0 }
     var mappedChildren = Children.map(children, (child, i) => {
         return (
-            <td key={i} style={i == 0 ? firstStyle : (i == (children as any).length - 1 ? lastStyle : normalStyle )}>
+            <td key={i} style={i == 0 ? firstStyle : (i == (children as any).length - 1 ? lastStyle : normalStyle)} onClick={onClick}>
                 {child}
             </td>
         )
