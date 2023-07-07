1bffb78164029f886dd02d37e49689e5b6962932
Joseph Maldjian <joseph.maldjian@hye.gg>
2023-07-07 02:54:26 +0000

    begin adding file creation popup

--- src/pages/instances/[instance]/files.tsx
+++ src/pages/instances/[instance]/files.tsx
@@ -1,5 +1,5 @@
 import { LxdInstance } from "@/types/instance";
-import { ActionIcon, Anchor, Breadcrumbs, Button, Grid, Group, Menu, SegmentedControl, Text, Center } from "@mantine/core";
+import { ActionIcon, Anchor, Breadcrumbs, Button, Grid, Group, Menu, SegmentedControl, Text, Center, Modal } from "@mantine/core";
 import { IconArrowLeft, IconArrowRight, IconFile, IconFolder, IconLayoutGrid, IconLayoutList, IconPlus } from "@tabler/icons-react";
 import { useContext, useEffect, useState } from "react";
 import { GridFileView } from "@/components/instances/instance/files/GridFileView";
@@ -8,6 +8,7 @@ import { connectOIDC } from "js-lxd";
 import { InstanceContext } from "@/components/AppShell";
 import { GetServerSidePropsContext } from "next";
 import { ParsedUrlQuery } from "querystring";
+import { useTheme } from "@emotion/react";
 
 export async function getServerSideProps({ req, res, params }: GetServerSidePropsContext) {
     // TODO: iterate nodes
@@ -33,12 +34,27 @@ export async function getServerSideProps({ req, res, params }: GetServerSideProp
 }
 export default function InstanceFiles({ instance }: { instance: LxdInstance }) {
     let [fileView, setFileView] = useState<string>("grid")
-    const [instanceData, setInstanceData]= useContext(InstanceContext); 
+    const [creatingFile, setCreatingFile] = useState(false);
+    const [instanceData, setInstanceData]= useContext(InstanceContext);
     useEffect(() => {
         setInstanceData(instance);
     }, [])
+    useEffect(() => {
+        if (creatingFile) {
+            let audio = new Audio("/audio/popup.mp3");
+            audio.play();
+        }
+    }, [creatingFile])
+    const theme = useTheme()
     return (
         <>
+        <Modal centered overlayProps={{
+                color: theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[2],
+                opacity: 0.55,
+                blur: 3,
+            }}  opened={creatingFile} onClose={() => setCreatingFile(false)} title="Create File">
+            
+        </Modal>
          <Group spacing={10} mt={"sm"}>
             <ActionIcon color="blue" variant="light" size={"md"}>
                 <IconArrowLeft />
@@ -65,7 +81,7 @@ export default function InstanceFiles({ instance }: { instance: LxdInstance }) {
                     <Menu.Item icon={<IconFolder />}>
                         Directory
                     </Menu.Item>
-                    <Menu.Item icon={<IconFile />}>
+                    <Menu.Item onClick={() => setCreatingFile(true)} icon={<IconFile />}>
                         File
                     </Menu.Item>
                 </Menu.Dropdown>
