11c45e37decf830a2ee97660e6e0f00657e9f0fd
Wolfo <klokko06@gmail.com>
2023-07-09 15:38:19 +0000

    Move to diffrent instances route layout

---
+++ src/pages/instances/[node]/[instance]/networks.tsx
@@ -0,0 +1,37 @@
+import InstanceShell from "@/components/instances/instance/InstanceShell";
+import { fetchInstance } from "@/lib/lxd";
+import { redirect } from "@/lib/next";
+import { validateSession } from "@/lib/oidc";
+import { NodeLxdInstance } from "@/types/instance";
+import { GetServerSidePropsContext } from "next";
+import { ParsedUrlQuery } from "querystring";
+
+export async function getServerSideProps({ req, res, params, query }: GetServerSidePropsContext) {
+    if (!params || !params.instance || !params.node) return redirect("/instances");
+    var valid = await validateSession((req.cookies as any).access_token)
+    if (!valid) {
+        res.setHeader("Set-Cookie", ["access_token=deleted; Max-Age=0"])
+        return redirect("/authentication/login")
+    };
+    try {
+        let instance: NodeLxdInstance | undefined = await fetchInstance((params.instance as string), (params.node as string), (req.cookies.access_token as string))
+        if (!instance) return redirect('/instances');
+
+        return {
+            props: {
+                instance: instance
+            }
+        }
+    } catch (error) {
+        console.log(error)
+        return redirect(`/instances`);
+    }
+}
+
+export default function InstanceNetworks({ instance }: {instance:NodeLxdInstance}) {
+    return (
+        <>
+            <InstanceShell instance={instance} />
+        </>
+    )
+}
