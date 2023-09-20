90701b7cbd2d327a3c6e60501ace84fb44ae9969
Joseph Maldjian <josephavm@icloud.com>
2023-09-20 01:58:29 -0500

    use new authentication connection

--- src/pages/api/authentication/callback.ts
+++ src/pages/api/authentication/callback.ts
@@ -3,6 +3,7 @@ import { serialize } from "cookie"
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    console.log("asdfasdf")
     let url = process.env.URL;
     const formBody = new URLSearchParams();
     formBody.append("grant_type", "authorization_code");
@@ -11,9 +12,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     formBody.append("client_id", "lxd");
     formBody.append("scope", "openid profile");
     formBody.append("code_verifier", (await oidcClient()).codeVerifier);
+    console.log("asdfasdf2")
+    console.log(formBody.toString())
 
     process.env.NODE_DEBUG = "http"
-    const tokenResponse = await fetch(`http://${url}/oidc/token`, {
+    const tokenResponse = await fetch(`http://127.0.0.1:3002/oidc/token`, {
         method: "POST",
         headers: {
             "Content-Type": "application/x-www-form-urlencoded"
@@ -21,7 +24,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         body: formBody.toString(),
         cache: "no-cache"
     })
+    
     let data = await tokenResponse.json();
+    console.log("asdfasdf3")
+    console.log(data)
     let expires = data.expires_in;
     let id_token_cookie = serialize("id_token", data.id_token, {
         path: "/",
@@ -33,5 +39,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     });
     res.setHeader("Cache-Control", "no-cache");
     res.setHeader("Set-Cookie", [id_token_cookie, access_token_cookie]);
-    return res.redirect("/")
+    res.redirect("/")
+    res.end()
+    return;
 }
