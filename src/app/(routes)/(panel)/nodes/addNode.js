b69f2d0aca243a8c3a155bd1594a5bbc03e8dc41
Joseph Maldjian <josephavm@icloud.com>
2023-10-03 10:22:48 -0500

    add node creation music

--- src/app/(routes)/(panel)/nodes/addNode.js
+++ src/app/(routes)/(panel)/nodes/addNode.js
@@ -2,18 +2,34 @@
 
 import Modal from "@/app/_components/modal";
 import { Button, Text } from "@mantine/core";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function AddNode() {
   const [addingNode, setAddingNode] = useState(false);
+  const [audio, setAudio] = useState();
+  useEffect(() => {
+    setAudio(new Audio("/audio/createNode.m4a"));
+  }, []);
   return (
     <>
-      <Button onClick={() => setAddingNode(true)} ml="auto" my="auto">
+      <Button
+        onClick={() => {
+          audio.play();
+          audio.loop = true;
+          setAddingNode(true);
+        }}
+        ml="auto"
+        my="auto"
+      >
         Add Node
       </Button>
       <Modal
         opened={addingNode}
-        onClose={() => setAddingNode(false)}
+        onClose={() => {
+          setAddingNode(false);
+          audio.pause();
+          audio.currentTime = 0;
+        }}
         size="xl"
         title="Add Node"
       >
