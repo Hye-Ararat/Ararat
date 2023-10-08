aae3bbaf72e2eac65bde977a97d04fbf7fbb3f52
Joseph Maldjian <josephavm@icloud.com>
2023-10-08 15:25:22 -0500

    refactor

--- src/app/(routes)/(auth)/auth/login/page.js
+++ src/app/(routes)/(auth)/auth/login/page.js
@@ -1,5 +1,5 @@
-import { getProviders, signIn, useSession } from "next-auth/react"
-import { getServerSession } from "next-auth/next"
+import { getProviders, signIn, useSession } from "next-auth/react";
+import { getServerSession } from "next-auth/next";
 import { authOptions } from "../../../api/auth/[...nextauth]/route";
 import { redirect, useSearchParams } from "next/navigation";
 import LoginForm from "./LoginForm";
@@ -7,19 +7,23 @@ import { BackgroundImage } from "@mantine/core";
 import { validateSession } from "@/app/_lib/session";
 
 export default async function SignIn() {
-    var session = await validateSession(false)
-    if (session) return redirect('/');
-    const providers = JSON.parse(JSON.stringify(authOptions.providers)).map((p) => {
-        return {
-            id: p.id,
-            name: p.name
-        };
-    })
-    const random = Math.floor(Math.random() * 10) + 1;
-    return (
-        <BackgroundImage src={`/images/login/login${random}.jpg`} style={{width: "100%", height: "100%"}}>
-            <LoginForm providers={providers} />
-        </BackgroundImage>
-
-    );
+  var session = await validateSession(false);
+  if (session) return redirect("/");
+  const providers = JSON.parse(JSON.stringify(authOptions.providers)).map(
+    (p) => {
+      return {
+        id: p.id,
+        name: p.name,
+      };
+    }
+  );
+  const random = Math.floor(Math.random() * 10) + 1;
+  return (
+    <BackgroundImage
+      src={`/images/login/login${random}.jpg`}
+      style={{ width: "100%", height: "100%" }}
+    >
+      <LoginForm providers={providers} />
+    </BackgroundImage>
+  );
 }
