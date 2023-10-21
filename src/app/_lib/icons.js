c3511104b9f053eb74cca24876a898a052bc2472
Joseph Maldjian <josephavm@icloud.com>
2023-10-21 17:33:12 -0500

    icon fetching

---
+++ src/app/_lib/icons.js
@@ -0,0 +1,10 @@
+import { SiDell, SiHp, SiUbuntu } from "@icons-pack/react-simple-icons";
+
+export function getBrandIcon(brand, size, style) {
+  switch (brand) {
+    case "Dell":
+      return <SiDell style={style} size={size} color="#0076CE" />;
+    case "HP":
+      return <SiHp style={style} size={size} color="#0096D6" />;
+  }
+}
