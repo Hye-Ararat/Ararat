9bdc960bc2de26e07d6bbdbb0f589b034dd09c83
Joseph Maldjian <josephavm@icloud.com>
2023-10-21 17:32:57 -0500

    update data table styling

--- src/app/_components/datatable.js
+++ src/app/_components/datatable.js
@@ -1,22 +1,33 @@
 "use client";
-import { Avatar, Center, Grid, Group, Paper, Text, Stack, Button } from "@mantine/core"
-import classes from "./datatable.module.css"
+import {
+  Avatar,
+  Center,
+  Grid,
+  Group,
+  Paper,
+  Text,
+  Stack,
+  Button,
+  Flex,
+  Box,
+  SimpleGrid,
+} from "@mantine/core";
+import classes from "./datatable.module.css";
 import { IconHeadset } from "@tabler/icons-react";
 import React from "react";
+import Link from "next/link";
 
 const genRand = (len) => {
-    return Math.random().toString(36).substring(2,len+2);
-  }
+  return Math.random()
+    .toString(36)
+    .substring(2, len + 2);
+};
 
 /**
- * @param {React.PropsWithChildren<{}>} props 
+ * @param {React.PropsWithChildren<{}>} props
  */
 export function DataTable(props) {
-    return (
-        <div style={{ width: "100%" }}>
-            {props.children}
-        </div>
-    )
+  return <div style={{ width: "100%" }}>{props.children}</div>;
 }
 
 /**
@@ -26,36 +37,55 @@ export function DataTable(props) {
  *    value: string
  *   }[],
  *   icon: React.ReactNode
- * }>} props 
+ * }>} props
  */
-export function DataTableRow(props) {
-    return (
-        <Paper className={classes.row} key={genRand(10)}>
-            <Group>
-                <Avatar>
-                        {props.icon}
-                </Avatar>
-                <div className={classes.col}>
-                    <Text mt={"auto"} mb={"auto"} fw={600}>Instance</Text>
-                </div>
-                <Group style={{ marginLeft: "auto", marginRight: "auto" }}>
-                    {props.columns.map(col => {
-                        return (
-                            <div className={classes.col} style={{ marginLeft: "75px", marginRight: "75px" }}>
-                                <Stack gap={0}>
-                                    <Text fw={600}>{col.value}</Text>
-                                    <Text fz={"xs"} style={{ color: "gray" }}>{col.name}</Text>
-                                </Stack>
-                            </div>
-                        )
-                    })}
-                </Group>
-
-                <div className={classes.col} style={{ marginLeft: "auto" }}>
-                    <Button>Manage</Button>
-
-                </div>
-            </Group>
-        </Paper>
-    )
+export function DataTableRow({ columns, icon, button }) {
+  return (
+    <>
+      <Paper className={classes.row} p="md" radius="lg" mb="xs">
+        <Flex>
+          {icon}
+          <Flex direction="column" my="auto" ml="sm" style={{ width: "20vw" }}>
+            <Text fz="md" fw={550}>
+              {columns[0].primary}
+            </Text>
+            <Text c="dimmed" fz="xs">
+              {columns[0].secondary}
+            </Text>
+          </Flex>
+          <SimpleGrid ml="xl" cols={columns.length - 1}>
+            {columns.map((column, i) => {
+              if (i == 0) return "";
+              return (
+                <Flex direction="column" my="auto" ml="sm">
+                  <Text fz="md" fw={550}>
+                    {column.primary}
+                  </Text>
+                  <Text c="dimmed" fz="xs">
+                    {column.secondary}
+                  </Text>
+                </Flex>
+              );
+            })}
+          </SimpleGrid>
+          {button ? (
+            <Link
+              href={button.link}
+              style={{
+                marginLeft: "auto",
+                marginTop: "auto",
+                marginBottom: "auto",
+              }}
+            >
+              <Button ml="auto" my="auto">
+                {button.text}
+              </Button>
+            </Link>
+          ) : (
+            ""
+          )}
+        </Flex>
+      </Paper>
+    </>
+  );
 }
