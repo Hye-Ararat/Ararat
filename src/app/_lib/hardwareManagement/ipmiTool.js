f2bc8bf6bae47cd9e81772337b0a63a0a22942af
Joseph Maldjian <josephavm@icloud.com>
2023-10-29 01:22:24 -0500

    revamp structure

--- src/app/_lib/hardwareManagement/ipmiTool.js
+++
@@ -1,8 +0,0 @@
-import { execSync } from "node:child_process";
-
-export default async function ipmiTool(ip, username, password, command) {
-  let dat = execSync(
-    `ipmitool -I lanplus -H ${ip} -U ${username} -P ${password} ${command}`
-  );
-  return dat.toString();
-}
