b077ccc0a2b94d6150251bc0f5c0a357b1ebb42d
Joseph Maldjian <josephavm@icloud.com>
2023-10-29 01:23:22 -0500

    add new accordion table component

---
+++ src/app/_components/accordionTable.js
@@ -0,0 +1,37 @@
+"use client";
+
+import { Accordion, Flex, SimpleGrid, Text } from "@mantine/core";
+
+export default function AccordionTable({ rows }) {
+  return (
+    <Accordion variant="separated">
+      {rows.map((row) => {
+        return <Row key={row.id} row={row} />;
+      })}
+    </Accordion>
+  );
+}
+
+function Row({ row }) {
+  return (
+    <Accordion.Item value={row.id}>
+      <Accordion.Control>
+        <SimpleGrid columns={row.columns.length} w={"100%"}>
+          {row.columns.map((column) => {
+            return (
+              <Flex direction="column" my="auto" ml="sm">
+                <Text fz="md" fw={550}>
+                  {column.primary}
+                </Text>
+                <Text c="dimmed" fz="xs">
+                  {column.secondary}
+                </Text>
+              </Flex>
+            );
+          })}
+        </SimpleGrid>
+      </Accordion.Control>
+      <Accordion.Panel>{row.panel}</Accordion.Panel>
+    </Accordion.Item>
+  );
+}
