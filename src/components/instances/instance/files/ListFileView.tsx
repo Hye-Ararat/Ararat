f860f8859f26cbcdea7ad26c571bda5a8fd2b65e
joseph <josephavm@icloud.com>
2024-01-10 21:04:45 +0000

    fix deletion

--- src/components/instances/instance/files/ListFileView.tsx
+++ src/components/instances/instance/files/ListFileView.tsx
@@ -90,7 +90,7 @@ export function ListFileView({ files, path }: { files: string[], path: string })
                 <Dialog transition={"pop"} opened={selectedFiles.filter(s => s.checked == true).length > 0} sx={{ width: 111 }} radius="md" position={{ bottom: 80, left: leftValue }}>
                     <Button onClick={() => {
                         async function deleteFiles() {
-                            var files = selectedFiles;
+                            let files = selectedFiles.filter(s => s.checked == true);
                             var client = connectOIDC((instance?.node.url as string), access_token)
                             Promise.all(files.map((file) => {
                                 client.delete(`/instances/${instance.name}/files?path=${path}/${file.id}`).then(s => {
